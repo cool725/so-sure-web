2850e7ed756b9158f6ab3c0b0fd09bb20935bee2
Patrick McAndrew <patrick@peerj.com>
2016-01-24 13:21:29 +0000

    start on terraform

---
+++ ansible/run-terraform.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+AWS_PROFILE=sosure
+export AWS_ACCESS_KEY_ID=`credstash -p $AWS_PROFILE -r eu-west-1 get ops/setup/access_key`
+export AWS_SECRET_ACCESS_KEY=`credstash -p $AWS_PROFILE -r eu-west-1 get ops/setup/secret_key`
+
+terraform apply
