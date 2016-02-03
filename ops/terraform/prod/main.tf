c9e6e561d0794363c2ee731a16329c5316ad48f4
Patrick McAndrew <patrick@peerj.com>
2016-02-03 22:02:09 +0000

    fix ssh key perm

--- ops/terraform/prod/main.tf
+++ ops/terraform/prod/main.tf
@@ -295,7 +295,7 @@ resource "aws_elb" "web" {
 
 resource "aws_launch_configuration" "prod_web" {
     name_prefix = "web-v0-lc-"
-    image_id = "ami-4ed86e3d"
+    image_id = "ami-d1db6da2"
     instance_type = "t2.micro"
     security_groups = ["${aws_security_group.web.id}"]
     iam_instance_profile = "prod-web"
