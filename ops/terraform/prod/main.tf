4cd34e56c81568ca2b8cfa5ada8536f8baa3594a
Patrick McAndrew <patrick@so-sure.com>
2016-02-12 12:40:48 +0000

    add deploy role

--- ops/terraform/prod/main.tf
+++ ops/terraform/prod/main.tf
@@ -400,7 +400,7 @@ resource "aws_elb" "web" {
 
 resource "aws_launch_configuration" "prod_web" {
     name_prefix = "web-v0-lc-"
-    image_id = "ami-4cc87b3f"
+    image_id = "ami-4dcc7f3e"
     instance_type = "t2.micro"
     security_groups = ["${aws_security_group.web.id}"]
     iam_instance_profile = "prod-web"
