c8ab0ef94daf7dc2eba959807ae77cd7a8124b69
Patrick McAndrew <patrick@so-sure.com>
2016-02-14 04:55:41 +0000

    move to ops repo

--- ops/ansible/run-deploy-local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
-
-ansible-playbook -i "deploy_inventory" --connect=local deploy.yml
--- ops/packer/run-packer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-AWS_PROFILE="sosure"
-ACCESS_KEY=`credstash -p $AWS_PROFILE -r eu-west-1 get ops/setup/access_key`
-SECRET_KEY=`credstash -p $AWS_PROFILE -r eu-west-1 get ops/setup/secret_key`
-
-packer build \
-    -var "aws_access_key=$ACCESS_KEY" \
-    -var "aws_secret_key=$SECRET_KEY" \
-    $@ 
