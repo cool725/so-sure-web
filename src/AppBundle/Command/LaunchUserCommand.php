615f58c000cb61160b434cbd5a4263a8645a27f4
Patrick McAndrew <patrick@so-sure.com>
2016-02-19 09:33:31 +0000

    welcome and launch email functionality

--- src/AppBundle/Command/LaunchUserCommand.php
+++ src/AppBundle/Command/LaunchUserCommand.php
@@ -21,6 +21,11 @@ class LaunchUserCommand extends ContainerAwareCommand
                 'email',
                 InputArgument::REQUIRED,
                 'Email to add'
+            )->addOption(
+                'resend',
+                null,
+                InputOption::VALUE_NONE,
+                'Resend launch email'
             )
         ;
     }
@@ -28,13 +33,14 @@ class LaunchUserCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $email = $input->getArgument('email');
+        $resend = true === $input->getOption('resend');
         $shortLink = $this->getContainer()->get('app.shortlink');
         $router = $this->getContainer()->get('router');
         $launchUser = $this->getContainer()->get('app.user.launch');
 
         $user = new User();
         $user->setEmail($email);
-        $user = $launchUser->addUser($user);
+        $user = $launchUser->addUser($user, $resend);
         $url = $router->generate('homepage', ['referral' => $user->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
         $url = $shortLink->addShortLink($url);
         $output->writeln(sprintf('%s,%s', $email, $url));
