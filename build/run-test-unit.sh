365ab9e86157c8a75341e7d229a7250ff134c4fd
Patrick McAndrew <patrick@so-sure.com>
2016-03-16 13:55:40 +0000

    shouldn't be doing anything with the db for unit tests

--- build/run-test-unit.sh
+++ build/run-test-unit.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
-app/console --env=test sosure:doctrine:index
 ./vendor/phing/phing/bin/phing -f build/test.xml test:unit
