bf7ee93c5506f080a9a7711cb808f874a802d6c4
Patrick McAndrew <patrick@so-sure.com>
2016-03-23 13:02:27 +0000

    use stdclass/mocks instead of classes to fix phpcs

--- src/AppBundle/Tests/Service/PCAServiceTest.php
+++ src/AppBundle/Tests/Service/PCAServiceTest.php
@@ -5,32 +5,6 @@ namespace AppBundle\Tests\Controller;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use AppBundle\Document\User;
 
-class AddressTest
-{
-    public $Line1;
-    public $Line2;
-    public $Line3;
-    public $Line4;
-    public $Line5;
-    public $City;
-    public $PostalCode;
-}
-
-class MockAddress
-{
-    private $address;
-
-    public function __construct($address)
-    {
-        $this->address = $address;
-    }
-    
-    public function attributes()
-    {
-        return $this->address;
-    }
-}
-
 /**
  * @group functional
  */
@@ -60,14 +34,16 @@ class PCAServiceTest extends WebTestCase
 
     public function testTransform3()
     {
-        $testAddress = new AddressTest();
+        $testAddress = new \stdClass();
         $testAddress->Line1 = '1';
         $testAddress->Line2 = '2';
         $testAddress->Line3 = '3';
+        $testAddress->Line4 = null;
+        $testAddress->Line5 = null;
         $testAddress->City = 'city';
         $testAddress->PostalCode = 'se15';
 
-        $address = self::$pca->transformAddress(new MockAddress($testAddress));
+        $address = self::$pca->transformAddress($this->getTransformMock($testAddress));
         $this->assertEquals('1', $address->getLine1());
         $this->assertEquals('2', $address->getLine2());
         $this->assertEquals('3', $address->getLine3());
@@ -77,15 +53,16 @@ class PCAServiceTest extends WebTestCase
 
     public function testTransform4()
     {
-        $testAddress = new AddressTest();
+        $testAddress = new \stdClass();
         $testAddress->Line1 = '1';
         $testAddress->Line2 = '2';
         $testAddress->Line3 = '3';
         $testAddress->Line4 = '4';
+        $testAddress->Line5 = null;
         $testAddress->City = 'city';
         $testAddress->PostalCode = 'se15';
 
-        $address = self::$pca->transformAddress(new MockAddress($testAddress));
+        $address = self::$pca->transformAddress($this->getTransformMock($testAddress));
         $this->assertEquals('1', $address->getLine1());
         $this->assertEquals('2', $address->getLine2());
         $this->assertEquals('3, 4', $address->getLine3());
@@ -95,7 +72,7 @@ class PCAServiceTest extends WebTestCase
 
     public function testTransform5()
     {
-        $testAddress = new AddressTest();
+        $testAddress = new \stdClass();
         $testAddress->Line1 = '1';
         $testAddress->Line2 = '2';
         $testAddress->Line3 = '3';
@@ -104,11 +81,22 @@ class PCAServiceTest extends WebTestCase
         $testAddress->City = 'city';
         $testAddress->PostalCode = 'se15';
 
-        $address = self::$pca->transformAddress(new MockAddress($testAddress));
+        $address = self::$pca->transformAddress($this->getTransformMock($testAddress));
         $this->assertEquals('1, 2', $address->getLine1());
         $this->assertEquals('3', $address->getLine2());
         $this->assertEquals('4, 5', $address->getLine3());
         $this->assertEquals('se15', $address->getPostcode());
         $this->assertEquals('city', $address->getCity());
+    }
+
+    protected function getTransformMock($address)
+    {
+        $stub = $this->getMockBuilder('nonexistant')
+            ->setMockClassName('foo')
+            ->setMethods(array('attributes'))
+            ->getMock();
+        $stub->method('attributes')->willReturn($address);
+
+        return $stub;
     }
 }
