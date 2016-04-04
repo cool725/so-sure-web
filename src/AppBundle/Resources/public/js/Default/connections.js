ae08b44e258bebf6bba84192ead01438963a461e
Patrick McAndrew <patrick@so-sure.com>
2016-04-04 20:18:54 +0100

    dynamically set pot values and ensure max pot display in js is correct

--- src/AppBundle/Resources/public/js/Default/connections.js
+++ src/AppBundle/Resources/public/js/Default/connections.js
@@ -1,6 +1,7 @@
 
 var ctx = $("#connectionsChart").get(0).getContext("2d");
 var max_value = $('#connection-value').data('slider-max');
+var maxpot_value = $('#connection-value').data('slider-maxpot');
 var initial_value = $('#connection-value').data('slider-value');
 
 var data = [
@@ -14,16 +15,20 @@ var data = [
         value: max_value - initial_value,
         color:"#EFEFEF",
         highlight: "#6F6F6F",
-        label: "Possible additional savings"
+        label: "Possible savings"
     },
 ]
 
 var connectionsDoughnutChart = new Chart(ctx).Doughnut(data, {
-    tooltipTemplate: "<%if (label){%><%=label%>: <%}%>£<%= value * 10 %>",    
+    tooltipTemplate: "<%if (label){%><%=label%>: <%}%>£<%if (value * 10 > " + maxpot_value + "){%>" + maxpot_value + "<%} else {%><%= value * 10 %><% }%>",
 });
 
 var setConnectionText = function() {
-    var connectionText = "With " + slider.getValue() + " connections, you could save £" + slider.getValue() * 10 + " if you and your friend don't claim in the year";
+    var save_value = slider.getValue() * 10;
+    if (save_value > maxpot_value) {
+        save_value = maxpot_value;
+    }
+    var connectionText = "With " + slider.getValue() + " connections, you could save £" + save_value + " if you and your friend don't claim in the year";
     $('#connectionLegend').text(connectionText);
 }
 
