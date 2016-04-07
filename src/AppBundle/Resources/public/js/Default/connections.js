248b60ddc53b76c26e6ea87e27b1748269fc8a76
Patrick McAndrew <patrick@so-sure.com>
2016-04-07 10:41:55 +0100

    fix tooltip for potential value

--- src/AppBundle/Resources/public/js/Default/connections.js
+++ src/AppBundle/Resources/public/js/Default/connections.js
@@ -3,24 +3,30 @@ var ctx = $("#connectionsChart").get(0).getContext("2d");
 var max_value = $('#connection-value').data('slider-max');
 var maxpot_value = $('#connection-value').data('slider-maxpot');
 var initial_value = $('#connection-value').data('slider-value');
-
+var adjusted_potential_value = maxpot_value - (max_value * 10);
 var data = [
     {
         value: initial_value,
         color:"#1B262D",
         highlight: "#3399FF",
+        // If changing text, verify tooltipTemplate isn't affected
         label: "Pot Value"
     },
     {
         value: max_value - initial_value,
         color:"#EFEFEF",
         highlight: "#6F6F6F",
+        // If changing text, verify tooltipTemplate isn't affected
         label: "Potential Value"
     },
 ]
 
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2")  + "e-2");
+}
+
 var connectionsDoughnutChart = new Chart(ctx).Doughnut(data, {
-    tooltipTemplate: "<%if (label){%><%=label%>: <%}%>£<%if (value * 10 > " + maxpot_value + "){%>" + maxpot_value + "<%} else {%><%= value * 10 %><% }%>",
+    tooltipTemplate: "<%if (label){%><%=label%>: <%}%>£<% if (label && label.indexOf('Pot ') > -1) { if (value * 10 > " + maxpot_value + "){%>" + maxpot_value + "<%} else {%><%= value * 10 %><% }} else { %><%= roundToTwo(" + adjusted_potential_value + " + value * 10 ) %><% }%>"
 });
 
 var setConnectionText = function() {
