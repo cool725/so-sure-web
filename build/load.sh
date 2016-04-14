8fc1b26658cd59c5639dca2ddaf7b2c4f58d0cd0
Patrick McAndrew <patrick@so-sure.com>
2016-04-14 21:31:29 +0100

    Admin pages to mirror claims

--- build/load.sh
+++ build/load.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+
+IS_PRODUCTION=`grep mongodb_url app/config/parameters.yml | grep prod.so-sure.net | wc -l`
+if [ "$IS_PRODUCTION" != "0" ]; then
+    echo "This is a production server.  Do not run!"
+    exit 1
+else
+    echo "Non-production server - safe to run"
+fi
+
 sudo app/console --env=vagrant doctrine:mongodb:fixtures:load 
 sudo app/console --env=vagrant fos:user:create patrick@so-sure.com patrick@so-sure.com test
 sudo app/console --env=vagrant fos:user:promote patrick@so-sure.com ROLE_CLAIMS
+sudo app/console --env=vagrant fos:user:promote patrick@so-sure.com ROLE_ADMIN
