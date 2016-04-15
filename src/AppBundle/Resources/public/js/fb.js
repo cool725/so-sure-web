08435e8fc82315e27d4ba252781261d3b7404d7b
Patrick McAndrew <patrick@so-sure.com>
2016-04-15 15:29:03 +0100

    fix facebook sdk

--- src/AppBundle/Resources/public/js/fb.js
+++ src/AppBundle/Resources/public/js/fb.js
@@ -1,36 +1,17 @@
-    window.fbAsyncInit = function() {
-        // init the FB JS SDK
-        FB.init({
-            appId      : $('#ss-root').data('fb-id'),                        // App ID from the app dashboard
-            channelUrl : '//yourdomain.com/channel.html',      // Channel file for x-domain comms
-            status     : true,                                 // Check Facebook Login status
-            xfbml      : true                                  // Look for social plugins on the page
-        });
-    };
-
-    // Load the SDK asynchronously
-    (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/all.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
-    function fb_login() {
-        FB.getLoginStatus(function(response) {
-            if (response.status === 'connected') {
-                // connected
-                document.location = $('#ss-root').data('fb-redirect');
-            } else {
-                // not_authorized
-                FB.login(function(response) {
-                    if (response.authResponse) {
-                        document.location = $('#ss-root').data('fb-redirect');
-                    } else {
-                        alert('Cancelled.');
-                    }
-                }, {scope: 'email'});
-            }
-        });
-    }
+$('.facebook_login').on('click', function() {
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            // connected
+            document.location = $('#ss-root').data('fb-redirect');
+        } else {
+            // not_authorized
+            FB.login(function(response) {
+                if (response.authResponse) {
+                    document.location = $('#ss-root').data('fb-redirect');
+                } else {
+                    alert('Cancelled.');
+                }
+            }, {scope: 'email'});
+        }
+    });
+});
