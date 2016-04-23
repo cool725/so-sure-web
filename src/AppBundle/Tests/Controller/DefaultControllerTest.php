06375a95c5b35fd2d432e31ab23c79efea80c92d
Patrick McAndrew <patrick@so-sure.com>
2016-04-23 15:47:59 +0100

    add test cases to ensure phones appear and quote is correct price

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -4,11 +4,13 @@ namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use AppBundle\Document\User;
+use AppBundle\Document\Phone;
+use Symfony\Component\DomCrawler\Field\ChoiceFormField;
 
 /**
  * @group functional-net
  */
-class DefaultControllerTest extends WebTestCase
+class DefaultControllerTest extends BaseControllerTest
 {
     use \AppBundle\Tests\PhingKernelClassTrait;
 
@@ -19,74 +21,85 @@ class DefaultControllerTest extends WebTestCase
     
     public function testIndex()
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $crawler = self::$client->request('GET', '/');
+        self::verifyResponse(200);
+        $form = $crawler->selectButton('launch_phone[next]')->form();
+        $values = [];
+        foreach ($form->all() as $field) {
+            if ($field instanceof ChoiceFormField) {
+                $values = $field->availableOptionValues();
+            }
+        }
+        $this->assertGreaterThan(10, count($values));
+    }
+
+    public function testQuotePhone()
+    {
+        $repo = self::$dm->getRepository(Phone::class);
+        $phone = $repo->findOneBy(['devices' => 'iPhone 5', 'memory' => 64]);
+
+        $url = sprintf('/quote/%s', $phone->getId());
+        $crawler = self::$client->request('GET', $url);
+        self::verifyResponse(200);
+        $this->assertContains(
+            sprintf("£%.2f", $phone->getCurrentPolicyPremium()->getPolicyPrice()),
+            self::$client->getResponse()->getContent()
+        );
     }
 
     public function testLaunchIndex()
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = self::$client->request('GET', '/');
         $form = $crawler->selectButton('launch_bottom[save]')->form();
         
         // set some values
         $form['launch_bottom[email]'] = 'foo@bar.com';
         // submit the form
-        $crawler = $client->submit($form);
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $crawler = self::$client->submit($form);
+        self::verifyResponse(302);
 
-        $crawler = $client->followRedirect();
+        $crawler = self::$client->followRedirect();
         $this->assertContains('http://goo.gl', $crawler->text());
     }
 
     public function testLaunchRetryIndex()
     {
-        $client = static::createClient();
-        $dm = $this->getManager($client);
-        $repo = $dm->getRepository(User::class);
+        $repo = self::$dm->getRepository(User::class);
         $fooUser = $repo->findOneBy(['email' => 'foo@bar.com']);
 
-        $crawler = $client->request('GET', '/');
+        $crawler = self::$client->request('GET', '/');
         $form = $crawler->selectButton('launch_bottom[save]')->form();
         
         // set some values
         $form['launch_bottom[email]'] = 'foo@bar.com';
         // submit the form
-        $crawler = $client->submit($form);
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $crawler = self::$client->submit($form);
+        self::verifyResponse(302);
 
-        $crawler = $client->followRedirect();
+        $crawler = self::$client->followRedirect();
         $this->assertContains('http://goo.gl', $crawler->text());
     }
 
     public function testLaunchReferalIndex()
     {
-        $client = static::createClient();
-        $dm = $this->getManager($client);
-        $repo = $dm->getRepository(User::class);
+        $repo = self::$dm->getRepository(User::class);
         $fooUser = $repo->findOneBy(['email' => 'foo@bar.com']);
         $this->assertTrue($fooUser !== null);
 
-        $crawler = $client->request('GET', sprintf('/?referral=%s', $fooUser->getId()));
+        $crawler = self::$client->request('GET', sprintf('/?referral=%s', $fooUser->getId()));
         $form = $crawler->selectButton('launch_bottom[save]')->form();
 
         // set some values
         $form['launch_bottom[email]'] = 'bar@foo.com';
         // submit the form
-        $crawler = $client->submit($form);
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $crawler = self::$client->submit($form);
+        self::verifyResponse(302);
 
-        $crawler = $client->followRedirect();
+        $crawler = self::$client->followRedirect();
         $this->assertContains('http://goo.gl', $crawler->text());
 
         $barUser = $repo->findOneBy(['email' => 'bar@foo.com']);
         $this->assertEquals($fooUser->getId(), $barUser->getReferred()->getId());
         $this->assertEquals(1, count($fooUser->getReferrals()));
-    }
-
-    protected function getManager($client)
-    {
-        return $client->getContainer()->get('doctrine_mongodb.odm.default_document_manager');
     }
 }
