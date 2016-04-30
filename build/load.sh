50b397e43a9a3cd12a1d4c24f6faadc36e72b2de
Patrick McAndrew <patrick@so-sure.com>
2016-04-30 10:25:08 +0100

    move create user to fixture add claims user

--- build/load.sh
+++ build/load.sh
@@ -11,6 +11,3 @@ else
 fi
 
 sudo app/console --env=vagrant doctrine:mongodb:fixtures:load 
-sudo app/console --env=vagrant fos:user:create patrick@so-sure.com patrick@so-sure.com test
-sudo app/console --env=vagrant fos:user:promote patrick@so-sure.com ROLE_CLAIMS
-sudo app/console --env=vagrant fos:user:promote patrick@so-sure.com ROLE_ADMIN
