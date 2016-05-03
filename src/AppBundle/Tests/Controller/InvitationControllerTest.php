5c8416522c1f10b899de02bb789b8cebae5e3ce7
Patrick McAndrew <patrick@so-sure.com>
2016-05-03 16:30:13 +0100

    fix invitation template

---
+++ src/AppBundle/Tests/Controller/InvitationControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use AppBundle\Document\Invitation\EmailInvitation;
+
+/**
+ * @group functional-net
+ */
+class InvitationControllerTest extends BaseControllerTest
+{
+    use \AppBundle\Tests\PhingKernelClassTrait;
+
+    public function tearDown()
+    {
+    }
+
+    public function testInvitation()
+    {
+        $invitationRepo = self::$dm->getRepository(EmailInvitation::class);
+        $invitation = $invitationRepo->findOneBy([]);
+        $url = sprintf('/invitation/%s', $invitation->getId());
+        $crawler = self::$client->request('GET', $url);
+        self::verifyResponse(200);
+    }
+}
