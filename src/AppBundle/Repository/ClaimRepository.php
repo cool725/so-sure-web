2f5179113794cbd9336794133440d6f010d7e00f
Patrick McAndrew <patrick@so-sure.com>
2016-06-02 16:29:25 +0100

    fix phpcs

--- src/AppBundle/Repository/ClaimRepository.php
+++ src/AppBundle/Repository/ClaimRepository.php
@@ -8,6 +8,8 @@ class ClaimRepository extends DocumentRepository
 {
     public function getAllClaimsForExport(\DateTime $date)
     {
+        \AppBundle\Classes\NoOp::noOp([$date]);
+        // TODO: state open or closed date within x weeks of date
         return $this->createQueryBuilder()
             ->getQuery()
             ->execute();
