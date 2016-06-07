3b94d67e74601a58ca48be335e9fd736e33c329f
Patrick McAndrew <patrick@so-sure.com>
2016-06-07 13:30:24 +0100

    for staging, add ability to random fail responses + manually set response

---
+++ src/AppBundle/Command/ApiResponseCommand.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\Table;
+use AppBundle\Listener\ApiResponseSubscriber;
+
+class ApiResponseCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('sosure:apiresponse')
+            ->setDescription('Return known error for testing')
+            ->addOption(
+                'random',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Random % to fail with 500'
+            )
+            ->addOption(
+                'path',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Url path (start with /api/v1/)'
+            )
+            ->addOption(
+                'path-error',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'If used with path, error code to use',
+                500
+            )
+            ->addOption(
+                'clear',
+                null,
+                InputOption::VALUE_NONE,
+                'Clear all responses'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $path = $input->getOption('path');
+        $pathError = $input->getOption('path-error');
+        $clear = true === $input->getOption('clear');
+        $random = $input->getOption('random');
+
+        $redis = $this->getContainer()->get('snc_redis.default');
+        if ($random) {
+            $redis->set(ApiResponseSubscriber::KEY_RANDOM_FAILURE, $random);
+        }
+        if ($path) {
+            $redis->hset(ApiResponseSubscriber::KEY_HASH_PATH, $path, $pathError);
+        }
+        
+        if ($clear) {
+            $redis->del(ApiResponseSubscriber::KEY_RANDOM_FAILURE);
+            $redis->del(ApiResponseSubscriber::KEY_HASH_PATH);
+            $output->writeln('Cleared');
+        } else {
+            $output->writeln(sprintf("Response %s%%", $redis->get(ApiResponseSubscriber::KEY_RANDOM_FAILURE)));
+            print_r($redis->hgetall(ApiResponseSubscriber::KEY_HASH_PATH));
+        }
+        
+    }
+}
---
+++ src/AppBundle/Listener/ApiResponseSubscriber.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AppBundle\Listener;
+
+use Doctrine\ODM\MongoDB\Event\LifecycleEventArgs;
+use AppBundle\Document\Invitation\Invitation;
+use AppBundle\Event\InvitationEvent;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ApiResponseSubscriber implements EventSubscriberInterface
+{
+    const KEY_HASH_PATH = 'apiresponse:paths';
+    const KEY_RANDOM_FAILURE = 'apiresponse:random';
+    protected $redis;
+
+    public function __construct($redis)
+    {
+        $this->redis = $redis;
+    }
+
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        print $request->getPathInfo();
+        if (stripos($request->getPathInfo(), '/api/v1/') === 0) {
+            if (($errorCode = $this->redis->hget(self::KEY_HASH_PATH, $request->getPathInfo())) !== null) {
+                $event->setResponse($this->generateJsonError($errorCode));
+            } elseif (($random = $this->redis->get(self::KEY_RANDOM_FAILURE)) !== null) {
+                if (rand(0, 100) <= $random) {
+                    $event->setResponse($this->generateJsonError(500));                    
+                }
+            }
+        }
+    }
+
+    private function generateJsonError($errorCode)
+    {
+        return new JsonResponse([
+            'code' => 1,
+            'description' => 'ApiResponseSubscriber'
+        ], $errorCode);        
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::RESPONSE => 'onKernelResponse',
+        );
+    }
+}
