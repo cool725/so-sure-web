38ec1f53aeb1c8235b046153a7086f4526447cf6
Patrick McAndrew <patrick@so-sure.com>
2016-07-27 12:14:19 +0100

    improve tests

--- src/AppBundle/Tests/Document/InvitationTest.php
+++ src/AppBundle/Tests/Document/InvitationTest.php
@@ -97,4 +97,42 @@ class InvitationTest extends WebTestCase
         $api = $invitation->toApiArray();
         $this->assertFalse(isset($api['inviter_id']));
     }
+
+    public function testImageUrlEmailWithName()
+    {
+        $invitation = new EmailInvitation();
+        $invitation->setEmail('foo@bar.com');
+        $invitation->setName('Foo Bar');
+        $api = $invitation->toApiArray();
+        $this->assertEquals(
+            'https://www.gravatar.com/avatar/f3ada405ce890b6f8204094deb12d8a8?d=https%3A%2F%2Fcdn.so-sure.com%2Fimages%2Fprofile%2Ff.png&s=100',
+            $api['image_url']
+        );
+    }
+
+    public function testImageUrlEmailWithoutName()
+    {
+        $invitation = new EmailInvitation();
+        $invitation->setEmail('foo@bar.com');
+        $api = $invitation->toApiArray();
+        $this->assertEquals(
+            'https://www.gravatar.com/avatar/f3ada405ce890b6f8204094deb12d8a8?d=https%3A%2F%2Fcdn.so-sure.com%2Fimages%2Fprofile%2Funknown.png&s=100',
+            $api['image_url']
+        );
+    }
+
+    public function testImageUrlSmsWithoutName()
+    {
+        $invitation = new SmsInvitation();
+        $api = $invitation->toApiArray();
+        $this->assertEquals('https://cdn.so-sure.com/images/profile/unknown.png', $api['image_url']);
+    }
+
+    public function testImageUrlSmsWithName()
+    {
+        $invitation = new SmsInvitation();
+        $invitation->setName('Foo Bar');
+        $api = $invitation->toApiArray();
+        $this->assertEquals('https://cdn.so-sure.com/images/profile/f.png', $api['image_url']);
+    }
 }
