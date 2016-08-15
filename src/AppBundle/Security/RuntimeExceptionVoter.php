6cca4cb43c23958d91e6b5575cdf1077c16fce06
Patrick McAndrew <patrick@so-sure.com>
2016-08-15 16:03:59 +0100

    try voter instead for oauth 404

---
+++ src/AppBundle/Security/RuntimeExceptionVoter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Security;
+
+use Staffim\RollbarBundle\Voter\ReportVoterInterface;
+
+class RuntimeExceptionVoter implements ReportVoterInterface
+{
+    private function support($exception)
+    {
+        // Don't sent HWI OAuth No resource owner
+        // Verify: GET -UsEd https://wearesosure.com/login/LoginForm.jsp
+        if ($exception instanceof \RuntimeException &&
+            stripos($exception->getMessage(), "No resource owner") !== false) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function vote($exception)
+    {
+        return !$this->support($exception);
+    }
+}
