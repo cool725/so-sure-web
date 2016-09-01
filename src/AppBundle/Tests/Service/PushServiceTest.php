c32ed5380e7bd2f59a87c029710279a13f523ad4
Patrick McAndrew <patrick@so-sure.com>
2016-09-01 12:23:52 +0100

    adjust push notifications to use new fields as per https://github.com/so-sure/product-backlog/wiki/Push-Notification-Rules

---
+++ src/AppBundle/Tests/Service/PushServiceTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace AppBundle\Tests\Service;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use AppBundle\Service\PushService;
+
+/**
+ * @group functional-nonet
+ */
+class PushServiceTest extends WebTestCase
+{
+    use \AppBundle\Tests\PhingKernelClassTrait;
+    use \AppBundle\Tests\UserClassTrait;
+    protected static $container;
+    protected static $push;
+
+    public static function setUpBeforeClass()
+    {
+         //start the symfony kernel
+         $kernel = static::createKernel();
+         $kernel->boot();
+
+         //get the DI container
+         self::$container = $kernel->getContainer();
+
+         //now we can instantiate our service (if you want a fresh one for
+         //each test method, do this in setUp() instead
+         self::$push = self::$container->get('app.push');
+    }
+
+    public function tearDown()
+    {
+    }
+
+    public function testCustomDataMessageGeneral()
+    {
+        $data = self::$push->getCustomData(PushService::MESSAGE_GENERAL);
+        $this->assertEquals(['display' => 'popup', 'type' => 'alert'], $data);
+    }
+
+    public function testCustomDataMessageConnected()
+    {
+        $data = self::$push->getCustomData(PushService::MESSAGE_CONNECTED);
+        $this->assertEquals([
+            'uri' => 'sosure://open/pot',
+            'display' => 'popup',
+            'refresh' => true,
+            'type' => 'alert'
+        ], $data);
+    }
+
+    public function testCustomDataMessageInvitation()
+    {
+        $data = self::$push->getCustomData(PushService::MESSAGE_INVITATION);
+        $this->assertEquals([
+            'uri' => 'sosure://open/pot',
+            'display' => 'popup',
+            'refresh' => true,
+            'type' => 'alert'
+        ], $data);
+    }
+
+    public function testGcm()
+    {
+        $data = self::$push->generateGCMMessage(PushService::MESSAGE_GENERAL, 'foo');
+        $this->assertEquals(['data' => [
+            'message' => 'foo',
+            'display' => 'popup',
+            'type' => 'alert'
+        ]], $data);
+    }
+
+    public function testApns()
+    {
+        $data = self::$push->generateAPNSMessage(PushService::MESSAGE_GENERAL, 'foo');
+        $this->assertEquals([
+            'aps' => ['alert' => 'foo'],
+            'display' => 'popup',
+            'type' => 'alert'
+        ], $data);
+    }
+}
