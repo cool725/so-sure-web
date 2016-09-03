b9a096ec7b3a1d4c1c6405981290ce8fb449e83c
Patrick McAndrew <patrick@so-sure.com>
2016-09-03 14:16:59 +0100

    set sosure employee cookie for admin users

---
+++ src/AppBundle/Listener/KernelResponseListener.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace AppBundle\Listener;
+
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class KernelResponseListener
+{
+    const SOSURE_EMPLOYEE_COOKIE_NAME = 'sosure-employee';
+    const SOSURE_EMPLOYEE_COOKIE_LENGTH = 604800; // 7 days 
+    protected $tokenStorage;
+    protected $authChecker;
+    protected $adminCookieValue;
+    protected $logger;
+
+    public function __construct($tokenStorage, $authChecker, $adminCookieValue, $logger)
+    {
+        $this->tokenStorage = $tokenStorage;
+        $this->authChecker = $authChecker;
+        $this->adminCookieValue = $adminCookieValue;
+        $this->logger = $logger;
+    }
+
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        $response = $event->getResponse();
+        $request  = $event->getRequest();
+
+        try {
+            $token = $this->tokenStorage->getToken();
+            if (!$token || !$token->getUser() || !$token->getUser() instanceof UserInterface) {
+                return;
+            }
+
+            // authChecker doesn't seem to be working :(
+            // WARNING - this means that ROLE_ADMIN needs to be directly assigned instead of chainging
+            // TODO: Fixme
+            $adminUser = false;
+            foreach ($token->getUser()->getRoles() as $role) {
+                if ($role == "ROLE_ADMIN") {
+                    $adminUser = true;
+                }
+            }
+            if (!$adminUser) {
+                return;
+            }
+        } catch (\Exception $e) {
+            return;
+        }
+
+        // create/update cookie
+        $cookie = new Cookie(
+            self::SOSURE_EMPLOYEE_COOKIE_NAME,
+            $this->adminCookieValue,
+            time() + self::SOSURE_EMPLOYEE_COOKIE_LENGTH
+        );
+
+        // set cookie in response
+        $response->headers->setCookie($cookie);
+    }
+}
