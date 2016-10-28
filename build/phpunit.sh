35aa52b91b571eb2e5040cd182431cf135fdcfb3
Patrick McAndrew <patrick@so-sure.com>
2016-10-28 12:40:21 +0100

    avoid displaying depreciation notices when running an individual phpunit test add option to avoid refreshing the db for test cases

--- build/phpunit.sh
+++ build/phpunit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
-php vendor/phpunit/phpunit/phpunit $@
+php vendor/phpunit/phpunit/phpunit --configuration app/phpunit.xml.dist $@
