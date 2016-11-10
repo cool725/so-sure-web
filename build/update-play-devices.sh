a9d24ced73424d5eb22161909d6636fb99a411e9
Patrick McAndrew <patrick@so-sure.com>
2016-11-10 10:22:47 +0000

    forgot to add env option

--- build/update-play-devices.sh
+++ build/update-play-devices.sh
@@ -2,5 +2,24 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
+ENV=""
+while getopts "e:h" opt; do
+  case $opt in
+    e)
+      ENV=$OPTARG
+      ;;
+    h)
+      echo "Usage: $0 [-e prod|test|vagrant]"
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+if [ "$ENV" == "" ]; then
+    echo "See $0 -h"
+    exit 1
+fi
+
 # /c/ version of PlayDevice Fixtures will run upset on existing data, so safe to rerun
-sudo app/console --env=vagrant doctrine:mongodb:fixtures:load --append --fixtures src/AppBundle/DataFixtures/MongoDB/c/PlayDevice
+echo "sudo app/console --env=$ENV doctrine:mongodb:fixtures:load --append --fixtures src/AppBundle/DataFixtures/MongoDB/c/PlayDevice"
+sudo app/console --env=$ENV doctrine:mongodb:fixtures:load --append --fixtures src/AppBundle/DataFixtures/MongoDB/c/PlayDevice
