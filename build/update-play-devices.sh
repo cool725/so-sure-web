0477964e78bb8ec7abc021aeea419d9ec723364a
Patrick McAndrew <patrick@so-sure.com>
2016-11-10 10:15:20 +0000

    update comment

--- build/update-play-devices.sh
+++ build/update-play-devices.sh
@@ -2,5 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
-# TODO: clear play devices
+# /c/ version of PlayDevice Fixtures will run upset on existing data, so safe to rerun
 sudo app/console --env=vagrant doctrine:mongodb:fixtures:load --append --fixtures src/AppBundle/DataFixtures/MongoDB/c/PlayDevice
