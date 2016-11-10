f24041d8bdfaf043695c1ef05ae3ef83128808fb
Patrick McAndrew <patrick@so-sure.com>
2016-11-10 08:06:16 +0000

    move video to common twig & js includes add modal class to video to fix display issues

--- src/AppBundle/Resources/public/js/Default/index.js
+++ src/AppBundle/Resources/public/js/Default/index.js
@@ -16,13 +16,3 @@
         }, 1500);
     });
 
-    var iframe = document.querySelector('#exp-vid');
-    var player = new Vimeo.Player(iframe);
-
-    $('.modal-video').on('hidden.bs.modal', function() {
-        player.pause();
-    })
-
-    $('.modal-video').on('shown.bs.modal', function() {
-        player.play();
-    })
---
+++ src/AppBundle/Resources/public/js/Default/modalVideo.js
@@ -0,0 +1,10 @@
+    var iframe = document.querySelector('#exp-vid');
+    var player = new Vimeo.Player(iframe);
+
+    $('.modal-video').on('hidden.bs.modal', function() {
+        player.pause();
+    })
+
+    $('.modal-video').on('shown.bs.modal', function() {
+        player.play();
+    })
