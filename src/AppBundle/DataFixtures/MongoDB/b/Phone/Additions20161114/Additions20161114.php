85c632faab9952189a81224217886b284cd2715c
Patrick McAndrew <patrick@so-sure.com>
2016-11-14 12:51:46 +0000

    add leon

---
+++ src/AppBundle/DataFixtures/MongoDB/b/Phone/Additions20161114/Additions20161114.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\DataFixtures\MongoDB\b\Phone\Additions20160101;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Document\Phone;
+use AppBundle\DataFixtures\MongoDB\b\Phone\LoadPhoneData;
+
+class Additions20161114 extends LoadPhoneData implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $this->loadCsv($manager, '20161114.csv', new \DateTime('2016-10-01'));
+    }
+}
