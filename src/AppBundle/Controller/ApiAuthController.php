067dc5f6d0f8942fcdd277d9130606b209bbd4b9
Patrick McAndrew <patrick@so-sure.com>
2016-12-01 16:56:58 +0000

    method already adds multipay object

--- src/AppBundle/Controller/ApiAuthController.php
+++ src/AppBundle/Controller/ApiAuthController.php
@@ -915,7 +915,7 @@ class ApiAuthController extends BaseController
                         $multiPay->getPayee()->getName(),
                         $multiPayPolicy->getPremium()->getMonthlyPremiumPrice(),
                         $multiPayPolicy->getPremium()->getYearlyPremiumPrice()
-                    ), null, ['multipay' => ['id' => $multiPay->getId()]]);
+                    ), null, ['id' => $multiPay->getId()]);
                     // @codingStandardsIgnoreEnd
                 } catch (\Exception $e) {
                     $this->get('logger')->error(sprintf("Error in multipay push."), ['exception' => $e]);
