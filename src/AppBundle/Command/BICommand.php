6ca836ae830a87600e6f250b126ea3f89e75b35b
Patrick McAndrew <patrick@so-sure.com>
2016-12-17 07:48:18 +0000

    add claims to bi process

--- src/AppBundle/Command/BICommand.php
+++ src/AppBundle/Command/BICommand.php
@@ -9,6 +9,8 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Helper\Table;
 use AppBundle\Document\User;
+use AppBundle\Document\Claim;
+use AppBundle\Classes\SoSure;
 
 class BICommand extends ContainerAwareCommand
 {
@@ -21,6 +23,34 @@ class BICommand extends ContainerAwareCommand
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->exportUsers();
+        $this->exportClaims();
+    }
+
+    private function exportClaims()
+    {
+        $repo = $this->getManager()->getRepository(Claim::class);
+        $claims = $repo->findAll();
+        $lines = [];
+        $lines[] = implode(',', [
+            '"Policy Number"',
+            '"Policy Start Date"',
+            '"FNOL"',
+            '"Postcode"',
+        ]);
+        foreach ($claims as $claim) {
+            $lines[] = implode(',', [
+               sprintf('"%s"', $claim->getPolicy()->getPolicyNumber()),
+               sprintf('"%s"', $claim->getPolicy()->getStart()->format('Y-m-d H:i:s')),
+               sprintf('"%s"', $claim->getNotificationDate()->format('Y-m-d H:i:s')),
+               sprintf('"%s"', $claim->getPolicy()->getUser()->getBillingAddress()->getPostcode()),
+            ]);
+        }
+        $this->uploadS3(implode(PHP_EOL, $lines), 'claims.csv');
+    }
+
+    private function exportUsers()
     {
         $repo = $this->getManager()->getRepository(User::class);
         $users = $repo->findAll();
