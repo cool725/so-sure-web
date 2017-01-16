5bb3828ddab9bc5ede94e75ac1350bdd0fe47633
Patrick McAndrew <patrick@so-sure.com>
2017-01-16 10:48:38 +0000

    Record last login ip for user

---
+++ src/AppBundle/Listener/SecurityListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Listener;
+
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+use AppBundle\Document\IdentityLog;
+
+class SecurityListener
+{
+    protected $logger;
+
+    /** @var RequestStack */
+    protected $requestStack;
+
+    public function __construct($logger, RequestStack $requestStack)
+    {
+        $this->logger = $logger;
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * @param InteractiveLoginEvent $event
+     */
+    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
+    {
+        if (!$event->getAuthenticationToken()) {
+            return;
+        }
+
+        $identityLog = new IdentityLog();
+        $identityLog->setIp($this->requestStack->getCurrentRequest()->getClientIp());
+        $user = $event->getAuthenticationToken()->getUser();
+        $user->setLatestWebIdentityLog($identityLog);
+
+    }
+}
