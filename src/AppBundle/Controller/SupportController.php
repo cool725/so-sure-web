1d48d06a01625adf4d0ba0498a8ffb8222c47d1d
Patrick McAndrew <patrick@so-sure.com>
2017-02-05 08:07:29 +0000

    add support route to serve from s3

---
+++ src/AppBundle/Controller/SupportController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+/**
+ * @Route("/support")
+ */
+class SupportController extends BaseController
+{
+    /**
+     * @Route("/{name}", name="support", requirements={"name"=".+"})
+     */
+    public function allAction(Request $request, $name)
+    {
+        if ($name == "") {
+            $name = "index.html";
+        }
+        $file = sprintf('%s', $name);
+
+        $filesystem = $this->get('oneup_flysystem.mount_manager')->getFilesystem('s3support_fs');
+        $mimetype = $filesystem->getMimetype($file);
+        return StreamedResponse::create(
+            function () use ($file, $filesystem) {
+                $stream = $filesystem->readStream($file);
+                echo stream_get_contents($stream);
+                flush();
+            },
+            200,
+            array('Content-Type' => $mimetype)
+        );
+    }
+}
