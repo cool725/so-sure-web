c62c7605c9764ffb85b555ba6a87697df1064ab4
Patrick McAndrew <patrick@so-sure.com>
2017-02-06 10:03:39 +0000

    added pending cancellation to risk model moved bar for pending cancellation to claims warning display claims warnings for admins as well

--- src/AppBundle/Document/Policy.php
+++ src/AppBundle/Document/Policy.php
@@ -34,6 +34,7 @@ abstract class Policy
     const RISK_CONNECTED_NO_CLAIM = 'connected w/no claim';
     const RISK_NOT_CONNECTED_NEW_POLICY = 'not connected w/new policy';
     const RISK_NOT_CONNECTED_ESTABLISHED_POLICY = 'not connected w/established policy (>30 days)';
+    const RISK_PENDING_CANCELLATION_POLICY = 'policy is pending cancellation';
 
     const STATUS_PENDING = 'pending';
     const STATUS_ACTIVE = 'active';
@@ -74,6 +75,7 @@ abstract class Policy
         self::RISK_CONNECTED_NO_CLAIM => self::RISK_LEVEL_LOW,
         self::RISK_NOT_CONNECTED_NEW_POLICY => self::RISK_LEVEL_HIGH,
         self::RISK_NOT_CONNECTED_ESTABLISHED_POLICY => self::RISK_LEVEL_MEDIUM,
+        self::RISK_PENDING_CANCELLATION_POLICY => self::RISK_LEVEL_HIGH,
     ];
 
     /**
@@ -1270,6 +1272,10 @@ abstract class Policy
             $date = new \DateTime();
         }
 
+        if ($this->getPendingCancellation()) {
+            return self::RISK_PENDING_CANCELLATION_POLICY;
+        }
+
         if (count($this->getConnections()) > 0) {
             // Connected and value of their pot is zero
             if ($this->hasMonetaryClaimed(true)) {
@@ -1974,6 +1980,13 @@ abstract class Policy
 
         if (!$this->isAdditionalClaimLostTheftApprovedAllowed()) {
             $warnings[] = sprintf('Policy already has 2 lost/theft claims. No further lost/theft claims are allowed');
+        }
+
+        if ($this->getPendingCancellation()) {
+            $warnings[] = sprintf(
+                'Policy is scheduled to be cancelled by user request on %s.',
+                $this->getPendingCancellation()->format('d M Y H:i')
+            );
         }
 
         return $warnings;
