3bd7c9437c504ef90e3c35af1357fa0c5d96ffd2
Ted Eriksson <ted.eriksson@me.com>
2017-02-06 16:00:16 +0000

    add cookie consent

---
+++ src/AppBundle/Resources/public/js/cookies.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#dddddd",
+      "text": "#000000"
+    },
+    "button": {
+      "background": "#3399ff",
+      "text": "#ffffff"
+    }
+  },
+  "theme": "classic",
+  "position": "top",
+  "static": true,
+  "content": {
+    "href": "https://www.iubenda.com/privacy-policy/7805295"
+  }
+})});
