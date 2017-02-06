8af9906b05bb709fcc02e1f6ceb82086059e8e8b
cool725 <109250241+cool725@users.noreply.github.com>
2017-02-06 17:56:45 +0000

    New reward calculator and slider

--- src/AppBundle/Resources/public/js/Default/slider.js
+++ src/AppBundle/Resources/public/js/Default/slider.js
@@ -1,56 +1,115 @@
 $(function(){
 
-    var max_value = $('#slider-value').data('slider-max');
-    var maxpot_value = $('#slider-value').data('slider-maxpot');
-    var initial_value = $('#slider-value').data('slider-value');
-    var connection_value = $('#slider-value').data('slider-connection-value');
-    var adjusted_potential_value = maxpot_value - (max_value * connection_value);
-    var data = [
+    const $element = $('input[type="range"]');
+
+    const claimStates = [
+
         {
-            value: initial_value,
-            color:"#3399FF",
-            highlight: "#3399FF",
-            // If changing text, verify tooltipTemplate isn't affected
-            label: "Value of Your Reward Pot"
+            name: 'no',
+            text: 'Cashback when no one claims',
         },
         {
-            value: max_value - initial_value,
-            color:"#202532",
-            highlight: "#6F6F6F",
-            // If changing text, verify tooltipTemplate isn't affected
-            label: "Remaining Reward Pot Value"
+            name: 'me',
+            text: 'Cashback when I claim',
         },
-    ]
+        {
+            name: 'one',
+            text: 'Cashback when 1 friend claims',
+        },
+        {
+            name: 'two',
+            text: 'Cashback when 2 friends claim',
+        }
+
+    ];
+
+    var currentState;
+    var $handle;
+
+    $element
+
+    .rangeslider({
+
+        polyfill: false,
+
+        rangeClass: 'rewardslider',
+        disabledClass: 'rewardslider--disabled',
+        horizontalClass: 'rewardslider--horizontal',
+        verticalClass: 'rewardslider--vertical',
+        fillClass: 'rewardslider__fill',
+        handleClass: 'rewardslider__handle',
+
+
+        onInit: function() {
+            $handle = $('.rewardslider__handle', this.$range);
+            updateHandle($handle[0], this.value);           
+        },
+
+        onSlide: function(position, value) {
+
+            var current_value    = $('#reward-slider').val();
+            var max_value        = $('#reward-slider').prop('max');
+            var maxpot_value     = $('#reward-slider').data('slider-maxpot');
+            var initial_value    = $('#reward-slider').prop('value');
+            var connection_value = $('#reward-slider').data('slider-connection-value');
+
+            // console.log(current_value, max_value, maxpot_value, initial_value, connection_value);
+        
+
+            var setCashback = function() {
+                var save_value = current_value * connection_value;
+
+                if (save_value > maxpot_value) {
+                    save_value = maxpot_value;
+                }
+
+                $('#cashback').text('£' + save_value);
+            }
+
+            var updateValue = function() {
+                setCashback();
+            }
+
+            setCashback();
+
+        },
+
+
+        onSlideEnd: function(position, value) {}
+
+    })
+
+    .on('input', function() {
+        updateHandle($handle[0], this.value);   
+    });
+
+    function updateHandle(el, val) {
+        el.textContent = val;
+    }    
 
     function roundToTwo(num) {
         return +(Math.round(num + "e+2")  + "e-2");
     }
 
+    // var setConnectionText = function() {
+    //     var save_value = current_value * connection_value;
 
-    var setConnectionText = function() {
-        var save_value = slider.getValue() * connection_value;
-        if (save_value > maxpot_value) {
-            save_value = maxpot_value;
-        }
-        var potential_value = roundToTwo(maxpot_value - save_value);
-        $('#num_friends').text(slider.getValue() + (slider.getValue() == 1 ? ' friend' : ' friends'));
-        $('#money_back').text('£' + save_value); 
-    }
+    //     // console.log(save_value);
 
-    var updateValue = function() {
-        setConnectionText();
-        $('#buy_form_slider_used').val(true);
-        $('#buy_form_banner_slider_used').val(true);
-        $('#compare_buy_form_slider_used').val(true);
-    }
+    //     if (save_value > maxpot_value) {
+    //         save_value = maxpot_value;
+    //     }
 
-    var slider = $('#slider-value').slider({
-        formatter: function(value) {
-            return value + (value == 1 ? ' connection' : ' connections');
-        }})
-        .on('change', updateValue)
-        .data('slider');
+    //     var potential_value = roundToTwo(maxpot_value - save_value);
 
-    //setConnectionText();
+    //     $('#money_back').text('£' + save_value); 
+    // }    
+
+    // var updateValue = function() {
+    //     setConnectionText();;
+    // }    
+
+    // setConnectionText();   
+
 
 });
