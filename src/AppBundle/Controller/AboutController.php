b056f2f83f3018d687adf2a31b9dceff6df32c72
Julien Champagne <julienchampagne@Juliens-MacBook-Pro-2.local>
2017-02-08 15:43:35 +0000

    fix php formatting

--- src/AppBundle/Controller/AboutController.php
+++ src/AppBundle/Controller/AboutController.php
@@ -20,7 +20,7 @@ class AboutController extends BaseController
      */
     public function indexAction(Request $request)
     {
-	}
+    }
 
     /**
      * @Route("/are-we-regulated-within-the-uk", name="about_are_we_regulated_within_the_uk")
@@ -28,7 +28,7 @@ class AboutController extends BaseController
      */
     public function areWeRegulatedWithinTheUkAction(Request $request)
     {
-	}
+    }
 
     /**
      * @Route("/how-secure-is-so-sure", name="about_how_secure_is_so_sure")
@@ -36,7 +36,7 @@ class AboutController extends BaseController
      */
     public function howSecureIsSoSureAction(Request $request)
     {
-	}
+    }
 
     /**
      * @Route("/what-do-you-do-with-my-personal-information", name="about_what_do_you_do_with_my_personal_information")
@@ -44,7 +44,7 @@ class AboutController extends BaseController
      */
     public function whatDoYouDoWithMyPersonalInformationAction(Request $request)
     {
-	}
+    }
 
     /**
      * @Route("/what-personal-information-do-you-capture", name="about_what_personal_information_do_you_capture")
@@ -52,7 +52,7 @@ class AboutController extends BaseController
      */
     public function whatPersonalInformationDoYouCaptureAction(Request $request)
     {
-	}
+    }
 
     /**
      * @Route("/the-team", name="about_the_team")
@@ -60,7 +60,7 @@ class AboutController extends BaseController
      */
     public function theTeamAction(Request $request)
     {
-	}
+    }
 
     /**
      * @Route("/how-to-contact-so-sure", name="about_how_to_contact_so_sure")
@@ -68,7 +68,7 @@ class AboutController extends BaseController
      */
     public function howToContactSoSureAction(Request $request)
     {
-	}
+    }
 
     /**
      * @Route("/our-mission", name="about_our_mission")
@@ -76,7 +76,7 @@ class AboutController extends BaseController
      */
     public function ourMissionAction(Request $request)
     {
-	}
+    }
 
     /**
      * @Route("/why-we-re-better", name="about_why_we_re_better")
@@ -84,7 +84,7 @@ class AboutController extends BaseController
      */
     public function whyWeReBetterAction(Request $request)
     {
-	}
+    }
 
     /**
      * @Route("/what-is-social-insurance", name="about_what_is_social_insurance")
@@ -92,6 +92,5 @@ class AboutController extends BaseController
      */
     public function whatIsSocialInsuranceAction(Request $request)
     {
-	}
-
+    }
 }
