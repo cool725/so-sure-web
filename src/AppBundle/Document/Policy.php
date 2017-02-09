65b48431ec5ca87273411001b9f769f79ba9ca27
Patrick McAndrew <patrick@so-sure.com>
2017-02-09 09:46:41 +0000

    hide policy related claimschecks for claims handlers (causes too much confusion :( make warning section red + increase font size add receipro to all claimscheck text

--- src/AppBundle/Document/Policy.php
+++ src/AppBundle/Document/Policy.php
@@ -1975,7 +1975,7 @@ abstract class Policy
         }
 
         if ($this->hasOpenClaim(true)) {
-            $warnings[] = sprintf('Policy has an open claim, but ClaimsCheck has not been run.');
+            $warnings[] = sprintf('Policy has an open claim, but Recipero (ClaimsCheck) has not been run.');
         }
 
         if (!$this->isAdditionalClaimLostTheftApprovedAllowed()) {
