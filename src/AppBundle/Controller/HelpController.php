1477d95d38f9a8479a5d39136008da8e246b24d2
Julien Champagne <julienchampagne@Juliens-MacBook-Pro-2.local>
2017-02-14 13:22:11 +0000

    update help section

--- src/AppBundle/Controller/HelpController.php
+++ src/AppBundle/Controller/HelpController.php
@@ -13,25 +13,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\DomCrawler\Crawler;
 
-class SupportController extends BaseController
+class HelpController extends BaseController
 {
     /**
-     * @Route("/support/{file}", name="support", requirements={"file"=".*"})
-     * @Route("/how-it-works/{file}", name="howitworks", requirements={"file"=".*"})
-     * @Route("/looking-after-your-phone/{file}", name="advise", requirements={"file"=".*"})
+     * @Route("/help/{file}", name="help", requirements={"file"=".*"})
      * @Template()
      */
     public function allAction(Request $request, $file = null)
     {
         $filesystem = $this->get('oneup_flysystem.mount_manager')->getFilesystem('s3support_fs');
-        $routeName = $request->get('_route');
-        if ($routeName == 'support') {
-            $file = sprintf('/support/%s', $file);
-        } elseif ($routeName == 'howitworks') {
-            $file = sprintf('/how-it-works/%s', $file);
-        } elseif ($routeName == 'advise') {
-            $file = sprintf('/looking-after-your-phone/%s', $file);            
-        }
 
         // assume html extension if not given
         if (pathinfo($file, PATHINFO_EXTENSION) == "") {
