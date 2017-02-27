85868ae3f677db51057dc895241ca160c206794b
cool725 <109250241+cool725@users.noreply.github.com>
2017-02-27 13:45:40 +0000

    Added page removed route

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -399,17 +399,6 @@ class DefaultController extends BaseController
     }
 
     /**
-     * @Route("/is-insurance-with-vodafone-all-that-it-seems", name="landing_voda")
-     * @Route("/is-iphone-insurance-with-vodafone-all-that-it-seems", name="landing_voda_iphone")
-     * @Route("/is-samsung-insurance-with-vodafone-all-that-it-seems", name="landing_voda_samsung")
-     * @Template("AppBundle::Default/isInsuranceWithVodafoneAllThatItSeems.html.twig")
-     */
-    // public function isInsuranceWithVodafoneAllThatItSeems()
-    // {
-    //     return array();
-    // }
-
-    /**
      * @Route("/phone/{make}/{model}", name="phone_make_model")
      * @Template
      */
