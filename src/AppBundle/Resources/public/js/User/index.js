131d9fe12361f51f5a245048dc04d6b6f7f905ec
cool725 <109250241+cool725@users.noreply.github.com>
2017-03-07 17:58:38 +0000

    Hooked up chart for reward pot

--- src/AppBundle/Resources/public/js/User/index.js
+++ src/AppBundle/Resources/public/js/User/index.js
@@ -1,58 +1,33 @@
 $(function(){
 
-    // Chart.defaults.global.defaultFontFamily = '"Avenir LT Std 45 Book","Helvetica Neue",Helvetica,Arial,sans-serif';
-    // Chart.defaults.global.tooltips.enabled = false;
-    // var ctx = document.getElementById("myChart");
-    // var maxConnections = $('.doughnut-outer').data('max-connections');
-    // var connections = $('.doughnut-outer').data('connections');
-    // var invites = $('.doughnut-outer').data('invites');
-
-    // // a complete 0 value for data total will result in display not show
-    // var data = [1, 1];
-    // if (connections + invites > 0 && maxConnections != 0) {
-    //     data = [
-    //         Math.min(connections, maxConnections) / maxConnections,
-    //         Math.min(connections + invites, maxConnections) / maxConnections
-    //     ];
-    // }
-    // var myChart = new Chart(ctx, {
-    //     type: 'doughnut',
-    //     data: {
-    //         labels: ["Connections [" + connections + "]", "Invites [" + invites + "]"],
-    //         datasets: [{
-    //             data: data,
-    //             backgroundColor: [
-    //                 'rgba(51, 153, 255, 0.8)',
-    //                 'rgba(51, 204, 204, 0.8)'
-    //             ]
-    //         }]
-    //     },
-    //     options: {
-    //       cutoutPercentage: 85
-    //     }
-    // });
-
+    // Reward pot chart
     var rewardPot = $('#reward-pot-chart');
     var potValue  = $(rewardPot).data('pot-value');
     var maxPot    = $(rewardPot).data('max-pot');
-    var percent   = Math.round((potValue / maxPot) * 100);
-    var potDisp   = Math.round(potValue);
 
-    // console.log(percent);
+    var totalInit = Math.round((potValue / maxPot) * 100);
+    var total = totalInit / 100;
 
-    // $(rewardPot).circliful({
-    //     percent: percent,
-    //     backgroundColor: '#efefef',
-    //     foregroundColor: '#3399ff',
-    //     backgroundBorderWidth: 10,
-    //     foregroundBorderWidth: 10,
+    $(rewardPot).circleProgress({
+        value: total,
+        size: 180,
+        // startAngle: 1
+        lineCap: 'round',
+        emptyFill: '#efefef',
+        fill: '#3399ff'
+    });
 
-    //     fontColor: '#3399ff',
-    //     replacePercentageByText: '£'+potDisp,
-    //     textAdditionalCss: 'font-weight: bold; font-family: "Avenir LT Std 85 Heavy";',
-    //     // text: 'of a maximum of £'+maxPot
+    // Connection bonus chart
+    // $(rewardPot).circleProgress({
+    //     value: total,
+    //     size: 180,
+    //     // startAngle: 1
+    //     lineCap: 'round',
+    //     emptyFill: '#efefef',
+    //     fill: '#3399ff'
     // });
 
+    // Copy to clipboard
     $('.btn-clipboard').tooltip({'title':'Copied', 'trigger': 'manual'});
     var clipboard = new Clipboard('.btn-clipboard');
 
@@ -60,6 +35,8 @@ $(function(){
         $('.btn-clipboard').tooltip('show');
         setTimeout(function() { $('.btn-clipboard').tooltip('hide'); }, 1500);
     });
+
+    // Share buttons
     $("#share").jsSocials({
         shares: ["twitter", "facebook"],
         url: $('.btn-clipboard').data('clipboard-text'),
@@ -68,6 +45,7 @@ $(function(){
         showCount: false,
     });
 
+    // Scode form
     $('#scode-form-submit').click(function() {
         var url = '/user/scode/' + $('#scode-form-code').val();
         var token = $(this).data('token');
