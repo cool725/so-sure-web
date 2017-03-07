080709c0dbe9ab4027de723c24a2ca4fce60a97d
cool725 <109250241+cool725@users.noreply.github.com>
2017-03-07 17:01:24 +0000

    Basic header and sections for user area

--- src/AppBundle/Resources/public/js/User/index.js
+++ src/AppBundle/Resources/public/js/User/index.js
@@ -1,34 +1,48 @@
 $(function(){
-    Chart.defaults.global.defaultFontFamily = '"Avenir LT Std 45 Book","Helvetica Neue",Helvetica,Arial,sans-serif';
-    Chart.defaults.global.tooltips.enabled = false;
-    var ctx = document.getElementById("myChart");
-    var maxConnections = $('.doughnut-outer').data('max-connections');
-    var connections = $('.doughnut-outer').data('connections');
-    var invites = $('.doughnut-outer').data('invites');
 
-    // a complete 0 value for data total will result in display not show
-    var data = [1, 1];
-    if (connections + invites > 0 && maxConnections != 0) {
-        data = [
-            Math.min(connections, maxConnections) / maxConnections,
-            Math.min(connections + invites, maxConnections) / maxConnections
-        ];
-    }
-    var myChart = new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-            labels: ["Connections [" + connections + "]", "Invites [" + invites + "]"],
-            datasets: [{
-                data: data,
-                backgroundColor: [
-                    'rgba(51, 153, 255, 0.8)',
-                    'rgba(51, 204, 204, 0.8)'
-                ]
-            }]
-        },
-        options: {
-          cutoutPercentage: 85
-        }
+    // Chart.defaults.global.defaultFontFamily = '"Avenir LT Std 45 Book","Helvetica Neue",Helvetica,Arial,sans-serif';
+    // Chart.defaults.global.tooltips.enabled = false;
+    // var ctx = document.getElementById("myChart");
+    // var maxConnections = $('.doughnut-outer').data('max-connections');
+    // var connections = $('.doughnut-outer').data('connections');
+    // var invites = $('.doughnut-outer').data('invites');
+
+    // // a complete 0 value for data total will result in display not show
+    // var data = [1, 1];
+    // if (connections + invites > 0 && maxConnections != 0) {
+    //     data = [
+    //         Math.min(connections, maxConnections) / maxConnections,
+    //         Math.min(connections + invites, maxConnections) / maxConnections
+    //     ];
+    // }
+    // var myChart = new Chart(ctx, {
+    //     type: 'doughnut',
+    //     data: {
+    //         labels: ["Connections [" + connections + "]", "Invites [" + invites + "]"],
+    //         datasets: [{
+    //             data: data,
+    //             backgroundColor: [
+    //                 'rgba(51, 153, 255, 0.8)',
+    //                 'rgba(51, 204, 204, 0.8)'
+    //             ]
+    //         }]
+    //     },
+    //     options: {
+    //       cutoutPercentage: 85
+    //     }
+    // });
+
+    var rewardPot = $('#reward-pot-chart');
+    var maxConnections = $(rewardPot).data('max-connections');
+    var connections = $(rewardPot).data('connections'); 
+    var invites =  $(rewardPot).data('invites');
+
+    console.log(maxConnections, connections, invites);
+
+    
+
+    $(rewardPot).circliful({
+        // percent: 
     });
 
     $('.btn-clipboard').tooltip({'title':'Copied', 'trigger': 'manual'});
