93ff0b7d960cada7a35f4f4f7a93a2b5183d3585
cool725 <109250241+cool725@users.noreply.github.com>
2017-03-20 16:12:33 +0000

    Adjust get a quote btn to not open modal but scroll to hero

--- src/AppBundle/Resources/public/js/Default/index.js
+++ src/AppBundle/Resources/public/js/Default/index.js
@@ -1,15 +1,25 @@
 $(function(){
-    if(window.location.href.indexOf('?quote=1') != -1) {
-        $('#quoteModel').modal('show');
-        sosuretrack('Get A Quote Link', function() {
-        });
-    }
+
+    // if(window.location.href.indexOf('?quote=1') != -1) {
+    //     $('#quoteModel').modal('show');
+    //     sosuretrack('Get A Quote Link', function() {
+    //     });
+    // }
 
     $.fn.extend({
         toggleText: function(a, b){
             return this.text(this.text() == b ? a : b);
         }
-    });    
+    });  
+
+    $("#get-quote-btn").click(function(event) {
+
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $("#get-quote").offset().top
+        }, 1500);
+    });      
 
     $('#phone_phone').change(function() {
         $.get('/price/' + this.value + '/', function(data) {
