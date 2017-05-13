c502271721523aa53ae568230929c9f714feeefb
Patrick McAndrew <patrick@so-sure.com>
2017-05-13 11:31:24 +0100

    if email already exists but not connected w/facebook before, throw exception to avoid duplicate account creation ensure formatting for name if email doesn't exist (e.g. phone number username), do not process

--- src/AppBundle/Security/FOSUBUserProvider.php
+++ src/AppBundle/Security/FOSUBUserProvider.php
@@ -6,9 +6,19 @@ use HWI\Bundle\OAuthBundle\Security\Core\User\FOSUBUserProvider as BaseClass;
 use Symfony\Component\Security\Core\User\UserInterface;
 use AppBundle\Service\FacebookService;
 use HWI\Bundle\OAuthBundle\Security\Core\Exception\AccountNotLinkedException;
+use AppBundle\Validator\Constraints\AlphanumericValidator;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class FOSUBUserProvider extends BaseClass
 {
+    /** @var RequestStack */
+    protected $requestStack;
+
+    public function setRequestStack(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -51,6 +61,23 @@ class FOSUBUserProvider extends BaseClass
         $user = $this->userManager->findUserBy(array($search => $username));
         //when the user is registrating
         if (null === $user) {
+            // Not guarenteed an email address
+            if (!$response->getEmail()) {
+                return null;
+            }
+            if ($this->userManager->findUserBy(['emailCanonical' => strtolower($response->getEmail())])) {
+                $msg = 'You appear to already have an account, but its not connected. Please login and connect.';
+
+                if ($request = $this->requestStack->getCurrentRequest()) {
+                    if ($session = $request->getSession()) {
+                        if ($session->isStarted()) {
+                            $session->getFlashBag()->add('error', $msg);
+                        }
+                    }
+                }
+
+                throw new AccountNotLinkedException($msg);
+            }
             $service = $response->getResourceOwner()->getName();
             $setter = 'set'.ucfirst($service);
             $setter_id = $setter.'Id';
@@ -61,8 +88,8 @@ class FOSUBUserProvider extends BaseClass
             $user->$setter_token($this->getLongLivedAccessToken($response));
 
             $user->setEmail($response->getEmail());
-            $user->setFirstName(explode(' ', $response->getFirstName())[0]);
-            $user->setLastName(explode(' ', $response->getLastName())[0]);
+            $user->setFirstName($this->conformAlphanumeric(explode(' ', $response->getFirstName())[0], 50));
+            $user->setLastName($this->conformAlphanumeric(explode(' ', $response->getLastName())[0], 50));
             $user->setEnabled(true);
 
             $this->userManager->updateUser($user);
@@ -87,6 +114,13 @@ class FOSUBUserProvider extends BaseClass
     public function setFacebook(FacebookService $facebook)
     {
         $this->facebook = $facebook;
+    }
+
+    protected function conformAlphanumeric($value, $length)
+    {
+        $validator = new AlphanumericValidator();
+
+        return $validator->conform(substr($value, 0, $length));
     }
 
     private function getLongLivedAccessToken(UserResponseInterface $response)
