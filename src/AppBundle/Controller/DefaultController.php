ab2fe8b7beb830d85f6f3a1a939f27f5e63dbcce
cool725 <109250241+cool725@users.noreply.github.com>
2017-05-23 11:04:13 +0100

    Line length error

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -281,9 +281,11 @@ class DefaultController extends BaseController
     }
 
     /**
-     * @Route("/mobile-phone-insurance-for-your-company", name="mobile_phone_insurance_for_your_company", options={"sitemap"="priority":"1.0"})
+     * @Route("/mobile-phone-insurance-for-your-company",
+     name="mobile_phone_insurance_for_your_company",
+     options={"sitemap"={"priority":"1.0","changefreq":"daily"}})
      * @Route("/mobile-phone-insurance-for-your-company/thank-you",
-     *          name="mobile_phone_insurance_for_your_company_thanks")
+     * name="mobile_phone_insurance_for_your_company_thanks")
      * @Template
      */
     public function mobileInsuranceForYourCompany(Request $request)
