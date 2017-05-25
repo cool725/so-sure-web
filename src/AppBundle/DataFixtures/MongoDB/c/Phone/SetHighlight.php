94ce5f61426b3b559212ba5095a6eefdd2bb237b
Patrick McAndrew <patrick@so-sure.com>
2017-05-25 06:41:48 +0100

    group phones for memory add highligh flag on phones for display; ability to update via admin; add samsung to fixtures

---
+++ src/AppBundle/DataFixtures/MongoDB/c/Phone/SetHighlight.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\DataFixtures\MongoDB\b\PlayDevice;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Document\Phone;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+// @codingStandardsIgnoreFile
+class SetHighlight implements FixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $repo = $manager->getRepository(Phone::class);
+        $data = [
+            'Samsung' => [
+                'Galaxy S8', 'Galaxy S8+', 'Galaxy S7', 'Galaxy S7 edge', 'Galaxy S6', 'Galaxy S6 Edge'
+            ]
+        ];
+        foreach ($data as $make => $models) {
+            foreach ($data[$make] as $model) {
+                $phones = $repo->findBy(['make' => $make, 'model' => $model]);
+                foreach ($phones as $phone) {
+                    $phone->setHighlight(true);
+                }
+            }
+        }
+        $manager->flush();
+    }
+}
