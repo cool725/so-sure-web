00aa738695d598a21d4ea1035e164681df153e83
cool725 <109250241+cool725@users.noreply.github.com>
2017-06-05 14:39:20 +0100

    Testing show part of next phone

--- src/AppBundle/Resources/public/js/Default/makeInsurance.js
+++ src/AppBundle/Resources/public/js/Default/makeInsurance.js
@@ -1,7 +1,18 @@
 // Make Insurance JS
 $(function(){
 
-
+    $('.carousel .item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+        if (next.next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+        } else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
 
 
 });
