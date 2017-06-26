b0bba005cd28aa10d3032b56c7183c996db94813
cool725 <109250241+cool725@users.noreply.github.com>
2017-06-26 17:21:48 +0100

    Added phones to set highlight

--- src/AppBundle/DataFixtures/MongoDB/c/Phone/SetHighlight.php
+++ src/AppBundle/DataFixtures/MongoDB/c/Phone/SetHighlight.php
@@ -25,9 +25,12 @@ class SetHighlight implements FixtureInterface, ContainerAwareInterface
     {
         $repo = $manager->getRepository(Phone::class);
         $data = [
+            'Apple' => [
+                'iPhone 7', 'iPhone 7 Plus', 'iPhone 6S Plus', 'iPhone 6S', 'iPhone SE', 'iPhone 6 Plus', 'iPhone 6'
+            ],
             'Samsung' => [
                 'Galaxy S8', 'Galaxy S8+', 'Galaxy S7', 'Galaxy S7 Edge', 'Galaxy S6', 'Galaxy S6 Edge'
-            ]
+            ],
         ];
         foreach ($data as $make => $models) {
             foreach ($data[$make] as $model) {
