55dd59aface09371003848fcee3a84c6bd574fe2
Patrick McAndrew <patrick@so-sure.com>
2017-07-07 10:22:05 +0100

    add web types for payments to determine how better to handle the result on callback from judopay simplify the callback processing for web payments and remove the requirement for user to be logged in

--- src/AppBundle/Service/JudopayService.php
+++ src/AppBundle/Service/JudopayService.php
@@ -30,6 +30,15 @@ class JudopayService
 {
     use CurrencyTrait;
 
+    /** Standard payment (monthly/yearly; initial payment or payment after card fails */
+    const WEB_TYPE_STANDARD = 'standard';
+
+    /** No payment, just updating the card details */
+    const WEB_TYPE_CARD_DETAILS = 'card-details';
+
+    /** Remainder of policy payment (typically cancelled policy w/claim) */
+    const WEB_TYPE_REMAINDER = 'remainder';
+
     /** @var LoggerInterface */
     protected $logger;
 
@@ -123,6 +132,32 @@ class JudopayService
         $this->webClient = new Judopay($webData);
         $this->statsd = $statsd;
         $this->environment = $environment;
+    }
+
+    public function getTransaction($receiptId)
+    {
+        $transaction = $this->apiClient->getModel('Transaction');
+        $data = array(
+            'judoId' => $this->judoId,
+        );
+        $transaction->setAttributeValues($data);
+        $details = $transactions->find($receiptId);
+
+        return $details;
+    }
+
+    public function getTransactionWebType($receiptId)
+    {
+        try {
+            $data = $this->getTransaction($receiptId);
+            if (isset($receipt['yourPaymentMetaData']) && isset($receipt['yourPaymentMetaData']['web_type'])) {
+                return $receipt['yourPaymentMetaData']['web_type'];
+            }
+        } catch (\Exception $e) {
+            $this->logger->warning(sprintf('Unable to get transaction receipt %s', $receiptId));
+        }
+    
+        return null;
     }
 
     public function getTransactions($pageSize, $logMissing = true)
@@ -928,7 +963,7 @@ class JudopayService
     /**
      *
      */
-    public function webpay(Policy $policy, $amount, $ipAddress, $userAgent)
+    public function webpay(Policy $policy, $amount, $ipAddress, $userAgent, $type = null)
     {
         $payment = new JudoPayment();
         $payment->setAmount($amount);
@@ -948,6 +983,7 @@ class JudopayService
                 'yourPaymentReference' => $payment->getId(),
                 'yourPaymentMetaData' => [
                     'policy_id' => $policy->getId(),
+                    'web_type' => $type ? $type : null,
                 ],
                 'amount' => $this->toTwoDp($amount),
                 'currency' => 'GBP',
