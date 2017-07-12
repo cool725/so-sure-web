57aeed8b5b6f2f5293a5487d892167e112de660d
Patrick McAndrew <patrick@so-sure.com>
2017-07-12 18:31:36 +0100

    remove unused controller actions + setup redirects

--- src/AppBundle/Controller/AboutController.php
+++ src/AppBundle/Controller/AboutController.php
@@ -41,58 +41,10 @@ class AboutController extends BaseController
     }
 
     /**
-     * @Route("/are-we-regulated-within-the-uk", name="about_are_we_regulated_within_the_uk")
-     * @Template
-     */
-    public function areWeRegulatedWithinTheUkAction(Request $request)
-    {
-    }
-
-    /**
-     * @Route("/how-secure-is-so-sure", name="about_how_secure_is_so_sure")
-     * @Template
-     */
-    public function howSecureIsSoSureAction(Request $request)
-    {
-    }
-
-    /**
-     * @Route("/the-team", name="about_the_team")
-     * @Template
-     */
-    public function theTeamAction(Request $request)
-    {
-    }
-
-    /**
      * @Route("/how-to-contact-so-sure", name="about_how_to_contact_so_sure")
      * @Template
      */
     public function howToContactSoSureAction(Request $request)
-    {
-    }
-
-    /**
-     * @Route("/our-mission", name="about_our_mission")
-     * @Template
-     */
-    public function ourMissionAction(Request $request)
-    {
-    }
-
-    /**
-     * @Route("/why-we-re-better", name="about_why_we_re_better")
-     * @Template
-     */
-    public function whyWeReBetterAction(Request $request)
-    {
-    }
-
-    /**
-     * @Route("/what-is-social-insurance", name="about_what_is_social_insurance")
-     * @Template
-     */
-    public function whatIsSocialInsuranceAction(Request $request)
     {
     }
 }
