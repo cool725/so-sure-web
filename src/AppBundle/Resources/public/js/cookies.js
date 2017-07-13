bc3df502390f4df03bf25343d3ac82399e6dc01f
cool725 <109250241+cool725@users.noreply.github.com>
2017-07-13 16:00:52 +0100

    Copy changed for button

--- src/AppBundle/Resources/public/js/cookies.js
+++ src/AppBundle/Resources/public/js/cookies.js
@@ -16,7 +16,7 @@ window.addEventListener("load", function(){
         },
         content: {
             message: "<small>This website uses cookies to ensure you get the best experience on our website.</small>",
-            dismiss: "Got it!",
+            dismiss: "Close",
             link: "<small>Learn More</small>",
             href: "https://www.iubenda.com/privacy-policy/7805295/cookie-policy"
         }
