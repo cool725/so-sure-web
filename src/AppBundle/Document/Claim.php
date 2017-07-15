b0b61239562e6cc1ca71d4256ed5543f50136518
Patrick McAndrew <patrick@so-sure.com>
2017-07-15 07:46:45 +0100

    show what claim it is when throwing exception

--- src/AppBundle/Document/Claim.php
+++ src/AppBundle/Document/Claim.php
@@ -408,7 +408,11 @@ class Claim
 
         if (in_array($this->getType(), [self::TYPE_WARRANTY]) &&
             in_array($status, [self::STATUS_APPROVED, self::STATUS_DECLINED])) {
-            throw new \InvalidArgumentException(sprintf('Unable to use approved/declined with Warranty Types'));
+            throw new \InvalidArgumentException(sprintf(
+                'Unable to use approved/declined with Warranty Types. %s/%s',
+                $this->getNumber(),
+                $this->getId()
+            ));
         }
 
         $this->status = $status;
