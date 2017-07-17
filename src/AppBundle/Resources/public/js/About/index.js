605cd1a7e2287eb2e0680491c2bb77eb63680342
cool725 <109250241+cool725@users.noreply.github.com>
2017-07-17 17:17:07 +0100

    Adjusted validation to allow landlines

--- src/AppBundle/Resources/public/js/About/index.js
+++ src/AppBundle/Resources/public/js/About/index.js
@@ -3,7 +3,7 @@ $(function(){
     // UK mobile number
     jQuery.validator.addMethod('phoneUK', function(phone_number, element) {
     return this.optional(element) || phone_number.length > 9 &&
-    phone_number.match(/^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$/);
+    phone_number.match(/^((\(?0\d{4}\)?\s?\d{3}\s?\d{3})|(\(?0\d{3}\)?\s?\d{3}\s?\d{4})|(\(?0\d{2}\)?\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/);
     }, 'Please enter a valid phone number');
 
 	$('#team-member-info-modal').on('show.bs.modal', function(event) {
