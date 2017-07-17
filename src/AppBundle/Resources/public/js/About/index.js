e524f6d89a1fa80f9b7ec17bd56ae4478d4af1d5
cool725 <109250241+cool725@users.noreply.github.com>
2017-07-17 15:25:37 +0100

    Added jquery validation to contact page

--- src/AppBundle/Resources/public/js/About/index.js
+++ src/AppBundle/Resources/public/js/About/index.js
@@ -23,4 +23,43 @@ $(function(){
         Intercom('trackEvent', 'clicked intercom contact us');
         Intercom('showNewMessage', $(this).data('intercom-msg'));
     });
+
+    $('#contact-us-form').validate({
+        debug: false,
+        onkeyup: false,
+        focusCleanup: true,
+        validClass: 'has-success',
+        rules: {
+            "contact_form[name]" : {
+                required: true
+            },
+            "contact_form[email]" : {
+                required: true,
+                email: true
+            },
+            "contact_form[phone]" : {
+                required: true,
+                digits: true
+            },
+            "contact_form[message]" : {
+                required: true
+            }
+        },
+        messages: {
+            "contact_form[name]" : {
+                required: 'Please enter your name'
+            },
+            "contact_form[email]" : {
+                required: 'Please enter a valid email address',
+                email:  'Please enter a valid email address'
+            },
+            "contact_form[phone]" : {
+                required: 'Please enter a valid phone number',
+                digits: 'Phone must contain digits only please'
+            },
+            "contact_form[message]" : {
+                required: "Please tell us what you'd like to talk about"
+            }
+        }
+    });
 });
