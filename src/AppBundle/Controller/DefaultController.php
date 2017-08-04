8d88286e0d897c93820865d62a5960d8de380fe6
cool725 <109250241+cool725@users.noreply.github.com>
2017-08-04 14:45:09 +0100

    Added three competitor page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -549,6 +549,15 @@ class DefaultController extends BaseController
     }
 
     /**
+     * @Route("/so-sure-vs-three-phone-insurance", name="so-sure-vs-three_phone_insurance")
+     * @Template
+     */
+    public function soSureVsThree()
+    {
+        return array();
+    }
+
+    /**
      * @Route("/so-sure-vs-protect-your-bubble", name="so_sure_vs_protect_your_bubble")
      * @Route("/so-sure-vs-protect-your-bubble-phone-insurance", name="so_sure_vs_protect_your_bubble_phone_insurance")
      * @Route("/so-sure-vs-carphone-warehouse-phone-insurance", name="so_sure_vs_carphone_warehouse_phone_insurance")
