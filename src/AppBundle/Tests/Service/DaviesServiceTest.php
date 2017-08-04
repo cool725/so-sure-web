a649911bb0ed8438a8c976b1f62e66f7c8f43c2d
Patrick McAndrew <patrick@so-sure.com>
2017-08-04 15:05:47 +0100

    fix test

--- src/AppBundle/Tests/Service/DaviesServiceTest.php
+++ src/AppBundle/Tests/Service/DaviesServiceTest.php
@@ -331,10 +331,10 @@ class DaviesServiceTest extends WebTestCase
         $policy->setUser($user);
         $claim = new Claim();
         $policy->addClaim($claim);
-        $policy->setPolicyNumber(1);
+        $policy->setPolicyNumber('TEST/2017/123456');
 
         $daviesClaim = new DaviesClaim();
-        $daviesClaim->policyNumber = 1;
+        $daviesClaim->policyNumber = 'TEST/2017/123456';
         $daviesClaim->reserved = 1;
         $daviesClaim->insuredName = 'Mr foo bar';
         $daviesClaim->riskPostCode = 'se152sz';
