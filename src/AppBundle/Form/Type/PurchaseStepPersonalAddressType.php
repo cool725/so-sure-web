94f56563a2258583aeb111461a8e538f061432ad
cool725 <109250241+cool725@users.noreply.github.com>
2017-08-25 10:07:53 +0100

    No need to include in form type

--- src/AppBundle/Form/Type/PurchaseStepPersonalAddressType.php
+++ src/AppBundle/Form/Type/PurchaseStepPersonalAddressType.php
@@ -12,7 +12,6 @@ use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use AppBundle\Document\Phone;
 use AppBundle\Validator\Constraints\AgeValidator;
