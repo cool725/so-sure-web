9e941c4a2d8c5c09cdd45e394f65c84409b2cae5
Patrick McAndrew <patrick@so-sure.com>
2017-08-30 09:41:42 +0100

    tweak copy

--- src/AppBundle/Document/User.php
+++ src/AppBundle/Document/User.php
@@ -1401,7 +1401,9 @@ class User extends BaseUser implements TwoFactorInterface, TrustedComputerInterf
             ),
             'can_purchase_policy' => $this->canPurchasePolicy(),
             'has_payment_method' => $this->hasValidPaymentMethod(),
-            'card_details' => $this->getPaymentMethod() ? $this->getPaymentMethod()->__toString() : 'Unknown card details',
+            'card_details' => $this->getPaymentMethod() ?
+                $this->getPaymentMethod()->__toString() :
+                'Please update your card',
         ];
     }
 }
