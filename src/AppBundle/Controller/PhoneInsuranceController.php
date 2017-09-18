616198025f357b63abd7641f175786c35cac77e9
cool725 <109250241+cool725@users.noreply.github.com>
2017-09-18 15:08:55 +0100

    Added slider

--- src/AppBundle/Controller/PhoneInsuranceController.php
+++ src/AppBundle/Controller/PhoneInsuranceController.php
@@ -172,7 +172,6 @@ class PhoneInsuranceController extends BaseController
      *          requirements={"make":"[a-zA-Z]+","model":"[\+\-\.a-zA-Z0-9() ]+","memory":"[0-9]+"})
      * @Route("/insure/{make}+{model}", name="insure_make_model",
      *          requirements={"make":"[a-zA-Z]+","model":"[\+\-\.a-zA-Z0-9() ]+"})
-     * @Route("/phone-insurance/learn-more", name="phone_insurance_learn_more")
      */
     public function quoteAction(Request $request, $id = null, $make = null, $model = null, $memory = null)
     {
@@ -484,5 +483,140 @@ class PhoneInsuranceController extends BaseController
         } else {
             return $this->render($template, $data);
         }
+    }
+
+    /**
+     * Note that any changes to actual path routes need to be reflected in the Google Analytics Goals
+     *   as these will impact Adwords
+     *
+     * @Route("/phone-insurance/{id}/learn-more", name="learn_more_phone", requirements={"id":"[0-9a-f]{24,24}"})
+     * @Route("/phone-insurance/{make}+{model}+{memory}GB/learn-more", name="learn_more_make_model_memory",
+     *          requirements={"make":"[a-zA-Z]+","model":"[\+\-\.a-zA-Z0-9() ]+","memory":"[0-9]+"})
+     * @Route("/phone-insurance/{make}+{model}/learn-more", name="learn_more_make_model",
+     *          requirements={"make":"[a-zA-Z]+","model":"[\+\-\.a-zA-Z0-9() ]+"})
+     */
+    public function learnMoreAction(Request $request, $id = null, $make = null, $model = null, $memory = null)
+    {
+        $dm = $this->getManager();
+        $repo = $dm->getRepository(Phone::class);
+        $phonePolicyRepo = $dm->getRepository(PhonePolicy::class);
+        $phone = null;
+        $decodedModel = Phone::decodeModel($model);
+        if ($id) {
+            $phone = $repo->find($id);
+            if ($phone->getMemory()) {
+                return $this->redirectToRoute('learn_more_make_model_memory', [
+                    'make' => $phone->getMake(),
+                    'model' => $phone->getEncodedModel(),
+                    'memory' => $phone->getMemory(),
+                ], 301);
+            } else {
+                return $this->redirectToRoute('learn_more_make_model', [
+                    'make' => $phone->getMake(),
+                    'model' => $phone->getEncodedModel(),
+                ], 301);
+            }
+        } elseif ($memory) {
+            $phone = $repo->findOneBy([
+                'active' => true,
+                'make' => $make,
+                'model' => $decodedModel,
+                'memory' => (int) $memory
+            ]);
+        } else {
+            $phones = $repo->findBy(
+                ['active' => true, 'make' => $make, 'model' => $decodedModel],
+                ['memory' => 'asc'],
+                1
+            );
+            if (count($phones) != 0 && stripos($model, ' ') === false) {
+                $phone = $phones[0];
+            }
+        }
+        if (!$phone) {
+            $this->get('logger')->info(sprintf(
+                'Failed to find phone for id: %s make: %s model: %s mem: %s',
+                $id,
+                $make,
+                $model,
+                $memory
+            ));
+
+            return new RedirectResponse($this->generateUrl('homepage'));
+        }
+
+        $user = new User();
+
+        $form = $this->get('form.factory')
+            ->createNamedBuilder('launch', LaunchType::class, $user)
+            ->getForm();
+
+        $buyForm = $this->get('form.factory')
+            ->createNamedBuilder('buy_form')
+            ->add('buy_tablet', SubmitType::class)
+            ->add('slider_used', HiddenType::class)
+            ->add('claim_used', HiddenType::class)
+            ->getForm();
+        $buyBannerForm = $this->get('form.factory')
+            ->createNamedBuilder('buy_form_banner')
+            ->add('buy', SubmitType::class)
+            ->add('slider_used', HiddenType::class)
+            ->add('claim_used', HiddenType::class)
+            ->getForm();
+
+        // if no price, will be sample policy of £100 annually
+        $maxPot = $phone->getCurrentPhonePrice() ? $phone->getCurrentPhonePrice()->getMaxPot() : 80;
+        $maxConnections = $phone->getCurrentPhonePrice() ? $phone->getCurrentPhonePrice()->getMaxConnections() : 8;
+        $annualPremium = $phone->getCurrentPhonePrice() ? $phone->getCurrentPhonePrice()->getYearlyPremiumPrice() : 100;
+        $maxComparision = $phone->getMaxComparision() ? $phone->getMaxComparision() : 80;
+
+        // // only need to run this once - if its a post, then ignore
+        // if ('GET' === $request->getMethod() && $phone->getCurrentPhonePrice()) {
+        //     $event = MixpanelService::EVENT_QUOTE_PAGE;
+        //     if (in_array($request->get('_route'), ['insure_make_model_memory', 'insure_make_model'])) {
+        //         $event = MixpanelService::EVENT_CPC_QUOTE_PAGE;
+        //     }
+        //     $this->get('app.mixpanel')->queueTrackWithUtm($event, [
+        //         'Device Selected' => $phone->__toString(),
+        //         'Monthly Cost' => $phone->getCurrentPhonePrice()->getMonthlyPremiumPrice(),
+        //     ]);
+        //     $this->get('app.mixpanel')->queuePersonProperties([
+        //         'First Device Selected' => $phone->__toString(),
+        //         'First Monthly Cost' => $phone->getCurrentPhonePrice()->getMonthlyPremiumPrice(),
+        //     ], true);
+
+        //     // Deprecated - Landing Page event - Keep for awhile for a transition period
+        //     // TODO: Remove
+        //     if ($event == MixpanelService::EVENT_CPC_QUOTE_PAGE) {
+        //         $this->get('app.mixpanel')->queueTrackWithUtm(MixpanelService::EVENT_LANDING_PAGE, [
+        //             'Device Selected' => $phone->__toString(),
+        //             'Monthly Cost' => $phone->getCurrentPhonePrice()->getMonthlyPremiumPrice(),
+        //         ]);
+        //     }
+        // }
+
+        $data = array(
+            'phone' => $phone,
+            'phone_price' => $phone->getCurrentPhonePrice(),
+            'policy_key' => $this->getParameter('policy_key'),
+            'connection_value' => PhonePolicy::STANDARD_VALUE,
+            'annual_premium' => $annualPremium,
+            'max_connections' => $maxConnections,
+            'max_pot' => $maxPot,
+            'form' => $form->createView(),
+            'buy_form' => $buyForm->createView(),
+            'buy_form_banner' => $buyBannerForm->createView(),
+            'phones' => $repo->findBy(
+                ['active' => true, 'make' => $make, 'model' => $decodedModel],
+                ['memory' => 'asc']
+            ),
+            'comparision' => $phone->getComparisions(),
+            'comparision_max' => $maxComparision,
+            'coming_soon' => $phone->getCurrentPhonePrice() ? false : true,
+            //'slider_test' => $sliderTest,
+            'slider_test' => 'slide-me',
+        );
+
+        return $this->render('AppBundle:PhoneInsurance:learnMore.html.twig', $data);
     }
 }
