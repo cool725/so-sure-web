d30fc6a3056921f51293453fb07c71a291185c80
Patrick McAndrew <patrick@so-sure.com>
2017-09-19 10:03:56 +0100

    add iphone 8

--- src/AppBundle/DataFixtures/MongoDB/b/Phone/Additions20170919/Additions20170919.php
+++ src/AppBundle/DataFixtures/MongoDB/b/Phone/Additions20170919/Additions20170919.php
@@ -1,16 +1,16 @@
 <?php
 
-namespace AppBundle\DataFixtures\MongoDB\b\Phone\Additions20170909;
+namespace AppBundle\DataFixtures\MongoDB\b\Phone\Additions20170919;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use AppBundle\Document\Phone;
 use AppBundle\DataFixtures\MongoDB\b\Phone\LoadPhoneData;
 
-class Additions20170909 extends LoadPhoneData implements FixtureInterface
+class Additions20170919 extends LoadPhoneData implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $this->loadCsv($manager, '20170909.csv', new \DateTime('2017-06-01'));
+        $this->loadCsv($manager, '20170919.csv', new \DateTime('2017-06-01'));
     }
 }
