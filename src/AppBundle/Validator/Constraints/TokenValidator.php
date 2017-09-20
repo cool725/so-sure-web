900563bd1a438fc355b23ecab59953519a45ab35
Patrick McAndrew <patrick@so-sure.com>
2017-09-20 15:58:14 +0100

    conform to token validation when importing data

--- src/AppBundle/Validator/Constraints/TokenValidator.php
+++ src/AppBundle/Validator/Constraints/TokenValidator.php
@@ -9,13 +9,34 @@ class TokenValidator extends ConstraintValidator
 {
     public function validate($value, Constraint $constraint)
     {
-        // main concerns are around [] (php array) and $ (mongodb expression)
-        $expr = preg_quote('-.,;:+():_£&@*!^#"%');
-        $regex = sprintf('/^[ %s\/a-zA-Z0-9]*$/', $expr);
-        if (!preg_match($regex, $value, $matches)) {
+        if (is_object($value)) {
+            throw new \Exception(sprintf('Expected string %s', json_encode($value)));
+        }
+
+        if (!preg_match($this->getRegex(), $value, $matches)) {
             $this->context->buildViolation($constraint->message)
                 ->setParameter('%string%', $value)
                 ->addViolation();
         }
+    }
+
+    public function conform($value)
+    {
+        preg_match_all($this->getRegex(false), $value, $matches);
+        return implode('', $matches[0]);
+    }
+
+    private function getRegex($exact = true)
+    {
+        // main concerns are around [] (php array) and $ (mongodb expression)
+        $expr = preg_quote('-.,;:+():_£&@*!^#"%');
+        $regex = sprintf('[ %s\/a-zA-Z0-9]*', $expr);
+        if ($exact) {
+            $regex = sprintf('/^%s$/u', $regex);
+        } else {
+            $regex = sprintf('/%s/u', $regex);
+        }
+
+        return $regex;
     }
 }
