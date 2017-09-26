2ed27a60b0b5325b3fcb98fd261497263386e92f
Patrick McAndrew <patrick@so-sure.com>
2017-09-26 16:07:50 +0100

    fix phpcs

--- src/AppBundle/Tests/Document/AttributionTest.php
+++ src/AppBundle/Tests/Document/AttributionTest.php
@@ -20,10 +20,12 @@ class AttributionTest extends \PHPUnit_Framework_TestCase
     public function testRefererConform()
     {
         $attribution = new Attribution();
+        // @codingStandardsIgnoreStart
         $attribution->setReferer('https://mg.mail.yahoo.com/neo/rd?s=cA.lBygDeL4.DeewGiHtP59uy1wiLzu4ELDVPfJEBqtFtxfXgrLuUZ_M6DxWewN9pPKCI2bwwA4Pe4zign1u9jpvyxNd2LcgHrwj6RLf1UQUf23_zQb.y2h6NlkqNBdb7ydW4GRby.C.dDiXBmlbPNOjOZRB4NPsh_x.oVGKA9uyN0Yb0Fc2MzTrkDjUxrvhUTWu~A&ncrumb=1JSX.2y6ghv');
         $this->assertEquals(
             'https://mg.mail.yahoo.com/neo/rd?s=cA.lBygDeL4.DeewGiHtP59uy1wiLzu4ELDVPfJEBqtFtxfXgrLuUZ_M6DxWewN9pPKCI2bwwA4Pe4zign1u9jpvyxNd2LcgHrwj6RLf1UQUf23_zQb.y2h6NlkqNBdb7ydW4GRby.C.dDiXBmlbPNOjOZRB4NPsh_x.oVGKA9uyN0Yb0Fc2MzTrkDjUxrvhUTWuA&ncrumb=1JSX.2y6ghv',
             $attribution->getReferer()
         );
+        // @codingStandardsIgnoreEnd
     }
 }
