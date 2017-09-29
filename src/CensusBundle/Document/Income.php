8a15c57e5ce685a1e330b3e4cfa045a0dba02a20
Patrick McAndrew <patrick@so-sure.com>
2017-09-29 08:16:13 +0100

    add income by postcode

---
+++ src/CensusBundle/Document/Income.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace CensusBundle\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @MongoDB\Document(collection="Income2014")
+ */
+class Income
+{
+    /**
+     * @MongoDB\Id
+     */
+    protected $id;
+
+    /**
+     * @MongoDB\Field(type="string", name="MSOA")
+     * @MongoDB\Index(unique=true, sparse=true)
+     */
+    protected $msoa;
+
+    /**
+     * @MongoDB\Field(type="string", name="LOCAL_AUTHORITY_CODE")
+     */
+    protected $localAuthorityCode;
+
+    /**
+     * @MongoDB\EmbedOne(targetDocument="IncomeType")
+     */
+    protected $total;
+
+    /**
+     * @MongoDB\EmbedOne(targetDocument="IncomeType")
+     */
+    protected $net;
+
+    /**
+     * @MongoDB\EmbedOne(targetDocument="IncomeType")
+     */
+    protected $netBeforeHousing;
+
+    /**
+     * @MongoDB\EmbedOne(targetDocument="IncomeType")
+     */
+    protected $netAfterHousing;
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function getMSOA()
+    {
+        return $this->msoa;
+    }
+
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    public function getNet()
+    {
+        return $this->net;
+    }
+
+    public function getNetBeforeHousing()
+    {
+        return $this->netBeforeHousing;
+    }
+
+    public function getNetAfterHousing()
+    {
+        return $this->netAfterHousing;
+    }
+}
