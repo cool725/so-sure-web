d8e9bfc8f1d471bb1316ee4f3219f119f643a69a
cool725 <109250241+cool725@users.noreply.github.com>
2017-10-06 11:58:42 +0100

    Added sticky search option

--- src/AppBundle/Resources/public/js/Default/homepage.js
+++ src/AppBundle/Resources/public/js/Default/homepage.js
@@ -1,7 +1,23 @@
 // homepage.js
 $(function(){
 
-    // Defer images for faster page speed
+    // Sticky Search
+    // function stickySearch() {
+    //     var windowTop  = $(window).scrollTop();
+    //     var searchBox  = $('#select-phone-data-1');
+    //     var offsetTop  = $('.homepage--hero').height();
+    //     var hitPoint   = $('.homepage--instant-quote').offset().top;
+
+    //     if (windowTop > offsetTop && windowTop < hitPoint) {
+    //         $(searchBox).addClass('search-phone-form--sticky');
+    //     } else {
+    //         $(searchBox).removeClass('search-phone-form--sticky');
+    //     }
+    // }
+
+    // if ($('#search-phone-form-homepage-1').length) {
+    //     $(window).scroll(stickySearch);
+    // }
 
     // Enhance the focus on the search box when input focused
     $('.search-phone').each(function(index, el) {
