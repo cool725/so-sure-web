2b80b9cd1d7645698cac49afaab1c54ef6a5eda2
Patrick McAndrew <patrick@so-sure.com>
2017-10-14 13:18:17 +0100

    re-enable swiftmailer logging raise to error on failure + reque on disk

--- src/AppBundle/Listener/MailerListener.php
+++ src/AppBundle/Listener/MailerListener.php
@@ -10,10 +10,12 @@ class MailerListener implements Swift_Events_SendListener
 {
     /** @var LoggerInterface */
     protected $logger;
+    protected $spoolPath;
     
-    public function __construct(LoggerInterface $logger = null)
+    public function __construct(LoggerInterface $logger = null, $spoolPath = null)
     {
         $this->logger = $logger;
+        $this->spoolPath = $spoolPath;
     }
 
     public function beforeSendPerformed(Swift_Events_SendEvent $evt)
@@ -24,24 +26,50 @@ class MailerListener implements Swift_Events_SendListener
     public function sendPerformed(Swift_Events_SendEvent $evt)
     {
         $result = 'Unknown';
+        $level = 'info';
+        $respool = false;
         if ($evt->getResult() == Swift_Events_SendEvent::RESULT_PENDING) {
             $result = 'Pending';
         } elseif ($evt->getResult() == Swift_Events_SendEvent::RESULT_SPOOLED) {
             $result = 'Spooled';
         } elseif ($evt->getResult() == Swift_Events_SendEvent::RESULT_SUCCESS) {
             $result = 'Sent';
+            $respool = true;
         } elseif ($evt->getResult() == Swift_Events_SendEvent::RESULT_TENTATIVE) {
             $result = 'Tentative';
         } elseif ($evt->getResult() == Swift_Events_SendEvent::RESULT_FAILED) {
             $result = 'Failed';
+            $level = 'error';
+            $respool = true;
+
         }
+
+        if ($respool && $this->spoolPath) {
+            $spool = new \Swift_FileSpool($this->spoolPath);
+            $message = $evt->getMessage();
+            /*
+            $message->getHeaders()->get('To')->setNameAddresses($evt->getFailedRecipients());
+            $message->getHeaders()->get('Cc')->setNameAddresses(null);
+            $message->getHeaders()->get('Bcc')->setNameAddresses(['bcc@so-sure.com']);
+            */
+            $spool->queueMessage($message);
+        }
+
+        $msg = sprintf(
+            'Email %s %s [%s]',
+            $result,
+            json_encode($evt->getMessage()->getTo()),
+            $evt->getMessage()->getSubject()
+        );
+
         if ($this->logger) {
-            $this->logger->info(sprintf(
+            $msg = sprintf(
                 'Email %s %s [%s]',
                 $result,
                 json_encode($evt->getMessage()->getTo()),
                 $evt->getMessage()->getSubject()
-            ));
+            );
+            call_user_func([$this->logger, $level], $msg);
         }
     }
 }
