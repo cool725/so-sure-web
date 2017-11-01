00b8781369bc7310c0f89ba8ee971f0e9d01a49a
cool725 <109250241+cool725@users.noreply.github.com>
2017-11-01 15:40:12 +0000

    Highlighting tweaks and header copy change

--- src/AppBundle/Resources/public/js/Default/homepage.js
+++ src/AppBundle/Resources/public/js/Default/homepage.js
@@ -20,14 +20,14 @@ $(function(){
     // }
 
     // Enhance the focus on the search box when input focused
-    $('.search-phone').each(function(index, el) {
-        var container = $(this).parents('.search-phone-form');
-        $(this).on('focus', function(){
-            $(container).addClass('search-phone-form--focus');
-        });
-        $(this).on('blur', function(){
-            $(container).removeClass('search-phone-form--focus');
-        });
-    });
+    // $('.search-phone').each(function(index, el) {
+    //     var container = $(this).parents('.search-phone-form');
+    //     $(this).on('focus', function(){
+    //         $(container).addClass('search-phone-form--focus');
+    //     });
+    //     $(this).on('blur', function(){
+    //         $(container).removeClass('search-phone-form--focus');
+    //     });
+    // });
 
 });
