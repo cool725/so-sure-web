cfc920c7bf90d3726dcb1dd88d9481274e9ec4c6
cool725 <109250241+cool725@users.noreply.github.com>
2017-11-02 12:03:41 +0000

    JS tidy up

--- src/AppBundle/Resources/public/js/Default/homepage.js
+++ src/AppBundle/Resources/public/js/Default/homepage.js
@@ -25,13 +25,17 @@ $(function(){
     $('.search-phone').each(function(index, el) {
         var container = $(this).parents('.search-phone-form');
         $(this).on('focus', function(){
+            // Add focus style
             $(container).addClass('search-phone-form--focus');
 
+            // If mobile slide up to make more room
             if (device == 'Mobile') {
                 console.log('Mobile');
                 $('html,body').animate({scrollTop: $(this).offset().top - 100}, 800);
             }
         });
+
+        // Remove class on blur
         $(this).on('blur', function(){
             $(container).removeClass('search-phone-form--focus');
         });
