f89813df9700ec49b2667f2e8e2f6c058753ba81
cool725 <109250241+cool725@users.noreply.github.com>
2017-11-02 12:04:17 +0000

    Remove code commented out

--- src/AppBundle/Resources/public/js/Default/homepage.js
+++ src/AppBundle/Resources/public/js/Default/homepage.js
@@ -1,24 +1,7 @@
 // homepage.js
 $(function(){
 
-    // Sticky Search
-    // function stickySearch() {
-    //     var windowTop  = $(window).scrollTop();
-    //     var searchBox  = $('#select-phone-data-1');
-    //     var offsetTop  = $('.homepage--hero').height();
-    //     var hitPoint   = $('.homepage--instant-quote').offset().top;
-
-    //     if (windowTop > offsetTop && windowTop < hitPoint) {
-    //         $(searchBox).addClass('search-phone-form--sticky');
-    //     } else {
-    //         $(searchBox).removeClass('search-phone-form--sticky');
-    //     }
-    // }
-
-    // if ($('#search-phone-form-homepage-1').length) {
-    //     $(window).scroll(stickySearch);
-    // }
-
+    // Get the current device
     var device = $('#ss-root').data('device-category');
 
     // Enhance the focus on the search box when input focused
@@ -30,7 +13,6 @@ $(function(){
 
             // If mobile slide up to make more room
             if (device == 'Mobile') {
-                console.log('Mobile');
                 $('html,body').animate({scrollTop: $(this).offset().top - 100}, 800);
             }
         });
