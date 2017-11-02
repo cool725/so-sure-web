3df2331b99502e4a096c2f843ab43b89e4cea06d
cool725 <109250241+cool725@users.noreply.github.com>
2017-11-02 10:10:47 +0000

    Test scroll on mobile

--- src/AppBundle/Resources/public/js/Default/homepage.js
+++ src/AppBundle/Resources/public/js/Default/homepage.js
@@ -20,14 +20,17 @@ $(function(){
     // }
 
     // Enhance the focus on the search box when input focused
-    // $('.search-phone').each(function(index, el) {
-    //     var container = $(this).parents('.search-phone-form');
-    //     $(this).on('focus', function(){
-    //         $(container).addClass('search-phone-form--focus');
-    //     });
-    //     $(this).on('blur', function(){
-    //         $(container).removeClass('search-phone-form--focus');
-    //     });
-    // });
+    $('.search-phone').each(function(index, el) {
+        var container = $(this).parents('.search-phone-form');
+        $(this).on('focus', function(){
+            $(container).addClass('search-phone-form--focus');
+            if (/Mobi/.test(navigator.userAgent)) {
+            $('html,body').animate({scrollTop: $(this).offset().top - 100}, 800);
+            }
+        });
+        $(this).on('blur', function(){
+            $(container).removeClass('search-phone-form--focus');
+        });
+    });
 
 });
