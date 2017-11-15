fef61dca86fecb13e8f2a1838148112f7d153f1b
cool725 <109250241+cool725@users.noreply.github.com>
2017-11-15 12:48:48 +0000

    Removed old css js assets

--- src/AppBundle/Resources/public/js/Purchase/purchaseStep.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $('.form-control').on('change', function() {
-        $(this).parent().removeClass('has-error');
-        $(this).parent().find('.with-errors').empty();
-    });
-});
