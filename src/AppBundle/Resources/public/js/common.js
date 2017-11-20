9b23cbdefb49a734f7c0700c012145b0d449cbaa
cool725 <109250241+cool725@users.noreply.github.com>
2017-11-20 10:40:19 +0000

    Added back image swap src

--- src/AppBundle/Resources/public/js/common.js
+++ src/AppBundle/Resources/public/js/common.js
@@ -67,6 +67,15 @@ $(function(){
         $(window).scroll(stickyNav);
     }
 
+    // IMAGE SRC SWAP
+    $('.image-swap').each(function() {
+        $(this).on('mouseover', function() {
+            $(this).attr('src', $(this).data('hover-src'));
+        }).on('mouseout', function() {
+            $(this).attr('src', $(this).data('orig-src'));
+        });
+    });
+
     // Collapse Panels - FAQs
     $('.panel-heading').click(function(event) {
 
