55f0dfc1544bb50e1cf249b1af9eb9e4adfe3c7f
Patrick McAndrew <patrick@so-sure.com>
2017-12-05 15:10:17 +0000

    excape " rather than transform to '

--- src/AppBundle/Service/SalvaExportService.php
+++ src/AppBundle/Service/SalvaExportService.php
@@ -1119,6 +1119,6 @@ class SalvaExportService
 
     private function escapeQuotes($string)
     {
-        return str_replace('"', "'", $string);
+        return str_replace('"', '\"', $string);
     }
 }
