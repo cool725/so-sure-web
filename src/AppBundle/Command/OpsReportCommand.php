abf6d180bd50d6ed416285f097c2c1f7a0e8fc57
Sinisa <sinisa@so-sure.com>
2018-01-03 15:14:20 +0000

    yRebased to latest master

--- src/AppBundle/Command/OpsReportCommand.php
+++ src/AppBundle/Command/OpsReportCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Helper\Table;
 use AppBundle\Classes\Premium;
+use Symfony\Component\HttpFoundation\Request;
 
 class OpsReportCommand extends ContainerAwareCommand
 {
@@ -26,10 +27,10 @@ class OpsReportCommand extends ContainerAwareCommand
         $mailer = $this->getContainer()->get('app.mailer');
 
         $this->sendCsp($redis, $mailer);
-        $this->sendClientValidation($redis, $mailer);
-        $output->writeln('Sent');
+        $response = $this->sendClientValidation($redis, $mailer);
+        $output->writeln(sprintf('Sent %s', $response));
     }
-    
+
     private function sendCsp($redis, $mailer)
     {
         $items = [];
@@ -44,6 +45,31 @@ class OpsReportCommand extends ContainerAwareCommand
         $mailer->send('CSP Report', 'tech@so-sure.com', $html);
     }
 
+    private function parseErrors($errors)
+    {
+        $items = [];
+        $emptyValues = 0;
+
+        foreach ($errors as $error) {
+            if (isset($error['value']) && $error['value'] == '') {
+                $emptyValues++;
+            }
+            if (isset($error['name']) && isset($error['message'])) {
+                $items[] = sprintf(
+                    '%s => "%s" [Msg: %s]',
+                    $error['name'],
+                    isset($error['value']) ? $error['value'] : '',
+                    $error['message']
+                );
+            } else {
+                $items[] = sprintf('Unknown format: %s', $item);
+            }
+        }
+
+        // exception: if all values are empty, do not include in the email
+        return (count($items) == $emptyValues) ? false : $items;
+    }
+
     private function sendClientValidation($redis, $mailer)
     {
         $items = [];
@@ -56,36 +82,35 @@ class OpsReportCommand extends ContainerAwareCommand
                     if ($time) {
                         $time = new \DateTime(sprintf('@%s', $time));
                     }
-                    $items[] = sprintf(
-                        '%s : %s [Browser: %s]',
-                        $time ? $time->format(\DateTime::ATOM) : '?',
-                        $data['url'],
-                        isset($data['browser']) ? $data['browser'] : ''
-                    );
-                    foreach ($data['errors'] as $error) {
-                        if (isset($error['name']) && isset($error['message'])) {
-                            $items[] = sprintf(
-                                '%s => "%s" [Msg: %s]',
-                                $error['name'],
-                                isset($error['value']) ? $error['value'] : '',
-                                $error['message']
-                            );
-                        } else {
-                            $items[] = sprintf('Unknown format: %s', $item);
-                        }
+                    // parsing errors first to se if we need to add items to the email
+                    $parsedErrors = $this->parseErrors($data['errors']);
+                    if ($parsedErrors) {
+                        $items[] = sprintf(
+                            '%s : %s [Browser: %s]',
+                            $time ? $time->format(\DateTime::ATOM) : '?',
+                            $data['url'],
+                            isset($data['browser']) ? $data['browser'] : ''
+                        );
+                        $items = array_merge($items, $parsedErrors);
+                        $items[] = '';
                     }
                 } else {
+                    // exception: ignore items with no errors
+                    if (isset($data['url']) && !isset($data['errors'])) {
+                        continue;
+                    }
+
                     $items[] = sprintf('Unknown format: %s', $item);
+                    $items[] = '';
                 }
-                $items[] = '';
             }
             $redis->hdel('client-validation', $item);
         }
-
         $html = implode('<br>', $items);
         if (!$html) {
             $html = 'No client validation failures';
         }
         $mailer->send('Client Validation Failures', 'tech@so-sure.com', $html);
+        return count($items) > 0 ? 'found validation failures' : 'no validation failures';
     }
 }
---
+++ src/AppBundle/Tests/Command/OpsReportCommandTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace AppBundle\Tests\Command;
+
+use AppBundle\Command\OpsReportCommand;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * @group functional-net
+ */
+class OpsReportCommandTest extends KernelTestCase
+{
+
+    protected static $container;
+    protected static $redis;
+    protected static $kernel;
+
+    public static function setUpBeforeClass()
+    {
+        //start the symfony kernel
+        self::$kernel = static::createKernel();
+        self::$kernel->boot();
+
+        //get the DI container
+        self::$container = self::$kernel->getContainer();
+
+        //now we can instantiate our service (if you want a fresh one for
+        //each test method, do this in setUp() instead
+        self::$redis = self::$container->get('snc_redis.default');
+    }
+
+    public function callCommand($expectedOutput)
+    {
+
+        $application = new Application(self::$kernel);
+        $application->add(new OpsReportCommand());
+        $command = $application->find('sosure:ops:report');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array(
+            'command'  => $command->getName(),
+        ));
+        $output = $commandTester->getDisplay();
+        $this->assertContains($expectedOutput, $output);
+
+    }
+
+    public function testOpsReportCommandEmptyValues()
+    {
+        $data = [
+            'url' => '/testurl',
+            'errors' => [
+                ['name' => 'firstName', 'value' => '', 'message' => 'Missing First Name'],
+                ['name' => 'Address', 'value' => '', 'message' => 'Missing First Name'],
+                ['name' => 'lastName', 'value' => '', 'message' => 'Missing LastName']
+            ],
+            'browser' => 'Internal'
+        ];
+        $now = new \DateTime();
+        self::$redis->hset('client-validation', json_encode($data), $now->format('U'));
+        $this->callCommand('no validation');
+    }
+
+    public function testOpsReportCommandFoundErrors()
+    {
+        $data = [
+            'url' => '/testurl2',
+            'errors' => [
+                ['name' => 'firstName', 'value' => '2', 'message' => 'Missing First Name'],
+                ['name' => 'lastName', 'value' => '', 'message' => 'Missing LastName']
+            ],
+            'browser' => 'Internal'
+        ];
+        $now = new \DateTime();
+        self::$redis->hset('client-validation', json_encode($data), $now->format('U'));
+        $this->callCommand('found validation');
+    }
+
+    public function testOpsReportCommandNoData()
+    {
+        $data = [
+            'url' => '/testurl3',
+            'browser' => 'Internal'
+        ];
+        $now = new \DateTime();
+        self::$redis->hset('client-validation', json_encode($data), $now->format('U'));
+        $this->callCommand('no validation');
+    }
+}
