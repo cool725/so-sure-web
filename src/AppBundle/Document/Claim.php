72a7ef657d8169462e17030c97082bf0f6dbcdf8
Patrick McAndrew <patrick@so-sure.com>
2018-02-06 08:55:23 +0000

    better displaying/handling of attribution + rolling 12 month

--- src/AppBundle/Document/Claim.php
+++ src/AppBundle/Document/Claim.php
@@ -1002,6 +1002,43 @@ class Claim
         return $data;
     }
 
+    public static function attributeClaims($claims, $percent = false)
+    {
+        $total = 0;
+        foreach ($claims as $claim) {
+            if ($attribution = $this->getPolicy()->getUser()->getAttribution()) {
+                $source = strtolower($attribution->getCampaignSource());
+                if (strlen(trim($source)) == 0) {
+                    $source = 'Untracked';
+                }
+            } else {
+                $source = 'Untracked';
+            }
+
+            if (isset($data[$source])) {
+                $data[$source]++;
+            } else {
+                $data[$source] = 1;
+            }
+            $total++;
+        }
+
+        if ($percent) {
+            if ($total == 0) {
+                return [];
+            }
+
+            $percent = [];
+            foreach ($data as $key => $value) {
+                $percent[$key] = $value / $total;
+            }
+
+            return $percent;
+        }
+
+        return $data;
+    }
+
     public function toModalArray()
     {
         return [
