25a2101f0589ad23d54eedfbe9f4e709e3c77d75
cool725 <109250241+cool725@users.noreply.github.com>
2018-02-08 14:36:29 +0000

    Add content functionality

--- src/AppBundle/Resources/public/js/Purchase/cancel.js
+++ src/AppBundle/Resources/public/js/Purchase/cancel.js
@@ -1,13 +1,13 @@
 // $(document).ready(function(){
 
-//     $('.btn-cancel-policy').on("click", function() {
-//         var reason = $(this).data('reason');
-//         console.log(reason);
-//         if (confirm('Are you sure you want to cancel your policy? Cancellation can not be undone and once cancelled, your phone will no longer be covered for Theft, Loss, & Accidental Damage.')) {
-//             $('#cancel_form_reason').val(reason);
-//             $('#cancel_form').submit();
-//         }
-//     });
+    $('.btn-cancel-policy').on("click", function() {
+        var reason = $(this).data('reason');
+        console.log(reason);
+        if (confirm('Are you sure you want to cancel your policy? Cancellation can not be undone and once cancelled, your phone will no longer be covered for Theft, Loss, & Accidental Damage.')) {
+            $('#cancel_form_reason').val(reason);
+            $('#cancel_form').submit();
+        }
+    });
 
 //     $('#cancel_form_cancel').on("click",function() {
 //         return confirm('Are you sure you want to cancel your policy? Cancellation can not be undone and once cancelled, your phone will no longer be covered for Theft, Loss, & Accidental Damage.');
@@ -19,18 +19,53 @@
 
 $(function(){
 
-    $('.btn-cancel').click(function(event) {
+    // Select cancel option add active class disable button
+    $('.btn-cancel').on('click', function(e) {
+        e.preventDefault();
+
         // Remove class from other buttons to get this choice
         $('.btn-cancel').removeClass('active user-choice');
         // Add the active class choice
         $(this).toggleClass('active user-choice');
 
-        var reason = $(this).data('reason');
-        console.log(reason);
+        var data = $(this).data();
+        // console.log(data.reason);
 
         // if ($('.user-choice').length) {
         $('#btn-next-step').removeAttr('disabled');
+        $('#btn-next-step').data('info', data.target);
         // }
+    });
+
+    // Once active show the correct section to the user
+    $('#btn-next-step').on('click', function(e) {
+        e.preventDefault();
+
+        var data = $(this).data();
+
+        // console.log(data.info);
+
+        $('.reasons').fadeOut('fast', function() {
+            $(data.info).fadeIn();
+        });
+    });
+
+    // If they want to go back allow
+    $('.btn-back').on('click', function(e) {
+        e.preventDefault();
+
+        $('.information').fadeOut('fast', function() {
+            $('.reasons').fadeIn();
+        });
+    });
+
+    // Cancel button
+    $('.btn-cancel-policy').on('click', function() {
+        var reason = $(this).data('reason');
+        if (confirm('Are you sure you want to cancel your policy? Cancellation can not be undone and once cancelled, your phone will no longer be covered for Theft, Loss, & Accidental Damage.')) {
+            $('#cancel_form_reason').val(reason);
+            $('#cancel_form').submit();
+        }
     });
 
 });
