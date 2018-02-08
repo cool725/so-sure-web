bef80b60e7b2853ce501405a862db600e4d2117a
cool725 <109250241+cool725@users.noreply.github.com>
2018-02-08 11:25:56 +0000

    Adding basic structure and functionality

--- src/AppBundle/Resources/public/js/Purchase/cancel.js
+++ src/AppBundle/Resources/public/js/Purchase/cancel.js
@@ -1,13 +1,36 @@
-$(document).ready(function(){
-    $('.btn-cancel-policy').on("click", function() {
+// $(document).ready(function(){
+
+//     $('.btn-cancel-policy').on("click", function() {
+//         var reason = $(this).data('reason');
+//         console.log(reason);
+//         if (confirm('Are you sure you want to cancel your policy? Cancellation can not be undone and once cancelled, your phone will no longer be covered for Theft, Loss, & Accidental Damage.')) {
+//             $('#cancel_form_reason').val(reason);
+//             $('#cancel_form').submit();
+//         }
+//     });
+
+//     $('#cancel_form_cancel').on("click",function() {
+//         return confirm('Are you sure you want to cancel your policy? Cancellation can not be undone and once cancelled, your phone will no longer be covered for Theft, Loss, & Accidental Damage.');
+//     });
+
+
+
+// });
+
+$(function(){
+
+    $('.btn-cancel').click(function(event) {
+        // Remove class from other buttons to get this choice
+        $('.btn-cancel').removeClass('active user-choice');
+        // Add the active class choice
+        $(this).toggleClass('active user-choice');
+
         var reason = $(this).data('reason');
         console.log(reason);
-        if (confirm('Are you sure you want to cancel your policy? Cancellation can not be undone and once cancelled, your phone will no longer be covered for Theft, Loss, & Accidental Damage.')) {
-            $('#cancel_form_reason').val(reason);
-            $('#cancel_form').submit();
-        }
+
+        // if ($('.user-choice').length) {
+        $('#btn-next-step').removeAttr('disabled');
+        // }
     });
-    $('#cancel_form_cancel').on("click",function() {
-        return confirm('Are you sure you want to cancel your policy? Cancellation can not be undone and once cancelled, your phone will no longer be covered for Theft, Loss, & Accidental Damage.');
-    });
+
 });
