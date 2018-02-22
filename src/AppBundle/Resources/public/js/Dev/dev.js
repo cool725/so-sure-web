83c923522e3595540b2b164f6945fb6b9c301f40
cool725 <109250241+cool725@users.noreply.github.com>
2018-02-22 13:24:16 +0000

    Remove gb

--- src/AppBundle/Resources/public/js/Dev/dev.js
+++ src/AppBundle/Resources/public/js/Dev/dev.js
@@ -1,6 +1,4 @@
 // dev.js
-// Show it's dev environment in the console
-console.log('%c so-sure dev ', 'background: #3399ff; color: #fff; font-size: 30px');
 
 // Reload for dev bar
 $('#dev-reload').on('click', function(e) {
