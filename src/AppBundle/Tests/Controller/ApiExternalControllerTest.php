490a8bcf7b9659ccfea44e185dc8c09d5cd986ef
Patrick McAndrew <patrick@so-sure.com>
2018-02-24 20:46:26 +0000

    move to external controller with key

--- src/AppBundle/Tests/Controller/ApiExternalControllerTest.php
+++ src/AppBundle/Tests/Controller/ApiExternalControllerTest.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use AppBundle\Document\User;
 use AppBundle\Document\OptOut\EmailOptOut;
 use AppBundle\Classes\ApiErrorCode;
+use AppBundle\Classes\GoCompare;
 use AppBundle\Service\RateLimitService;
 
 /**
@@ -315,5 +316,84 @@ class ApiExternalControllerTest extends BaseControllerTest
         );
 
         $data = $this->verifyResponse(404, ApiErrorCode::ERROR_NOT_FOUND);
+    }
+
+    // gocompare
+
+    /**
+     *
+     */
+    public function testGoCompare()
+    {
+        $data = '{"request": {
+  "gadgets" : [
+    {"gadget": {"gadget_id": 20, "loss_cover": true}},
+    {"gadget": {"gadget_id": 21, "loss_cover": true}},
+    {"gadget": {"gadget_id": 22, "loss_cover": true}},
+    {"gadget": {"gadget_id": 23, "loss_cover": true}},
+    {"gadget": {"gadget_id": 24, "loss_cover": true}},
+    {"gadget": {"gadget_id": 25, "loss_cover": true}},
+    {"gadget": {"gadget_id": 26, "loss_cover": true}},
+    {"gadget": {"gadget_id": 27, "loss_cover": true}},
+    {"gadget": {"gadget_id": 28, "loss_cover": true}},
+    {"gadget": {"gadget_id": 29, "loss_cover": true}},
+    {"gadget": {"gadget_id": 30, "loss_cover": true}},
+    {"gadget": {"gadget_id": 31, "loss_cover": true}},
+    {"gadget": {"gadget_id": 32, "loss_cover": true}},
+    {"gadget": {"gadget_id": 33, "loss_cover": true}},
+    {"gadget": {"gadget_id": 34, "loss_cover": true}},
+    {"gadget": {"gadget_id": 35, "loss_cover": true}},
+    {"gadget": {"gadget_id": 36, "loss_cover": true}},
+    {"gadget": {"gadget_id": 37, "loss_cover": true}},
+    {"gadget": {"gadget_id": 38, "loss_cover": true}},
+    {"gadget": {"gadget_id": 39, "loss_cover": true}},
+    {"gadget": {"gadget_id": 40, "loss_cover": true}},
+    {"gadget": {"gadget_id": 41, "loss_cover": true}},
+
+    {"gadget": {"gadget_id": 215, "loss_cover": true}},
+    {"gadget": {"gadget_id": 216, "loss_cover": true}},
+    {"gadget": {"gadget_id": 217, "loss_cover": true}},
+    {"gadget": {"gadget_id": 218, "loss_cover": true}},
+    {"gadget": {"gadget_id": 219, "loss_cover": true}},
+    {"gadget": {"gadget_id": 220, "loss_cover": true}},
+    {"gadget": {"gadget_id": 221, "loss_cover": true}},
+    {"gadget": {"gadget_id": 222, "loss_cover": true}},
+    {"gadget": {"gadget_id": 223, "loss_cover": true}},
+    {"gadget": {"gadget_id": 224, "loss_cover": true}},
+
+    {"gadget": {"gadget_id": 806, "loss_cover": true}},
+    {"gadget": {"gadget_id": 807, "loss_cover": true}},
+    {"gadget": {"gadget_id": 808, "loss_cover": true}},
+    {"gadget": {"gadget_id": 809, "loss_cover": true}},
+
+    {"gadget": {"gadget_id": 814, "loss_cover": true}},
+    {"gadget": {"gadget_id": 815, "loss_cover": true}},
+    {"gadget": {"gadget_id": 816, "loss_cover": true}},
+    {"gadget": {"gadget_id": 817, "loss_cover": true}},
+    {"gadget": {"gadget_id": 818, "loss_cover": true}},
+    {"gadget": {"gadget_id": 819, "loss_cover": true}},
+    {"gadget": {"gadget_id": 820, "loss_cover": true}},
+
+    {"gadget": {"gadget_id": 835, "loss_cover": true}},
+    {"gadget": {"gadget_id": 836, "loss_cover": true}}
+  ]
+}}';
+        $url = sprintf(
+            '/external/gocompare?gocompare_key=%s',
+            static::$container->getParameter('gocompare_key')
+        );
+
+        $crawler =  static::$client->request(
+            "POST",
+            $url,
+            array(),
+            array(),
+            array(
+                'CONTENT_TYPE' => 'application/json'
+            ),
+            $data
+        );
+        $data = $this->verifyResponse(200);
+        $this->assertEquals(45, count($data['response']), json_encode($data));
     }
 }
