8442750013519b4c8a78ce0ec98e20d8aabb502e
Patrick McAndrew <patrick@so-sure.com>
2018-03-27 15:50:35 +0100

    add email notificaiton

--- src/AppBundle/Form/Type/BacsConfirmType.php
+++ src/AppBundle/Form/Type/BacsConfirmType.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Form\Type;
 
+use AppBundle\Service\PCAService;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -11,9 +12,31 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
 
 class BacsConfirmType extends AbstractType
 {
+    /**
+     * @var boolean
+     */
+    private $required;
+
+    /**
+     * @var PCAService
+     */
+    private $pcaService;
+
+    /**
+     * @param PCAService $pcaService
+     * @param boolean    $required
+     */
+    public function __construct(PCAService $pcaService, $required)
+    {
+        $this->pcaService = $pcaService;
+        $this->required = $required;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -22,6 +45,27 @@ class BacsConfirmType extends AbstractType
             ->add('accountNumber', HiddenType::class, ['required' => true])
             ->add('save', SubmitType::class)
         ;
+        $builder->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) {
+            /** @var Bacs $bacs */
+            $bacs = $event->getData();
+            $form = $event->getForm();
+            try {
+                // TODO: Add user
+                $bankAccount = $this->pcaService->getBankAccount($bacs->getSortCode(), $bacs->getAccountNumber());
+                $bacs->setBankAccount($bankAccount);
+            } catch (DirectDebitBankException $e) {
+                if ($e->getCode() == DirectDebitBankException::ERROR_SORT_CODE) {
+                    $form->get('sortCode')->addError(new FormError('Sort code does not exist'));
+                } elseif ($e->getCode() == DirectDebitBankException::ERROR_ACCOUNT_NUMBER) {
+                    $form->get('accountNumber')->addError(
+                        new FormError('Account number is invalid for this sort code')
+                    );
+                } elseif ($e->getCode() == DirectDebitBankException::ERROR_NON_DIRECT_DEBIT) {
+                    $form->get('accountNumber')->addError(new FormError('Account does not support direct debit'));
+                }
+            }
+
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver)
