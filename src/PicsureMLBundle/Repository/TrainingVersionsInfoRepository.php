4d4f08270bc40ae3d1ce331a0e5975f3b3306f2c
Julien Champagne <julienchampagne@Juliens-MacBook-Pro-2.local>
2018-03-28 11:10:42 +0100

    update fixtures loading

--- src/PicsureMLBundle/Repository/TrainingVersionsInfoRepository.php
+++ src/PicsureMLBundle/Repository/TrainingVersionsInfoRepository.php
@@ -7,13 +7,4 @@ use PicsureMLBundle\Document\TrainingVersionsInfo;
 
 class TrainingVersionsInfoRepository extends DocumentRepository
 {
-
-    public function create()
-    {
-        $versionInfo = new TrainingVersionsInfo();
-        $versionInfo->addVersion(1);
-        $versionInfo->setLatestVersion(1);
-
-        return $versionInfo;
-    }
 }
