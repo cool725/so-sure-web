d3b0d69b2837b5620d31888bb8eff0493c127406
cool725 <109250241+cool725@users.noreply.github.com>
2018-05-21 11:06:52 +0100

    Remove defaqto

--- src/AppBundle/Service/SixpackService.php
+++ src/AppBundle/Service/SixpackService.php
@@ -54,7 +54,7 @@ class SixpackService
     const EXPERIMENT_STEP_3 = 'step-3-payment-new';
     const EXPERIMENT_PURCHASE_FLOW_BACS = 'purchase-flow-bacs';
     const EXPERIMENT_CPC_QUOTE_HOMEPAGE = 'cpc-quote-or-homepage';
-    const EXPERIMENT_DEFACTO = 'defacto';
+    // const EXPERIMENT_DEFACTO = 'defacto';
     const EXPERIMENT_DOB = 'purchase-funnel-dob-dropdown';
     const EXPERIMENT_72_REPLACEMENT = 'seventytwo-hours';
     const EXPERIMENT_MONEY_LANDING = 'money-landing';
@@ -83,7 +83,7 @@ class SixpackService
         self::EXPERIMENT_STEP_3,
         self::EXPERIMENT_PURCHASE_FLOW_BACS,
         self::EXPERIMENT_CPC_QUOTE_HOMEPAGE,
-        self::EXPERIMENT_DEFACTO,
+        // self::EXPERIMENT_DEFACTO,
         self::EXPERIMENT_DOB,
         self::EXPERIMENT_72_REPLACEMENT,
         self::EXPERIMENT_MONEY_LANDING,
@@ -132,7 +132,7 @@ class SixpackService
     public static $purchaseConversionKpi = [
         self::EXPERIMENT_QUOTE_INTERCOM_PURCHASE,
         self::EXPERIMENT_HOMEPAGE_AA_V2,
-        self::EXPERIMENT_DEFACTO,
+        // self::EXPERIMENT_DEFACTO,
         self::EXPERIMENT_DOB,
     ];
 
