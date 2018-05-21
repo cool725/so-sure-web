abc005c0991a073eb338a91061183f76d07d97d7
Patrick McAndrew <patrick@so-sure.com>
2018-05-21 12:01:47 +0100

    fix clicking

--- src/AppBundle/Resources/public/js/About/privacy.js
+++ src/AppBundle/Resources/public/js/About/privacy.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $('.scroll-to-link').click(function(e) {
+        e.preventDefault();
         var link = $(this).data('link');
         $(this).closest('.panel-collapse').collapse('hide');
         $(link).closest('.panel-collapse').collapse('show');
@@ -7,7 +8,6 @@ $(document).ready(function () {
             scrollTop: $(link).offset().top - 500
         }, 2000).bind(link);
     });
-
 
     $('.print').click(function() {
         //$('#privacy-accordion .collapse').collapse('show');
