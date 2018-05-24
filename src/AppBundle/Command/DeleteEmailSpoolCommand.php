abde184c9f0bea3c96e41aa5464e83701a4a725f
Patrick McAndrew <patrick@so-sure.com>
2018-05-24 18:17:44 +0100

    add email delete command

---
+++ src/AppBundle/Command/DeleteEmailSpoolCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Service\PCAService;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\Table;
+use AppBundle\Classes\Premium;
+
+class DeleteEmailSpoolCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('sosure:delete:email:spool')
+            ->setDescription('Delete email spool')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** @var Client $redis */
+        $redis = $this->getContainer()->get('snc_redis.mailer');
+        $output->writeln(sprintf('%d emails in queue (prior to this)', $redis->llen('swiftmailer')));
+
+        $redis->del('swiftmailer');
+    }
+}
