960f51b4e0dde55372ae8500116a8eef499ef9d8
cool725 <109250241+cool725@users.noreply.github.com>
2018-05-24 11:03:18 +0100

    Finalise to test

--- src/AppBundle/Resources/public/js/Default/selectPhoneMakeDropdown.js
+++ src/AppBundle/Resources/public/js/Default/selectPhoneMakeDropdown.js
@@ -4,8 +4,6 @@ $(function() {
     if ($('.dropdown-phone-form').length) {
 
         // Elements & Data
-        var phone      = $('#phone');
-        var phoneName  = $('#phone-name');
         var phones     = $('#select-phone-data').data('phones');
         var memOptTest = $('#select-phone-data').data('show-single-mem-opt');
         var make       = $('.select-phone-make');
@@ -23,15 +21,12 @@ $(function() {
             // If make selected customise default value
             if (make.val()) {
                 model.append($('<option />').val('').text('Now select your ' + make.val() + ' device'));
-                // phoneName.text(make.val() + '...');
             } else {
                 model.append($('<option />').val('').text('Select your phone make first'));
-                // phoneName.text();
             }
 
             // Clear the other select out
             memory.append($('<option />').val('').text('Select your phone model first'));
-
 
             // Get phones from list and show featured
             $.each(phones[make.val()], function(key, value) {
@@ -70,10 +65,8 @@ $(function() {
             // If model selected customise default value
             if (model) {
                 memory.append($('<option />').val('').text('And finally select your memory size'));
-                // phoneName.text(make.val() + ' ' + model.val() + '...');
             } else {
                 memory.append($('<option />').val('').text('Select your phone model first'));
-                // phoneName.text(make.val());
             }
 
             // Get phones from list and add to options
@@ -115,10 +108,10 @@ $(function() {
         // When user selects model > update
         $('.select-phone-model').on('change', function() {
 
+            var value = $(this).val();
+
             // Update Memory
             updateMemory();
-
-            var value = $(this).val();
 
             // Get memory options
             var memSel = memory.children('option').not('[value=""]').val();
@@ -126,39 +119,29 @@ $(function() {
 
             // Check for memory test otherwise revert
             if (memOptTest == true) {
-                if (value != '') {
-                    memory.show();
-                    if (memOptTest == true) {
+                // If more than one storage option
+                if (memOpt > 1) {
+                    // console.log(make.val() + ' ' + model.val() + ' has more than one storage size');
+                    if (value != '') {
+                        memory.show();
+                        $(this).hide();
+                    } else {
+                        make.show();
+                        memory.hide();
                         $(this).hide();
                     }
                 } else {
-                    make.show();
-                    memory.hide();
-                    if (memOptTest == true) {
+                    // If only one storage option
+                    // console.log(make.val() + ' ' + model.val() + ' has only one storage size');
+                    if (value != '') {
+                        memory.val(memSel);
+                        controls.show();
+                    } else {
+                        make.show();
+                        memory.hide();
                         $(this).hide();
                     }
                 }
-                // If more than one memory size
-                // if (memOpt > 1) {
-                //     if (value != '') {
-                //         memory.show();
-                //     } else {
-                //         make.show();
-                //         memory.hide();
-                //     }
-                // } else {
-                //     // If only one memory option
-                //     if (value != '') {
-                //         memory.hide().val(memSel);
-                //         controls.show();
-                //         // $(this).val($(this).val() + );
-                //         // phoneName.text(make.val() + ' ' + model.val() + ' ' + memory.find('option:selected').text());
-                //     } else {
-                //         make.show();
-                //         memory.hide();
-                //         // phoneName.text(make.val() + ' ' + model.val() + '...');
-                //     }
-                // }
             } else {
                 if (value != '') {
                     memory.show();
@@ -174,14 +157,12 @@ $(function() {
 
             if ($(this).val() != '') {
                 controls.show();
-                // phoneName.text(make.val() + ' ' + model.val() + ' ' + memory.find('option:selected').text());
             } else {
                 controls.hide();
                 if (memOptTest == true) {
                     $(this).hide();
                     model.show();
                 }
-                // phoneName.text(make.val() + ' ' + model.val() + '...');
             }
 
         });
