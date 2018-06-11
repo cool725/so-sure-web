cc9b92f50f98c813c9e11471b5c4b357e1898196
cool725 <109250241+cool725@users.noreply.github.com>
2018-06-11 11:06:26 +0100

    Update ignore message

--- app/Resources/public/js/rollbar.js
+++ app/Resources/public/js/rollbar.js
@@ -6,7 +6,7 @@ var _rollbarConfig = {
         environment: document.getElementById('ss-root').getAttribute('data-environment')
     },
     ignoredMessages: [
-        "(unknown)"
+        "(unknown): Script error."
     ]
 };
 // Rollbar Snippet
