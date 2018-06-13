6d84fc861a9bd2147f3dc50a039d039c94e3bafb
cool725 <109250241+cool725@users.noreply.github.com>
2018-06-13 14:37:58 +0100

    Removed check for sticky search as no longer test

--- src/AppBundle/Resources/public/js/Default/homepage.js
+++ src/AppBundle/Resources/public/js/Default/homepage.js
@@ -12,49 +12,33 @@ $(function(){
         });
     });
 
-    // Sticky search - now using affix BS
-    var stickySearch = $('#select-phone-data').data('sticky-search');
-
     // Single MEM Option/Look Test
     // Add test layer
     var memOptTest = $('#select-phone-data').data('show-single-mem-opt');
     var memory     = $('.select-phone-memory');
 
-    if (stickySearch) {
+    // Offset of search from top of page
+    var stickyOffset = $('#select-phone-data').offset().top;
 
-        // Offset of search from top of page
-        var stickyOffset = $('#select-phone-data').offset().top;
-
-        // Init BS affix
-        $('#select-phone-data').affix({
-            offset: {
-                top: stickyOffset + 50,
-                bottom: function () {
-                    return (this.bottom = $('footer').outerHeight(true) + 1000)
-                }
+    // Init BS affix
+    $('#select-phone-data').affix({
+        offset: {
+            top: stickyOffset + 50,
+            bottom: function () {
+                return (this.bottom = $('footer').outerHeight(true) + 1000)
             }
-        }).on('affixed.bs.affix', function(e) {
-            // This event is fired after the element has been affixed.
+        }
+    }).on('affixed.bs.affix', function(e) {
+        // This event is fired after the element has been affixed.
 
+        // Add animation
+        $(this).addClass('animated fadeInDown');
 
-            // $('#select-phone-data form').addClass('form-inline');
-            // $('#launch_phone_next').removeClass('btn-block');
+    }).on('affix-top.bs.affix',function() {
+        // This event fires immediately before the element has been affixed-top.
 
+        // Remove animation to refire
+        $(this).removeClass('animated fadeInDown');
 
-            // Add animation
-            $(this).addClass('animated fadeInDown');
-
-        }).on('affix-top.bs.affix',function() {
-            // This event fires immediately before the element has been affixed-top.
-
-            // Remove animation to refire
-            $(this).removeClass('animated fadeInDown');
-
-        });
-
-        // function checkScroll() {
-        //     if
-        // }
-    }
-
+    });
 });
