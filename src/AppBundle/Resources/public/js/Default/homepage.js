150af0ec8ffb6f875e3eb3402474e0137fd4542e
cool725 <109250241+cool725@users.noreply.github.com>
2018-06-13 14:43:51 +0100

    Wait till window has loaded before calling affix

--- src/AppBundle/Resources/public/js/Default/homepage.js
+++ src/AppBundle/Resources/public/js/Default/homepage.js
@@ -20,25 +20,26 @@ $(function(){
     // Offset of search from top of page
     var stickyOffset = $('#select-phone-data').offset().top;
 
-    // Init BS affix
-    $('#select-phone-data').affix({
-        offset: {
-            top: stickyOffset + 50,
-            bottom: function () {
-                return (this.bottom = $('footer').outerHeight(true) + 1000)
+    $(window).bind('load', function() {
+        // Init BS affix
+        $('#select-phone-data').affix({
+            offset: {
+                top: stickyOffset + 50,
+                bottom: function () {
+                    return (this.bottom = $('footer').outerHeight(true) + 1000)
+                }
             }
-        }
-    }).on('affixed.bs.affix', function(e) {
-        // This event is fired after the element has been affixed.
+        }).on('affixed.bs.affix', function(e) {
+            // This event is fired after the element has been affixed.
 
-        // Add animation
-        $(this).addClass('animated fadeInDown');
+            // Add animation
+            $(this).addClass('animated fadeInDown');
 
-    }).on('affix-top.bs.affix',function() {
-        // This event fires immediately before the element has been affixed-top.
+        }).on('affix-top.bs.affix',function() {
+            // This event fires immediately before the element has been affixed-top.
 
-        // Remove animation to refire
-        $(this).removeClass('animated fadeInDown');
-
+            // Remove animation to refire
+            $(this).removeClass('animated fadeInDown');
+        });
     });
 });
