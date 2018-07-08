edb26081e627be3e45518eacab7bdb2e44cf5137
Patrick McAndrew <patrick@so-sure.com>
2018-07-08 15:28:09 +0100

    not app.logger

--- src/AppBundle/Controller/OpsController.php
+++ src/AppBundle/Controller/OpsController.php
@@ -604,7 +604,7 @@ class OpsController extends BaseController
             $postcode = new Postcode($data['postcode']);
 
             if (!$this->get('census.search')->validatePostcode($data['postcode'])) {
-                $this->get('app.logger')->error(sprintf(
+                $this->get('logger')->error(sprintf(
                     'Postcode %s was found in PCA but missing from local db',
                     $data['postcode']
                 ));
