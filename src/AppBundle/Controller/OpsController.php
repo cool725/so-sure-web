94901809385b2ef2c5b92573472ccb0df664d392
cool725 <109250241+cool725@users.noreply.github.com>
2018-07-25 15:14:20 +0100

    Exclude domains from logging

--- src/AppBundle/Controller/OpsController.php
+++ src/AppBundle/Controller/OpsController.php
@@ -516,6 +516,8 @@ class OpsController extends BaseController
                 'savingsslider-a.akamaihd.net',
                 'cdncache-a.akamaihd.net',
                 'sxt.cdn.skype.com',
+                'fbrpc://call',
+                'cdn.joinhoney.com',
             ])) {
                 $logger->debug(sprintf('Content-Security-Policy called with ignore host: %s', $host));
 
