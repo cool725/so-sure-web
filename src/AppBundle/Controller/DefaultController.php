5ae3357f19f309bb27e371f9c2ac3dd1ef6513ac
cool725 <109250241+cool725@users.noreply.github.com>
2018-07-25 17:16:03 +0100

    Remove

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -125,7 +125,7 @@ class DefaultController extends BaseController
             $expSearch = $this->sixpack(
                 $request,
                 SixpackService::EXPERIMENT_TEXT_VS_DROPDOWN_MOBILE,
-                ['dropdown-search-mobile-old', 'dropdown-search']
+                ['dropdown-search-old', 'dropdown-search']
             );
         } else {
             $expSearch = $this->sixpack(
