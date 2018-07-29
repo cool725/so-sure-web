ddc4ec464669ae0d10e44b70950ddc87d32a1cce
Patrick McAndrew <patrick@so-sure.com>
2018-07-29 21:25:05 +0100

    add missing validator trait

--- src/AppBundle/Document/ValidatorTrait.php
+++ src/AppBundle/Document/ValidatorTrait.php
@@ -2,11 +2,19 @@
 
 namespace AppBundle\Document;
 
+use AppBundle\Validator\Constraints\AlphanumericSpaceDotPipeValidator;
 use AppBundle\Validator\Constraints\AlphanumericSpaceDotValidator;
 use AppBundle\Validator\Constraints\AlphanumericValidator;
 
 trait ValidatorTrait
 {
+    protected function conformAlphanumericSpaceDotPipe($value, $length)
+    {
+        $validator = new AlphanumericSpaceDotPipeValidator();
+
+        return $validator->conform(mb_substr($value, 0, $length));
+    }
+
     protected function conformAlphanumericSpaceDot($value, $length)
     {
         $validator = new AlphanumericSpaceDotValidator();
