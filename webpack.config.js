d0dd517d2df128c511de0b11bfb1013fa4f8e1ec
cool725 <109250241+cool725@users.noreply.github.com>
2018-08-02 10:02:56 +0100

    Update package.json from Patricks branch

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,8 @@ Encore
     // the public path used by the web server to access the previous directory
     .setPublicPath('/web')
 
-    // this creates a 'vendor.js' file with jquery and the bootstrap JS module
-    .createSharedEntry('vendor', './web/components/jquery/jquery.min.js')
+    // this creates a 'vendor.js' file with jquery and the bootstrap JS module plus popper
+    .createSharedEntry('vendor', './web/components/vendor.js')
 
     // List all files here
     .addEntry('global', './src/AppBundle/Resources/public/rebrand/js/global.js')
