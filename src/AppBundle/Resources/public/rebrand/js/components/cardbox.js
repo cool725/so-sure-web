73604c5a7e37e176513f69870c95e77aaed0cb3d
cool725 <109250241+cool725@users.noreply.github.com>
2018-08-09 10:22:47 +0100

    Latest

--- src/AppBundle/Resources/public/rebrand/js/components/cardbox.js
+++ src/AppBundle/Resources/public/rebrand/js/components/cardbox.js
@@ -7,10 +7,12 @@ $(function() {
     cardbox.on('click', function(e) {
         e.preventDefault();
 
+        // Toggle the content
         $(this).toggleClass('expanding-cardbox__open')
-        .find('.expanding-cardbox__expand')
+        .find('.expanding-cardbox__expand, .expanding-cardbox__excerpt')
         .slideToggle('fast');
 
+        // Adjust the arrow
         $(this).find('.fas').toggleClass('fa-chevron-up');
     });
 
