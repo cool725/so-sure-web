b6801e825f290998e6ed7bc6db6a35aca95a1105
cool725 <109250241+cool725@users.noreply.github.com>
2018-08-09 17:05:43 +0100

    Change ordering of js loading

--- src/AppBundle/Resources/public/rebrand/js/components/phoneSearchDropdown.js
+++ src/AppBundle/Resources/public/rebrand/js/components/phoneSearchDropdown.js
@@ -103,6 +103,7 @@ $(function() {
         });
 
         updateModels();
+        // make.resizeselect();
 
     }
 });
--- src/AppBundle/Resources/public/rebrand/js/global.js
+++ src/AppBundle/Resources/public/rebrand/js/global.js
@@ -5,8 +5,8 @@ require('../sass/global.scss');
 // Require components
 require('./common/track.js');
 require('./components/nav.js');
-require('./components/phoneSearchDropdown.js');
 require('./components/getQuote.js');
+require('./components/phoneSearchDropdown.js');
 require('./components/cardbox.js');
 
 // Enable fa pseudo elements
