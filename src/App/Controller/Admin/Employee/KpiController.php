023121f9a81db7d14bc352ea9013772089b00253
Alister Bulman <abulman@gmail.com>
2018-08-16 15:21:32 +0100

    Refactor to get to a kpiController::reportForPeriod(start, end)

--- src/App/Controller/Admin/Employee/KpiController.php
+++ src/App/Controller/Admin/Employee/KpiController.php
@@ -47,7 +47,7 @@ class KpiController extends AbstractController
      * @Route("/kpi/{now}", name="admin_kpi_date")
      * @Template("AppBundle::AdminEmployee/kpi.html.twig")
      */
-    public function kpiAction($now = null)
+    public function kpiAction($now = null): array
     {
         // default 30s for prod is no longer enough
         // TODO: Refactor method to improve performance
@@ -55,9 +55,9 @@ class KpiController extends AbstractController
         $numWeeks = 4;
 
         if (!$now) {
-            $now = new \DateTime();
+            $now = new \DateTimeImmutable();
         } else {
-            $now = new \DateTime($now);
+            $now = new \DateTimeImmutable($now);
         }
 
         $weeks = $this->getReportsByWeek($now, $numWeeks);
@@ -66,7 +66,7 @@ class KpiController extends AbstractController
         $reversedAdjustedWeeks = array_reverse($adjustedWeeks);
         /** @var \DateTime $prevPageDate */
         $prevPageDate = $reversedAdjustedWeeks[0]['end_date'];
-        $prevPageDate->add(new \DateInterval('P'.($numWeeks).'W'));
+        $prevPageDate->add(new \DateInterval('P'.$numWeeks.'W'));
 
         return [
             'weeks' => $reversedAdjustedWeeks,
@@ -80,7 +80,7 @@ class KpiController extends AbstractController
         ];
     }
 
-    private function getReportsByWeek($now, $numWeeks): array
+    private function getReportsByWeek(\DateTimeImmutable $now, int $numWeeks): array
     {
         $date = $this->showFromDate($now, $numWeeks);
 
@@ -90,53 +90,14 @@ class KpiController extends AbstractController
         while ($date < $now) {
             $end = clone $date;
             $end->add(new \DateInterval('P6D'));
-            $end = $this->endOfDay($end);
-            $week = [
-                'start_date' => clone $date,
-                'end_date' => $end,
-                'end_date_disp' => (clone $end)->sub(new \DateInterval('PT1S')),
-                'count' => $count,
-            ];
 
-            $start = $this->startOfDay(clone $date);
-            $date = $date->add(new \DateInterval('P7D'));
+            $week = $this->reportForPeriod($this->startOfDay(clone $date), $this->endOfDay($end));
 
-            $count++;
-            $week['period'] = $this->reporting->report($start, $end, true);
-            // $totalStart = clone $end;
-            // $totalStart = $totalStart->sub(new \DateInterval('P1Y'));
-            $week['total'] = $this->reporting->report(new \DateTime(SoSure::POLICY_START), $end, true);
-            $week['sumPolicies'] = $this->reporting->sumTotalPoliciesPerWeek($end);
-
-            $approved = $week['total']['approvedClaims'][Claim::STATUS_APPROVED]
-                + $week['total']['approvedClaims'][Claim::STATUS_SETTLED];
-
-            $week['freq-claims'] = 'N/A';
-            if ($week['sumPolicies'] != 0) {
-                $week['freq-claims'] = ((52 * $approved) / $week['sumPolicies']);
-            }
-            $week['total-policies'] = $this->policyRepo->countAllActivePolicies($date);
-
-            /** @var Stats[] $stats */
-            $stats = $this->statsRepo->getStatsByRange($start, $date);
-            foreach ($stats as $stat) {
-                if (!isset($week[$stat->getName()])) {
-                    $week[$stat->getName()] = 0;
-                }
-                if (!$stat->isAbsolute()) {
-                    $week[$stat->getName()] += $stat->getValue();
-                } else {
-                    $week[$stat->getName()] = $stat->getValue();
-                }
-            }
-
-            foreach (Stats::$allStats as $stat) {
-                if (!isset($week[$stat])) {
-                    $week[$stat] = '-';
-                }
-            };
-
+            $week['count'] = $count;
             $weeks[] = $week;
+
+            $date = $date->add(new \DateInterval('P7D'));
+            $count++;
         }
 
         return $weeks;
@@ -155,5 +116,50 @@ class KpiController extends AbstractController
         $date = $date->sub(new \DateInterval(sprintf('P%dD', $numWeeks * 7)));
 
         return $date;
+    }
+
+    private function reportForPeriod(\DateTime $start, \DateTime $end): array
+    {
+        $week = [
+            'start_date' => clone $start,
+            'end_date' => $end,
+            'end_date_disp' => (clone $end)->sub(new \DateInterval('PT1S')),
+        ];
+
+        $week['period'] = $this->reporting->report($start, $end, true);
+        // $totalStart = clone $end;
+        // $totalStart = $totalStart->sub(new \DateInterval('P1Y'));
+        $week['total'] = $this->reporting->report(new \DateTime(SoSure::POLICY_START), $end, true);
+        $week['sumPolicies'] = $this->reporting->sumTotalPoliciesPerWeek($end);
+
+        $approved = $week['total']['approvedClaims'][Claim::STATUS_APPROVED] +
+            $week['total']['approvedClaims'][Claim::STATUS_SETTLED];
+
+        $week['freq-claims'] = 'N/A';
+        if ($week['sumPolicies'] != 0) {
+            $week['freq-claims'] = ((52 * $approved) / $week['sumPolicies']);
+        }
+        $week['total-policies'] = $this->policyRepo->countAllActivePolicies($end);
+
+        /** @var Stats[] $stats */
+        $stats = $this->statsRepo->getStatsByRange($start, $end);
+        foreach ($stats as $stat) {
+            if (!isset($week[$stat->getName()])) {
+                $week[$stat->getName()] = 0;
+            }
+            if (!$stat->isAbsolute()) {
+                $week[$stat->getName()] += $stat->getValue();
+            } else {
+                $week[$stat->getName()] = $stat->getValue();
+            }
+        }
+
+        foreach (Stats::$allStats as $stat) {
+            if (!isset($week[$stat])) {
+                $week[$stat] = '-';
+            }
+        }
+
+        return $week;
     }
 }
