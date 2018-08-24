a051bddf26a14c314d604a8ae1eb642a2854947e
cool725 <109250241+cool725@users.noreply.github.com>
2018-08-24 09:26:45 +0100

    Btn shadow add hover class from Jules and remove

--- src/AppBundle/Resources/public/rebrand/js/components/phoneSearchDropdown.js
+++ src/AppBundle/Resources/public/rebrand/js/components/phoneSearchDropdown.js
@@ -103,9 +103,9 @@ $(function() {
 
                 if ($(this).val() != '') {
                     $(this).addClass('valid-select');
-                    button.prop('disabled', '').removeClass('btn-outline-white').addClass('btn-white btn-shadow-jules');
+                    button.prop('disabled', '').removeClass('btn-outline-white').addClass('btn-white btn-shadow');
                 } else {
-                    button.prop('disabled', 'disabled').removeClass('btn-white btn-shadow-jules').addClass('btn-outline-white');
+                    button.prop('disabled', 'disabled').removeClass('btn-white btn-shadow').addClass('btn-outline-white');
                     $(this).removeClass('valid-select');
                 }
             });
