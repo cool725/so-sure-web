b5abca3c811b9e2d6abcd5f799b3e8d25b9ab748
Patrick McAndrew <patrick@so-sure.com>
2018-09-11 09:45:52 +0100

    prevent fnol claims from being exported add claims handler to export

--- src/AppBundle/Service/SalvaExportService.php
+++ src/AppBundle/Service/SalvaExportService.php
@@ -472,6 +472,7 @@ class SalvaExportService
                     $claim->getReplacementPhone()->getModel() :
                     '',
                 $claim->getReplacementImei(),
+                $claim->getHandlingTeam() ? $claim->getHandlingTeam() : '',
             ];
         } else {
             $data = [
@@ -490,6 +491,7 @@ class SalvaExportService
                 'ReplacementMake',
                 'ReplacementModel',
                 'ReplacementImei',
+                'Claims Handler',
             ];
         }
 
