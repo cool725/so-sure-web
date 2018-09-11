11d36f30e2faf159eff33e645ff5ff3aa52c9d8e
cool725 <109250241+cool725@users.noreply.github.com>
2018-09-11 09:22:30 +0100

    Tidy up controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -84,13 +84,6 @@ class DefaultController extends BaseController
             $this->get('logger')->debug(sprintf('Referral %s', $referral));
         }
 
-        // $this->sixpack(
-        //     $request,
-        //     SixpackService::EXPERIMENT_HOMEPAGE_AA_V2,
-        //     ['A1', 'A2'],
-        //     SixpackService::LOG_MIXPANEL_CONVERSION
-        // );
-
         /** @var RequestService $requestService */
         $requestService = $this->get('app.request');
 
@@ -104,44 +97,12 @@ class DefaultController extends BaseController
             $trafficFraction = 1;
         }
 
-        // $replacement = $this->sixpack(
-        //     $request,
-        //     SixpackService::EXPERIMENT_PHONE_REPLACEMENT,
-        //     ['default', 'next-working-day', 'twentyfour-seventy-two'],
-        //     SixpackService::LOG_MIXPANEL_CONVERSION,
-        //     null,
-        //     $trafficFraction,
-        //     $force
-        // );
-
         $this->get('app.mixpanel')->queueTrackWithUtm(MixpanelService::EVENT_HOME_PAGE);
-
-        // $expContent = $this->getSessionSixpackTest(
-        //     $request,
-        //     SixpackService::EXPERIMENT_AB_CONTENT_HOMEPAGE,
-        //     ['old-homepage-copy', 'new-homepage-copy']
-        // );
-
-        // if ($this->get('app.request')->getDeviceCategory() == RequestService::DEVICE_CATEGORY_MOBILE) {
-        //     $expSearch = $this->sixpack(
-        //         $request,
-        //         SixpackService::EXPERIMENT_TEXT_VS_DROPDOWN_MOBILE,
-        //         ['dropdown-search-old', 'dropdown-search']
-        //     );
-        // } else {
-        //     $expSearch = $this->sixpack(
-        //         $request,
-        //         SixpackService::EXPERIMENT_TEXT_VS_DROPDOWN,
-        //         ['text-search', 'dropdown-search']
-        //     );
-        // }
 
         $data = array(
             // Make sure to check homepage landing below too
-            // 'replacement' => $replacement,
             'referral'    => $referral,
             'phone'       => $this->getQuerystringPhone($request),
-            // 'search_type' => $expSearch,
         );
 
         $template = 'AppBundle:Default:index.html.twig';
