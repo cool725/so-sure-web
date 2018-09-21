3c64d4b5c044960a197df2e0265cc98d489224c7
Alister Bulman <abulman@gmail.com>
2018-09-21 17:11:18 +0100

    make a specific map if allowed transitions from=>to
    plus some other checks

--- src/App/Controller/Admin/Policy/SetPolicyStatusByDescription.php
+++ src/App/Controller/Admin/Policy/SetPolicyStatusByDescription.php
@@ -4,10 +4,12 @@ namespace App\Controller\Admin\Policy;
 
 use AppBundle\Document\Policy;
 use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\DocumentNotFoundException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use UnexpectedValueException;
 
 /**
  * @Route("/admin/policy/new-status/{id}/{policyNewStatus}", name="admin_policy_set_status")
@@ -28,20 +30,34 @@ class SetPolicyStatusByDescription extends AbstractController
         $policyRepo = $this->documentManager->getRepository(Policy::class);
         /** @var Policy $policy */
         $policy = $policyRepo->find($id);
-
-        $allowedStatusMap = [
-            'active' => Policy::STATUS_ACTIVE,
-            'unpaid' => Policy::STATUS_UNPAID,
-        ];
-
-        if (!isset($allowedStatusMap[$policyNewStatus])) {
-            throw new \RuntimeException("Cannot set policy status to '{$policyNewStatus}'");
-        }
+        $this->assertTransitionAllowed($policy, $policyNewStatus);
 
         $policy->setStatus($policyNewStatus);
-        $this->documentManager->persist($policy);
         $this->documentManager->flush();
 
         return $this->redirectToRoute('admin_policy', ['id' => $policy->getId()]);
+    }
+
+    private function assertTransitionAllowed($policy, $requestedStatus)
+    {
+        if (!$policy) {
+            throw new DocumentNotFoundException("Policy not found");
+        }
+
+        if (!$this->transitionValid($policy, $requestedStatus)) {
+            throw new UnexpectedValueException("Cannot set policy status from the current value to '{$requestedStatus}'");
+        }
+    }
+
+    private function transitionValid(Policy $policy, string $requestedStatus): bool
+    {
+        $allowedTransitionsMap = [
+            Policy::STATUS_ACTIVE => Policy::STATUS_UNPAID,
+            Policy::STATUS_UNPAID => Policy::STATUS_ACTIVE,
+        ];
+
+        $currentStatus = $policy->getStatus();
+
+        return $allowedTransitionsMap[$currentStatus] === $requestedStatus;
     }
 }
