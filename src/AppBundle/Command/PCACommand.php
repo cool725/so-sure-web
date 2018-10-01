6c32c37da551e1a5653d728039b9a31c152a8880
Julien Champagne <julien@so-sure.com>
2018-10-01 14:21:30 +0100

    add command to query bank account via PCA

---
+++ src/AppBundle/Command/PCACommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Service\PCAService;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\Table;
+use AppBundle\Classes\Premium;
+
+class PCACommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('sosure:pca')
+            ->setDescription('Query bank account from the PCA provider and output the response.')
+            ->addArgument(
+                'sortcode',
+                InputArgument::REQUIRED,
+                'sortcode'
+            )
+            ->addArgument(
+                'account',
+                InputArgument::REQUIRED,
+                'account'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $sortcode = $input->getArgument('sortcode');
+        $accountNumber = $input->getArgument('account');
+        /** @var PCAService $pca */
+        $pca = $this->getContainer()->get('app.address');
+        $data = $pca->findBankAccountRequest($sortcode, $accountNumber);
+        $output->writeln(json_encode($data));
+    }
+}
