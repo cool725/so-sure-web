19b75e94c4d6be4df723d64939366a3406011deb
cool725 <109250241+cool725@users.noreply.github.com>
2018-10-01 10:37:07 +0100

    Split out cookies so can be hidden for the app

--- src/AppBundle/Resources/public/rebrand/js/global.js
+++ src/AppBundle/Resources/public/rebrand/js/global.js
@@ -2,30 +2,6 @@
 
 require('../sass/global.scss');
 
-// Cookies
-let CookieConsent = require('cookieconsent');
-
-document.addEventListener('DOMContentLoaded', () => {
-    cookieconsent.initialise({
-        container: document.getElementsByTagName('body'),
-        content: {
-            message: 'This website uses cookies to ensure you get the best experience on our website',
-            dismiss: 'Got it!',
-            href: "https://www.iubenda.com/privacy-policy/7805295/cookie-policy"
-        },
-        theme: "classic",
-        palette: {
-            popup: {
-                background: '#081B9A'
-            },
-            button: {
-                background: "#fff",
-                text: "#170B38"
-            }
-        }
-    });
-});
-
 // Require BS component(s)
 require('bootstrap/js/dist/alert');
 
