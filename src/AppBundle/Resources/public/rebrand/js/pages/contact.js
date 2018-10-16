aef912fb9a0b79b3dc79f9e31acfa4026172bf63
cool725 <109250241+cool725@users.noreply.github.com>
2018-10-16 14:17:32 +0100

    Adding form

---
+++ src/AppBundle/Resources/public/rebrand/js/pages/contact.js
@@ -0,0 +1,15 @@
+// contact.js
+
+require('../../sass/pages/contact.scss');
+
+// Require BS component(s)
+// require('bootstrap/js/dist/scrollspy');
+
+// Require components
+// require('../common/fixedWidth.js');
+
+$(function() {
+
+    // TODO: Add validation
+
+});
