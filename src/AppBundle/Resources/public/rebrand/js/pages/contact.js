175bf3b800389350a88a8bc17de433d1e9426585
cool725 <109250241+cool725@users.noreply.github.com>
2018-10-16 15:13:53 +0100

    Add validation

--- src/AppBundle/Resources/public/rebrand/js/pages/contact.js
+++ src/AppBundle/Resources/public/rebrand/js/pages/contact.js
@@ -11,8 +11,80 @@ require('../../../js/Default/jqueryValidatorMethods.js');
 
 const sosure = sosure || {};
 
+sosure.contactUs = (function() {
+    let self = {};
+    self.form = null;
+    self.isIE = null;
+
+    self.init = () => {
+        self.form = $('.validate-form');
+        self.isIE = !!navigator.userAgent.match(/Trident/g) || !!navigator.userAgent.match(/MSIE/g);
+        if (self.form.data('client-validation') && !self.isIE) {
+            self.addValidation();
+        }
+    }
+
+    self.addValidation = () => {
+        self.form.validate({
+            debug: false,
+            // When to validate
+            validClass: 'is-valid-ss',
+            errorClass: 'is-invalid',
+            onfocusout: false,
+            onkeyup: false,
+            // onclick: false,
+            rules: {
+                "contact_form[name]": {
+                    required: true
+                },
+                "contact_form[email]": {
+                    required: {
+                        depends:function(){
+                            $(this).val($.trim($(this).val()));
+                            return true;
+                        }
+                    },
+                    email: true,
+                    emaildomain: true
+                },
+                "contact_form[phone]": {
+                    required: {
+                        depends:function(){
+                            $(this).val($.trim($(this).val()));
+                            return true;
+                        }
+                    }
+                },
+                "contact_form[message]": {
+                    required: true
+                },
+            },
+            messages: {
+                "contact_form[name]": {
+                    required: 'Please enter your name'
+                },
+                "contact_form[email]": {
+                    required: 'Please enter your email'
+                },
+                "contact_form[phone]": {
+                    required: 'Please enter your phone number'
+                },
+                "contact_form[message]": {
+                    required: 'Please enter your message to us 🍆'
+                }
+            },
+
+            submitHandler: function(form) {
+                form.submit();
+            }
+        });
+    }
+
+    return self;
+})();
+
 $(function() {
 
-    // TODO: Add validation
+    sosure.contactUs.init();
 
 });
