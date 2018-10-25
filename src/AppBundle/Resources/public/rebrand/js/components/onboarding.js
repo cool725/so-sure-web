e484f93c7687f706f646fca39841283cba2985de
cool725 <109250241+cool725@users.noreply.github.com>
2018-10-25 13:23:53 +0100

    Adding content

---
+++ src/AppBundle/Resources/public/rebrand/js/components/onboarding.js
@@ -0,0 +1,32 @@
+// modalOnboarding.js
+
+$(function() {
+
+    // const carousel    = $('#onboarding-carousel'),
+    //       indicators  = $('.onboarding-indicators li'),
+    //       // Get the number of slides to set when to show the controls correctly
+    //       totalSlides = $('.carousel-item').length - 1;
+
+    // carousel.on('slide.bs.carousel', function(e){
+
+    //     // Adjust controls dependant on slide
+    //     if (e.to >= 1) {
+    //         $('#onboarding-prev-btn').removeClass('disabled');
+    //     } else {
+    //         $('#onboarding-prev-btn').addClass('disabled');
+    //     }
+
+    //     if (e.to == totalSlides) {
+    //         $('#onboarding-next-btn').addClass('hide-controls');
+    //         $('#onboarding-get-started-btn').removeClass('hide-controls');
+    //     } else {
+    //         $('#onboarding-next-btn').removeClass('hide-controls');
+    //         $('#onboarding-get-started-btn').addClass('hide-controls');
+    //     }
+
+    // });
+
+});
+
+
+
