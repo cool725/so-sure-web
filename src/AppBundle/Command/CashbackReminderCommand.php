65467c1e40aa6731a2f13416a447fef2757d5d45
Vallode <vallode@hotmail.co.uk>
2018-10-31 13:52:30 +0000

    Added tempalte for cashback email. Started command and command test

--- src/AppBundle/Command/CashbackReminderCommand.php
+++ src/AppBundle/Command/CashbackReminderCommand.php
@@ -2,7 +2,10 @@
 
 namespace AppBundle\Command;
 
+use AppBundle\Document\Cashback;
+use AppBundle\Service\MailerService;
 use AppBundle\Service\PolicyService;
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -14,6 +17,7 @@ use AppBundle\Document\Claim;
 use AppBundle\Document\Policy;
 use AppBundle\Document\PhonePolicy;
 use AppBundle\Classes\SoSure;
+use Symfony\Component\Templating\EngineInterface;
 
 class CashbackReminderCommand extends BaseCommand
 {
@@ -33,10 +37,43 @@ class CashbackReminderCommand extends BaseCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $dryRun = true === $input->getOption('dry-run');
-        /** @var PolicyService $policyService */
-        $policyService = $this->getContainer()->get('app.policy');
-        $lines = $policyService->cashbackReminder($dryRun);
-        $output->writeln(json_encode($lines, JSON_PRETTY_PRINT));
+        /** @var DocumentManager $dm */
+        $dm = $this->getManager();
+
+        /** @var MailerService $mailer */
+        $mailer = $this->getContainer()->get('app.mailer');
+
+        $debug = $input->getOption('dry-run');
+
+        $results = $dm->createQueryBuilder(Cashback::class)
+            ->field('status')
+            ->equals(Cashback::STATUS_PENDING_PAYMENT)
+            ->getQuery()
+            ->execute();
+
+        $policies = [];
+        foreach ($results as $result) {
+            $policies[] = sprintf("Policy %s with status %s is pending cashback payment", $result->getPolicy()->getPolicyNumber(), $result->getPolicy()->getStatus());
+        }
+
+        $mailer->sendTemplate(
+            'Biweekly cashback report',
+            ['dylan@so-sure.com', 'patrick@so-sure.com'],
+            'AppBundle:Email:policy/cashbackReminder.html.twig',
+            ['policies' => $policies]
+        );
+
+        if ($debug) {
+            /** @var EngineInterface $templating */
+            $templating = $this->getContainer()->get('templating');
+            $email = $templating->render(
+                'AppBundle:Email:policy/cashbackReminder.html.twig',
+                ['policies' => $policies]
+            );
+
+            $output->writeln($email);
+        }
+
+        $output->writeln(json_encode($results, JSON_PRETTY_PRINT));
     }
 }
---
+++ src/AppBundle/Tests/Command/CashbackReminderCommandTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace AppBundle\Tests\Command;
+
+use AppBundle\Command\OpsReportCommand;
+use AppBundle\Document\Cashback;
+use AppBundle\Document\CustomerCompany;
+use AppBundle\Document\Policy;
+use AppBundle\Document\Sanctions;
+use AppBundle\Listener\SanctionsListener;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use AppBundle\Tests\Controller\BaseControllerTest;
+use Symfony\Component\Console\Tester\CommandTester;
+use AppBundle\Document\User;
+
+/**
+ * @group functional-net
+ */
+class CashbackReminderCommandTest extends BaseControllerTest
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        /** @var DocumentManager $dm */
+        $dm = $this->getDocumentManager();
+        self::$dm = $dm;
+
+        /** @var PolicyService $policyService */
+        $policyService = self::$container->get('app.policy');
+        self::$policyService = $policyService;
+    }
+
+    public function callCommand($expectedOutput)
+    {
+        $application = new Application(self::$kernel);
+        $application->add(new OpsReportCommand());
+        $command = $application->find('sosure:cashback:reminder');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array(
+            'command' => $command->getName(),
+            '--dry-run',
+        ));
+        $output = $commandTester->getDisplay();
+        foreach ($expectedOutput as $item) {
+            $this->assertContains($item, $output);
+        }
+    }
+
+    public function testCashbackReminder()
+    {
+        $policy = self::createUserPolicy(true);
+        $policy->getUser()->setEmail(self::generateEmail('foobar', $this));
+        $policy->setStatus(Policy::STATUS_EXPIRED);
+
+        /** @var Cashback $cashback */
+        $cashback = new Cashback();
+        $cashback->setAccountName('foobar');
+        $cashback->setAccountNumber('12345678');
+        $cashback->setSortCode('123456');
+        $cashback->setStatus(Cashback::STATUS_PENDING_PAYMENT);
+
+        self::$policyService->cashback($policy, $cashback);
+
+        self::$dm->persist($cashback);
+        self::$dm->persist($policy->getUser());
+        self::$dm->persist($policy);
+        self::$dm->flush();
+
+        $this->callCommand(
+            [
+                ''
+            ]
+        );
+    }
+}
