7051dcb9793668ed999543de78acc95ea4f0f5b0
dalygbarron <daly@so-sure.com>
2018-10-31 15:44:51 +0000

    improved documentation and removed junk

--- src/AppBundle/Form/Type/PurchaseStepToJudoType.php
+++ src/AppBundle/Form/Type/PurchaseStepToJudoType.php
@@ -5,19 +5,10 @@ namespace AppBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\Extension\Core\Type\RadioType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use AppBundle\Document\Phone;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\Form\FormEvents;
-use Symfony\Component\Form\FormEvent;
 
+/**
+ * Implements a single button form used in the purchase flow to swap from bacs payment to Judo payment
+ */
 class PurchaseStepToJudoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
