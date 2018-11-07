ee2580992e9e26fc924306557488e432600b289d
Vallode <vallode@hotmail.co.uk>
2018-11-07 15:13:45 +0000

    Modal is inplace for the IMEI update

--- src/AppBundle/Resources/public/js/Admin/confirmModal.js
+++ src/AppBundle/Resources/public/js/Admin/confirmModal.js
@@ -1,7 +1,44 @@
 $('#confirmModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget); // Button that triggered the modal
+    var button = $(event.relatedTarget);
     var claim = button.data('confirm-msg');
+    var imei = button.data('imei');
+    var checkboxes = button.data('checkboxes');
     var modal = $(this);
 
     modal.find('#confirm-msg').text(claim);
+    modal.find('#imei').text(imei);
+    modal.find('#checkboxes').empty();
+
+    for ( var i = 0; i < checkboxes.length; i++) {
+        var current = checkboxes[i];
+
+        var checkbox = document.createElement('tr'),
+            td = document.createElement('td'),
+            input = document.createElement('input'),
+            label = document.createElement('label');
+
+        input.setAttribute('type', 'checkbox');
+
+        input.setAttribute('id', ('checkbox' + i));
+        label.setAttribute('for', ('checkbox' + i));
+
+        input.addEventListener('input', function (e) {
+            checkAll(checkboxes.length);
+        });
+
+        label.innerText = current;
+
+        td.append(input, label);
+        checkbox.append(td);
+
+        modal.find('#checkboxes').append(checkbox);
+    }
 });
+
+function checkAll(n) {
+    if ($('#checkboxes input:checked').length === n) {
+        $('#imei-update-button').attr('disabled', false)
+    } else {
+        $('#imei-update-button').attr('disabled', true)
+    }
+}
