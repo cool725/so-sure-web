17af351d45cee68857739bd421bd337c9ff837cf
dalygbarron <daly@so-sure.com>
2018-11-08 16:11:47 +0000

    removed banned function and typo

--- src/AppBundle/Document/ValidatorTrait.php
+++ src/AppBundle/Document/ValidatorTrait.php
@@ -12,20 +12,20 @@ trait ValidatorTrait
     {
         $validator = new AlphanumericSpaceDotPipeValidator();
         $string = $validator->conform(mb_substr($value, 0, $length));
-        return strlen($string) >= $minLength ? $string : nulll;
+        return mb_strlen($string) >= $minLength ? $string : null;
     }
 
     protected function conformAlphanumericSpaceDot($value, $length, $minLength = 0)
     {
         $validator = new AlphanumericSpaceDotValidator();
         $string = $validator->conform(mb_substr($value, 0, $length));
-        return strlen($string) >= $minLength ? $string : null;
+        return mb_strlen($string) >= $minLength ? $string : null;
     }
 
     protected function conformAlphanumeric($value, $length, $minLength = 0)
     {
         $validator = new AlphanumericValidator();
         $string = $validator->conform(mb_substr($value, 0, $length));
-        return strlen($string) >= $minLength ? $string : null;
+        return mb_strlen($string) >= $minLength ? $string : null;
     }
 }
