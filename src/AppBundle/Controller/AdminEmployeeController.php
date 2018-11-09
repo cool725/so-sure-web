e1a03735679084e7d4e3034c4c9d63490095bb79
Vallode <vallode@hotmail.co.uk>
2018-11-09 11:49:19 +0000

    Added dynamic routing so we can reuse confirmmodal

--- src/AppBundle/Controller/AdminEmployeeController.php
+++ src/AppBundle/Controller/AdminEmployeeController.php
@@ -505,11 +505,6 @@ class AdminEmployeeController extends BaseController implements ContainerAwareIn
         $noteForm = $this->get('form.factory')
             ->createNamedBuilder('note_form', NoteType::class)
             ->getForm();
-//        $imei = new Imei();
-//        $imei->setPolicy($policy);
-//        $imeiForm = $this->get('form.factory')
-//            ->createNamedBuilder('imei_form', ImeiType::class, $imei)
-//            ->getForm();
         $facebookForm = $this->get('form.factory')
             ->createNamedBuilder('facebook_form', FacebookType::class, $policy)
             ->getForm();
@@ -669,18 +664,6 @@ class AdminEmployeeController extends BaseController implements ContainerAwareIn
 
                     return $this->redirectToRoute('admin_policy', ['id' => $id]);
                 }
-//            } elseif ($request->request->has('imei_form')) {
-//                $imeiForm->handleRequest($request);
-//                if ($imeiForm->isValid()) {
-//                    $policy->adjustImei($imei->getImei(), false);
-//                    $dm->flush();
-//                    $this->addFlash(
-//                        'success',
-//                        sprintf('Policy %s imei updated.', $policy->getPolicyNumber())
-//                    );
-//
-//                    return $this->redirectToRoute('admin_policy', ['id' => $id]);
-//                }
             } elseif ($request->request->has('phone_form')) {
                 $phoneForm->handleRequest($request);
                 if ($phoneForm->isValid()) {
@@ -1191,7 +1174,6 @@ class AdminEmployeeController extends BaseController implements ContainerAwareIn
             'cancel_form' => $cancelForm->createView(),
             'pending_cancel_form' => $pendingCancelForm->createView(),
             'note_form' => $noteForm->createView(),
-//            'imei_form' => $imeiForm->createView(),
             'phone_form' => $phoneForm->createView(),
             'formClaimFlags' => $claimFlags->createView(),
             'facebook_form' => $facebookForm->createView(),
