e1fb8dbc1cc20bad94cb4222706bdf8e75e14700
Patrick McAndrew <patrick@urg.name>
2018-11-15 12:18:24 +0000

    improve indexes

--- src/AppBundle/Document/LogEntry.php
+++ src/AppBundle/Document/LogEntry.php
@@ -7,8 +7,9 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
- * @MongoDB\Document()
- * @MongoDB\Index(keys={"objectId"="asc"}, unique="false", sparse="true")
+ * @MongoDB\Document(repositoryClass="AppBundle\Repository\LogEntryRepository")
+ * @MongoDB\Index(keys={"objectId"="asc"}, sparse="true")
+ * @MongoDB\Index(keys={"objectClass"="asc", "objectId"="asc"}, sparse="true")
  */
 class LogEntry extends \Gedmo\Loggable\Document\LogEntry
 {
