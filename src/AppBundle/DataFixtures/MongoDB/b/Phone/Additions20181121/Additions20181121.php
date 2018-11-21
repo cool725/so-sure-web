afa66889fabda332f44d56b9b50cda9e89bbe04d
dalygbarron <daly@so-sure.com>
2018-11-21 14:35:26 +0000

    fixed old addition and added galaxy a7

--- src/AppBundle/DataFixtures/MongoDB/b/Phone/Additions20181121/Additions20181121.php
+++ src/AppBundle/DataFixtures/MongoDB/b/Phone/Additions20181121/Additions20181121.php
@@ -1,16 +1,16 @@
 <?php
 
-namespace AppBundle\DataFixtures\MongoDB\b\Phone\Additions20181120;
+namespace AppBundle\DataFixtures\MongoDB\b\Phone\Additions20181121;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use AppBundle\Document\Phone;
 use AppBundle\DataFixtures\MongoDB\b\Phone\LoadPhoneData;
 
-class Additions20181120 extends LoadPhoneData implements FixtureInterface
+class Additions20181121 extends LoadPhoneData implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $this->loadCsv($manager, '20181120.csv', new \DateTime('2018-11-20'));
+        $this->loadCsv($manager, '20181121.csv', new \DateTime('2018-11-21'));
     }
 }
