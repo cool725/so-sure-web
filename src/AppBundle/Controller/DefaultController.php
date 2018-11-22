8961ce86ef082f329349dfef2d9e0ce04e436d6d
cool725 <109250241+cool725@users.noreply.github.com>
2018-11-22 09:27:41 +0000

    Fix case for classname

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -100,8 +100,8 @@ class DefaultController extends BaseController
         // Start: Friday 23rd November Mighnight
         // End: Monday 26th November Mighnight
         $now   = \DateTime::createFromFormat('U', time());
-        $start = new \DateTime('2018-11-23 00:00:00', new \DateTimezone(SoSure::TIMEZONE));
-        $end   = new \DateTime('2018-11-26 23:59:59', new \DateTimezone(SoSure::TIMEZONE));
+        $start = new \DateTime('2018-11-23 00:00:00', new \DateTimeZone(SoSure::TIMEZONE));
+        $end   = new \DateTime('2018-11-26 23:59:59', new \DateTimeZone(SoSure::TIMEZONE));
 
         if ($now >= $start && $now <= $end) {
             $pageType = 'blackfriday';
