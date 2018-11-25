ffc0b9db8dc9128d303f4b98ed0f4e7bdf6a3801
Patrick McAndrew <patrick@urg.name>
2018-11-25 12:08:01 +0000

    display name/number

--- src/AppBundle/Resources/public/js/AdminEmployee/policies.js
+++ src/AppBundle/Resources/public/js/AdminEmployee/policies.js
@@ -3,7 +3,11 @@ $(function () {
         //console.log('modal');
         var button = $(event.relatedTarget);
         var policyId  = button.data('policy-id');
+        var name  = button.data('name');
+        var number  = button.data('number');
         //console.log(policyId);
         $('#call_form_policyId').val(policyId);
+        $('#call-form-name').html(name);
+        $('#call-form-number').html(number);
     });
 });
