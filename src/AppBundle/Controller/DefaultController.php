03befcadb961bf67aff0d3aee023b1f8a6990fe4
cool725 <109250241+cool725@users.noreply.github.com>
2018-11-28 10:11:44 +0000

    Fix error formatting wrong

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -144,7 +144,7 @@ class DefaultController extends BaseController
      */
     public function freePhoneCase()
     {
-        $this->get('app.mixpanel')->queueTrackWithUtm(MixpanelService::EVENT_HOME_PAGE ['page' => 'freephonecase']);
+        $this->get('app.mixpanel')->queueTrackWithUtm(MixpanelService::EVENT_HOME_PAGE, ['page' => 'freephonecase']);
 
         $pageType = 'phonecase';
 
@@ -162,7 +162,7 @@ class DefaultController extends BaseController
      */
     public function moneyLanding()
     {
-        $this->get('app.mixpanel')->queueTrackWithUtm(MixpanelService::EVENT_HOME_PAGE ['page' => 'money']);
+        $this->get('app.mixpanel')->queueTrackWithUtm(MixpanelService::EVENT_HOME_PAGE, ['page' => 'money']);
 
         return $this->render('AppBundle:Default:indexMoney.html.twig');
     }
@@ -172,7 +172,7 @@ class DefaultController extends BaseController
      */
     public function ivipLanding()
     {
-        $this->get('app.mixpanel')->queueTrackWithUtm(MixpanelService::EVENT_HOME_PAGE ['page' => 'ivip']);
+        $this->get('app.mixpanel')->queueTrackWithUtm(MixpanelService::EVENT_HOME_PAGE, ['page' => 'ivip']);
 
         return $this->render('AppBundle:Default:indexIVIP.html.twig');
     }
@@ -183,7 +183,7 @@ class DefaultController extends BaseController
      */
     public function starlingLanding(Request $request)
     {
-        $this->get('app.mixpanel')->queueTrackWithUtm(MixpanelService::EVENT_HOME_PAGE ['page' => 'starling']);
+        $this->get('app.mixpanel')->queueTrackWithUtm(MixpanelService::EVENT_HOME_PAGE, ['page' => 'starling']);
 
         $this->starlingOAuthSession($request);
 
@@ -195,6 +195,7 @@ class DefaultController extends BaseController
      */
     public function socialInsurance()
     {
+        $this->get('app.mixpanel')->queueTrackWithUtm(MixpanelService::EVENT_HOME_PAGE, ['page' => 'social-insurance']);
         return $this->render('AppBundle:Default:socialInsurance.html.twig');
     }
 
