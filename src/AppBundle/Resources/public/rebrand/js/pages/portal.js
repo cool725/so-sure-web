4df856112e66fb2dfa20f8b45c2acdfb1e36a2ec
cool725 <109250241+cool725@users.noreply.github.com>
2018-11-29 11:29:58 +0000

    Add js

--- src/AppBundle/Resources/public/rebrand/js/pages/portal.js
+++ src/AppBundle/Resources/public/rebrand/js/pages/portal.js
@@ -6,9 +6,62 @@ require('../../sass/pages/portal.scss');
 // require('bootstrap/js/dist/scrollspy');
 
 // Require components
+require('jquery-validation');
+require('../../../js/Default/jqueryValidatorMethods.js');
 
+const sosure = sosure || {};
+
+sosure.affPortal = (function() {
+    let self = {};
+    self.form = null;
+    self.isIE = null;
+
+    self.init = () => {
+        self.form = $('.validate-form');
+        self.isIE = !!navigator.userAgent.match(/Trident/g) || !!navigator.userAgent.match(/MSIE/g);
+        if (self.form.data('client-validation') && !self.isIE) {
+            self.addValidation();
+        }
+    }
+
+    self.addValidation = () => {
+        self.form.validate({
+            debug: true,
+            // When to validate
+            validClass: 'is-valid-ss',
+            errorClass: 'is-invalid',
+            onfocusout: false,
+            onkeyup: false,
+            // onclick: false,
+            rules: {
+
+            },
+            messages: {
+                // Leave validation messages blank as class gets added to the label
+                // "purchase_form[agreedDamage]": {
+                //     required: ''
+                // },
+                // "purchase_form[agreedAgeLocation]": {
+                //     required: ''
+                // },
+                // "purchase_form[agreedExcess]": {
+                //     required: ''
+                // },
+                // "purchase_form[agreedTerms]": {
+                //     required: ''
+                // }
+            },
+
+            submitHandler: function(form) {
+                form.submit();
+            }
+        });
+    }
+
+    return self;
+})();
 
 $(function() {
-
+    sosure.affPortal.init();
 
 });
