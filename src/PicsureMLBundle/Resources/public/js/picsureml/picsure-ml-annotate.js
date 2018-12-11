6b9c2cd533f067c803f9e83e7f1e19dffe607cdc
Julien Champagne <julien@Juliens-MacBook-Pro.local>
2018-12-11 11:39:09 +0000

    add for detection on training data

--- src/PicsureMLBundle/Resources/public/js/picsureml/picsure-ml-annotate.js
+++ src/PicsureMLBundle/Resources/public/js/picsureml/picsure-ml-annotate.js
@@ -1,9 +1,9 @@
 $(function() {
 
     $('#annotate').on('load', function(){
-        if ($('#picsureml_annotate_form_x').val() && $('#picsureml_annotate_form_y').val() && $('#picsureml_annotate_form_width').val() && $('#picsureml_annotate_form_height').val()) {
+        if ($('#picsureml_edit_form_x').val() && $('#picsureml_edit_form_y').val() && $('#picsureml_edit_form_width').val() && $('#picsureml_edit_form_height').val()) {
             $('#annotate').Jcrop({
-                setSelect: [parseInt($('#picsureml_annotate_form_x').val()), parseInt($('#picsureml_annotate_form_y').val()), parseInt($('#picsureml_annotate_form_width').val()), parseInt($('#picsureml_annotate_form_height').val())],
+                setSelect: [parseInt($('#picsureml_edit_form_x').val()), parseInt($('#picsureml_edit_form_y').val()), parseInt($('#picsureml_edit_form_width').val()), parseInt($('#picsureml_edit_form_height').val())],
                 boxWidth: 400
             });
         }
@@ -15,22 +15,22 @@ $(function() {
     });
 
     $('#crop-interface').on('cropstart cropmove cropend', function(e,s,c){
-      $('#picsureml_annotate_form_x').val(parseInt(c.x));
-      $('#picsureml_annotate_form_y').val(parseInt(c.y));
-      $('#picsureml_annotate_form_width').val(parseInt(c.w));
-      $('#picsureml_annotate_form_height').val(parseInt(c.h));
+      $('#picsureml_edit_form_x').val(parseInt(c.x));
+      $('#picsureml_edit_form_y').val(parseInt(c.y));
+      $('#picsureml_edit_form_width').val(parseInt(c.w));
+      $('#picsureml_edit_form_height').val(parseInt(c.h));
     });
 
     if ($('#annotate')[0].complete) {
         $('#annotate').load();
     }
 
-    $('#picsureml_annotate_form_clear').on('click', function(){
-      $('#picsureml_annotate_form_x').val('');
-      $('#picsureml_annotate_form_y').val('');
-      $('#picsureml_annotate_form_width').val('');
-      $('#picsureml_annotate_form_height').val('');
-      $('form[name="picsureml_annotate_form"]').submit();
+    $('#picsureml_edit_form_clear').on('click', function(){
+      $('#picsureml_edit_form_x').val('');
+      $('#picsureml_edit_form_y').val('');
+      $('#picsureml_edit_form_width').val('');
+      $('#picsureml_edit_form_height').val('');
+      $('form[name="picsureml_edit_form"]').submit();
     })
 
 });
