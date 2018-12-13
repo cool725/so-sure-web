1ae13f7cc60af7be2dfba65195c5f08f05805da6
Vallode <vallode@hotmail.co.uk>
2018-12-13 14:59:47 +0000

    Fix issue with newlines and other special characters breaking form submission

--- src/AppBundle/Form/Type/ImeiType.php
+++ src/AppBundle/Form/Type/ImeiType.php
@@ -5,10 +5,13 @@ namespace AppBundle\Form\Type;
 use AppBundle\Repository\PhoneRepository;
 use Doctrine\Bundle\MongoDBBundle\Form\Type\DocumentType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ImeiType extends AbstractType
@@ -27,10 +30,25 @@ class ImeiType extends AbstractType
                 'class' => 'AppBundle:Phone',
                 'query_builder' => function (PhoneRepository $dr) {
                     return $dr->findActive();
-                }
+                },
+                'required' => false
             ])
-            ->add('note', TextareaType::class)
+            ->add('note', TextareaType::class, [
+                'trim' => true
+            ])
             ->add('update', SubmitType::class)
+        ;
+
+        $builder
+            ->get('note')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($note) {
+                    return $note;
+                },
+                function ($noteAlphanumeric) {
+                    return preg_replace("/[^A-Za-z0-9 ]/", '', $noteAlphanumeric);
+                }
+            ))
         ;
     }
 
