fc6619f33b83ee85a55e575db2dcf73d13672a72
Patrick McAndrew <patrick@urg.name>
2018-12-20 07:44:17 +0000

    fix phpcs

--- src/AppBundle/Service/BacsService.php
+++ src/AppBundle/Service/BacsService.php
@@ -1173,6 +1173,7 @@ class BacsService
     public function bacsFileSubmittedBySerialNumber($serialNumber)
     {
         $repo = $this->dm->getRepository(AccessPayFile::class);
+        /** @var AccessPayFile $file */
         $file = $repo->findOneBy(['serialNumber' => $serialNumber, 'status' => AccessPayFile::STATUS_PENDING]);
         if (!$file) {
             return false;
