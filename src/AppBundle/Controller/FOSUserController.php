efab4509dac6adcf5411c10fa3bd3b73ef2ba9c4
Vallode <vallode@hotmail.co.uk>
2018-12-21 10:24:48 +0000

    No need to reuse code, can use parent

--- src/AppBundle/Controller/FOSUserController.php
+++ src/AppBundle/Controller/FOSUserController.php
@@ -103,7 +103,7 @@ class FOSUserController extends ResettingController
     }
 
     /**
-     * Send reset password email
+     * check for valid csrf and send reset email
      */
     public function sendEmailAction(Request $request)
     {
@@ -111,60 +111,6 @@ class FOSUserController extends ResettingController
             throw new \InvalidArgumentException('Invalid csrf token');
         }
 
-        $username = $request->request->get('username');
-
-        /** @var UserInterface $user */
-        $user = $this->get('fos_user.user_manager')->findUserByUsernameOrEmail($username);
-        /** @var EventDispatcherInterface $dispatcher */
-        $dispatcher = $this->get('event_dispatcher');
-
-        /* Dispatch init event */
-        $event = new GetResponseNullableUserEvent($user, $request);
-        $dispatcher->dispatch(FOSUserEvents::RESETTING_SEND_EMAIL_INITIALIZE, $event);
-
-        if (null !== $event->getResponse()) {
-            return $event->getResponse();
-        }
-
-        $ttl = $this->container->getParameter('fos_user.resetting.retry_ttl');
-
-        if (null != $user && !$user->isPasswordRequestNonExpired($ttl)) {
-            $event = new GetResponseUserEvent($user, $request);
-            $dispatcher->dispatch(FOSUserEvents::RESETTING_RESET_REQUEST, $event);
-
-            if (null !== $event->getResponse()) {
-                return $event->getResponse();
-            }
-
-            if (null == $user->getConfirmationToken()) {
-                /** @var TokenGeneratorInterface $tokenGenerator */
-                $tokenGenerator = $this->get('fos_user.util.token_generator');
-                $user->setConfirmationToken($tokenGenerator->generateToken());
-            }
-
-            /* Dispatch confirm event */
-            $event = new GetResponseUserEvent($user, $request);
-            $dispatcher->dispatch(FOSUserEvents::RESETTING_SEND_EMAIL_CONFIRM, $event);
-
-            if (null !== $event->getResponse()) {
-                return $event->getResponse();
-            }
-
-            $this->get('fos_user.mailer')->sendResettingEmailMessage($user);
-            $user->setPasswordRequestedAt(new \DateTime());
-            $this->get('fos_user.user_manager')->updateUser($user);
-
-            /* Dispatch completed event */
-            $event = new GetResponseUserEvent($user, $request);
-            $dispatcher->dispatch(FOSUserEvents::RESETTING_SEND_EMAIL_COMPLETED, $event);
-
-            if (null !== $event->getResponse()) {
-                return $event->getResponse();
-            }
-        }
-
-        return new RedirectResponse($this->generateUrl('fos_user_resetting_check_email', array(
-            'username' => $username
-        )));
+        return parent::sendEmailAction($request);
     }
 }
