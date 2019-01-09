ded107c17568055429d33f8aacc3fd4f86430b7c
cool725 <109250241+cool725@users.noreply.github.com>
2019-01-09 09:50:53 +0000

    Add tabs and search for banking

--- src/AppBundle/Resources/public/rebrand/js/pages/admin/admin.js
+++ src/AppBundle/Resources/public/rebrand/js/pages/admin/admin.js
@@ -4,6 +4,8 @@ require('../../../sass/pages/admin.scss');
 
 // Require BS component(s)
 require('bootstrap/js/dist/tooltip');
+require('bootstrap/js/dist/dropdown');
+require('bootstrap/js/dist/tab');
 
 // Require components
 // require('tempusdominus-bootstrap-4');
