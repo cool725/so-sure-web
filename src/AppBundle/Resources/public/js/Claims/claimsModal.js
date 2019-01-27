a8ea5f59619aa19e17056f95ff69d05d061b7769
cool725 <109250241+cool725@users.noreply.github.com>
2019-01-27 05:39:28 +0000

    Add check for data attribute being defined (#5141)

--- src/AppBundle/Resources/public/js/Claims/claimsModal.js
+++ src/AppBundle/Resources/public/js/Claims/claimsModal.js
@@ -12,115 +12,120 @@ function selectAll(selectBox, selectAll) {
     }
 }
 
-$(document).ready(function () {
+$(function () {
+
     $('#claim_search_status-all').on("click", function () {
         selectAll(document.getElementById('claim_search_status'), true)
     });
     $('#claim_search_status-none').on("click", function () {
         selectAll(document.getElementById('claim_search_status'), false)
     });
-});
-var clipboard = new Clipboard('.btn-copy');
 
-$('.btn-copy').tooltip({
-    'title': 'Copied',
-    'trigger': 'manual'
-});
+    var clipboard = new Clipboard('.btn-copy');
 
-$('.btn-copy').click(function (e) {
-    e.preventDefault();
-});
-
-clipboard.on('success', function (event) {
-    $('.btn-copy').tooltip('show');
-    setTimeout(function () {
-        $('.btn-copy').tooltip('hide');
-    }, 1500);
-});
-
-var ajax;
-
-$('#claimsModal').on('hide.bs.modal', function (event) {
-    var modal = $(this);
-
-    ajax.abort();
-
-    modal.find('.modal-content').html(
-        '<div class="modal-body">\
-            <div class="container-fluid">\
-                <div class="row text-center">\
-                    <i class="fa fa-spinner fa-pulse fa-5x fa-fw"></i>\
-                </div>\
-                <div class="row text-center">\
-                    <h3>Loading...</h3>\
-                </div>\
-            </div>\
-        </div>'
-    );
-});
-
-$('#claimsModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    var modal = $(this);
-
-    ajax = $.ajax({
-        url: button.data('route'),
-        type: "GET"
+    $('.btn-copy').tooltip({
+        'title': 'Copied',
+        'trigger': 'manual'
     });
 
-    ajax.success(function (form) {
-        modal.find('.modal-content').html(form);
-
-        modal.find('#claims_form_approvedDate').datetimepicker({
-            format: 'YYYY-MM-DD'
-        });
-
-        modal.find("#delete-claim").click(function(){
-            if (confirm('Are you sure you want to delete this claim?')) {
-                $("#delete-claim-form").submit();
-            }
-        });
-
-        modal.find("#process-claim").click(function(){
-            if (confirm('Are you sure you want to (re)-process this claim? Pot values will be updated and emails will be sent to user & connections.')) {
-                $("#process-claim-form").submit();
-            }
-        });
-
-        modal.find('.set-replacement-phone').click(function() {
-            var id = $('.set-replacement-phone').data('id');
-            $('#claims_form_replacementPhone').val(id).prop('selected', true);;
-        });
-
-        modal.find('.img-preview').viewer({
-            inline: false,
-            navbar: false
-        });
-
-        modal.find('.set-imei').click(function() {
-            var imei = $('#claims_form_replacementImei').val();
-            //console.log(imei);
-            $('#imei_form_imei').val(imei);
-            $('#claimsModal').modal('hide');
-            setTimeout(function() {
-                $('#imeiForm').modal('show');
-            }, 500);
-        });
+    $('.btn-copy').click(function (e) {
+        e.preventDefault();
     });
-});
 
-$('#flagsModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) // Button that triggered the modal
-    var flags = button.data('flags');
-    var update = button.data('update');
-    var modal = $(this);
-    if (flags) {
-        $('form[name="claimflags"]').attr('action', update);
-        $.each(flags, function (item) {
-            $('#claimflags_ignoreWarningFlags').find($("input[value='" + item + "']")).prop('checked', flags[item]);
-        });
-    } else {
-        $('#claimFlags').attr('action', null);
-        $('#claimflags_ignoreWarningFlags').find($("input[value='" + item + "']")).prop('checked', false);
-    }
+    clipboard.on('success', function (event) {
+        $('.btn-copy').tooltip('show');
+        setTimeout(function () {
+            $('.btn-copy').tooltip('hide');
+        }, 1500);
+    });
+
+    var ajax;
+
+    $('#claimsModal').on('hide.bs.modal', function (event) {
+        var modal = $(this);
+
+        ajax.abort();
+
+        modal.find('.modal-content').html(
+            '<div class="modal-body">\
+                <div class="container-fluid">\
+                    <div class="row text-center">\
+                        <i class="fa fa-spinner fa-pulse fa-5x fa-fw"></i>\
+                    </div>\
+                    <div class="row text-center">\
+                        <h3>Loading...</h3>\
+                    </div>\
+                </div>\
+            </div>'
+        );
+    });
+
+    $('#claimsModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        var modal = $(this);
+
+        if (button.data('route')) {
+            ajax = $.ajax({
+                url: button.data('route'),
+                type: "GET"
+            });
+
+            ajax.success(function (form) {
+                modal.find('.modal-content').html(form);
+
+                modal.find('#claims_form_approvedDate').datetimepicker({
+                    format: 'YYYY-MM-DD'
+                });
+
+                modal.find("#delete-claim").click(function(){
+                    if (confirm('Are you sure you want to delete this claim?')) {
+                        $("#delete-claim-form").submit();
+                    }
+                });
+
+                modal.find("#process-claim").click(function(){
+                    if (confirm('Are you sure you want to (re)-process this claim? Pot values will be updated and emails will be sent to user & connections.')) {
+                        $("#process-claim-form").submit();
+                    }
+                });
+
+                modal.find('.set-replacement-phone').click(function() {
+                    var id = $('.set-replacement-phone').data('id');
+                    $('#claims_form_replacementPhone').val(id).prop('selected', true);;
+                });
+
+                modal.find('.img-preview').viewer({
+                    inline: false,
+                    navbar: false
+                });
+
+                modal.find('.set-imei').click(function() {
+                    var imei = $('#claims_form_replacementImei').val();
+                    //console.log(imei);
+                    $('#imei_form_imei').val(imei);
+                    $('#claimsModal').modal('hide');
+                    setTimeout(function() {
+                        $('#imeiForm').modal('show');
+                    }, 500);
+                });
+            });
+        }
+    });
+
+    $('#flagsModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var flags = button.data('flags');
+        var update = button.data('update');
+        var modal = $(this);
+        if (flags) {
+            $('form[name="claimflags"]').attr('action', update);
+            $.each(flags, function (item) {
+                $('#claimflags_ignoreWarningFlags').find($("input[value='" + item + "']")).prop('checked', flags[item]);
+            });
+        } else {
+            $('#claimFlags').attr('action', null);
+            $('#claimflags_ignoreWarningFlags').find($("input[value='" + item + "']")).prop('checked', false);
+        }
+    });
+
 });
