ef84d5030dec54767ee500f6a9ed799b83aff41f
cool725 <109250241+cool725@users.noreply.github.com>
2019-02-07 09:58:49 +0000

    Update regex

--- src/AppBundle/Resources/public/rebrand/js/common/validationMethods.js
+++ src/AppBundle/Resources/public/rebrand/js/common/validationMethods.js
@@ -26,7 +26,7 @@ $(function(){
 
         value = removeAccents(value);
 
-        return this.optional(element) || value.match(/^[a-zA-Z]+(([ -][a-zA-Z ])?[a-zA-Z]*)*$/);
+        return this.optional(element) || value.match(/^[a-zA-Z]+\s[a-zA-Z]+\s?$/);
 
     }, 'Please enter your full name');
 
