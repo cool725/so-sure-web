275a0231e49cda024c7ea7b53b7fd5bf9c152d1b
Patrick McAndrew <urg@users.noreply.github.com>
2019-02-14 09:56:50 +0000

    add feature flag for app (#5332)

--- src/AppBundle/Document/Feature.php
+++ src/AppBundle/Document/Feature.php
@@ -24,6 +24,7 @@ class Feature
     const FEATURE_DAVIES_IMPORT_ERROR_EMAIL = 'davies-import-error-email';
     const FEATURE_BACS = 'bacs';
     const FEATURE_CARD_OPTION_WITH_BACS = 'card-option-with-bacs';
+    const FEATURE_CARD_SWAP_FROM_BACS = 'card-swap-from-bacs';
     const FEATURE_APP_IOS_IMEI_VALIDATION_COLOUR = 'app-ios-imei-validation-colour';
     const FEATURE_APP_PICSURE_ACCELEROMETER = 'app-picsure-accelerometer';
     const FEATURE_APP_PICSURE_DOTCODE = 'app-picsure-dotcode';
@@ -50,7 +51,8 @@ class Feature
         self::FEATURE_CLAIMS_DEFAULT_DIRECT_GROUP,
         self::FEATURE_RATE_LIMITING,
         self::FEATURE_PAYMENTS_BCC,
-        self::FEATURE_JUDO_RECURRING
+        self::FEATURE_JUDO_RECURRING,
+        self::FEATURE_CARD_SWAP_FROM_BACS,
     ];
 
     // @codingStandardsIgnoreStart
@@ -63,6 +65,7 @@ class Feature
         self::FEATURE_SALVA_POLICY_UPDATE => 'Use salva update api call instead of cancel/create',
         self::FEATURE_BACS => 'Bacs functionality - too integrated to turn off',
         self::FEATURE_CARD_OPTION_WITH_BACS => 'Allow users to also pay by card in web purchase flow',
+        self::FEATURE_CARD_SWAP_FROM_BACS => 'Allow user to swap from bacs to card',
         self::FEATURE_APP_IOS_IMEI_VALIDATION_COLOUR => 'Check the colour of the top left pixel in the screenshot and if it is too different from the iOS header colour it will return an error as an anti-fraud measure.',
         self::FEATURE_APP_PICSURE_ACCELEROMETER => 'If enabled it will use the accelerometer data to check any sudden movements while taking the picsure and prevent doing picsure if any are detected as an anti-fraud measure.',
         self::FEATURE_APP_PICSURE_DOTCODE => 'Display dotcode on iOS for on the background image. Allows us to validate the imei in cases of suspected hacking.',
