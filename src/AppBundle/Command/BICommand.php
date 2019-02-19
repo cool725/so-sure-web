ffda253ead40da4c829375c01a8bae44370c12a0
Patrick McAndrew <urg@users.noreply.github.com>
2019-02-19 14:49:05 +0000

    try array (#5357)

--- src/AppBundle/Command/BICommand.php
+++ src/AppBundle/Command/BICommand.php
@@ -288,7 +288,7 @@ class BICommand extends ContainerAwareCommand
     {
         /** @var PhonePolicyRepository $repo */
         $repo = $this->dm->getRepository(PhonePolicy::class);
-        $policies = $repo->findAllStartedPolicies($prefix, new \DateTime(SoSure::POLICY_START));
+        $policies = $repo->findAllStartedPolicies($prefix, new \DateTime(SoSure::POLICY_START))->toArray();
         $lines = [];
         $lines[] = implode(',', [
             '"Policy Number"',
