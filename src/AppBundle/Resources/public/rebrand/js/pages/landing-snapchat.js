a2f81396b447d597758ed3fff7262992689327e5
cool725 <109250241+cool725@users.noreply.github.com>
2019-05-24 16:07:37 +0100

    nickwaller/ch15239/trustpilot-toasts-desktop-site (#5654)
    * Add toasts to homepage
    
    * Adjust timing
    
    * Add positioning
    
    * Rotate toast and add review mix
    
    * Tweak timings
    
    * Wrong avatar for male and remove close as doesn't work as expected
    
    * Add toast to landing pages

--- src/AppBundle/Resources/public/rebrand/js/pages/landing-snapchat.js
+++ src/AppBundle/Resources/public/rebrand/js/pages/landing-snapchat.js
@@ -4,6 +4,8 @@ require('../../sass/pages/landing-snapchat.scss');
 
 // Require BS component(s)
 require('bootstrap/js/dist/carousel');
+require('bootstrap/js/dist/toast');
+require('bootstrap/js/dist/util');
 
 // Require components
 require('../components/table.js');
@@ -23,5 +25,33 @@ $(function() {
         minFontSize: 40,
         maxFontSize: 60
     });
+
+    setTimeout(function() {
+        $('.toast-1').toast('show');
+    }, 2000);
+
+    $('.toast-1').on('hidden.bs.toast', function () {
+        setTimeout(function() {
+            $('.toast-2').toast('show');
+        }, 200);
+    })
+
+    $('.toast-2').on('hidden.bs.toast', function () {
+        setTimeout(function() {
+            $('.toast-3').toast('show');
+        }, 200);
+    })
+
+    $('.toast-3').on('hidden.bs.toast', function () {
+        setTimeout(function() {
+            $('.toast-4').toast('show');
+        }, 200);
+    })
+
+    $('.toast-4').on('hidden.bs.toast', function () {
+        setTimeout(function() {
+            $('.toast-5').toast('show');
+        }, 200);
+    })
 });
 
