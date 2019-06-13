f681a8f96c507df6d62ce8198f8586382d253ffc
Daly Barron <daly@so-sure.com>
2019-06-13 15:35:36 +0100

    setting commission in arudd

--- src/AppBundle/Service/BacsService.php
+++ src/AppBundle/Service/BacsService.php
@@ -850,7 +850,7 @@ class BacsService
                     // refund requires commission to be set, but probably isn't at this point in time
                     if (!$submittedPayment->getTotalCommission()) {
                         try {
-                            $submittedPayment->setCommission();
+                            $submittedPayment->setCommission(true);
                         } catch (\Exception $e) {
                             $this->logger->error(sprintf(
                                 'Unable to set refund commission for policy %s',
@@ -858,7 +858,7 @@ class BacsService
                             ), ['exception' => $e]);
                         }
                     }
-                    $debitPayment->setRefundTotalCommission($submittedPayment->getTotalCommission());
+                    $debitPayment->setRefundCommission($submittedPayment->getTotalCommission());
                     $debitPayment->calculateSplit();
                     $submittedPayment->addReverse($debitPayment);
                     $submittedPayment->approve($currentProcessingDate, true, false);
