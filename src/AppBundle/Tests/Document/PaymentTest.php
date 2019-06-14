fb5b0cd3cc504a927afc37098099bf7f17eccccb
Blake "Badger" Payne <blake@so-sure.com>
2019-06-14 10:23:01 +0100

    moved functional tests to the functional test file and set the unit test file back to unit tests

--- src/AppBundle/Tests/Document/PaymentTest.php
+++ src/AppBundle/Tests/Document/PaymentTest.php
@@ -3,90 +3,27 @@
 namespace AppBundle\Tests\Document;
 
 use AppBundle\Classes\Salva;
-use AppBundle\Document\Address;
-use AppBundle\Document\DateTrait;
-use AppBundle\Document\Payment\CheckoutPayment;
 use AppBundle\Document\Payment\Payment;
+use AppBundle\Document\PhonePolicy;
 use AppBundle\Document\PhonePremium;
 use AppBundle\Document\PhonePrice;
-use AppBundle\Document\User;
-use AppBundle\Exception\CommissionException;
-use AppBundle\Repository\UserRepository;
-use AppBundle\Service\CheckoutService;
-use AppBundle\Service\PolicyService;
-use AppBundle\Tests\UserClassTrait;
-use Doctrine\Common\Persistence\ObjectRepository;
-use Doctrine\ODM\MongoDB\DocumentManager;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use AppBundle\Document\SalvaPhonePolicy;
+use AppBundle\Document\Payment\CheckoutPayment;
+use AppBundle\Document\CurrencyTrait;
 
 /**
  * @group unit
  */
-class PaymentTest extends WebTestCase
+class PaymentTest extends \PHPUnit\Framework\TestCase
 {
-    use UserClassTrait;
-    use DateTrait;
-
-    protected static $container;
-    /** @var DocumentManager */
-    protected static $dm;
-    /** @var CheckoutService */
-    protected static $checkout;
-    protected static $userRepo;
-    /** @var User */
-    protected static $user;
-    protected static $policyService;
+    use CurrencyTrait;
 
     public static function setUpBeforeClass()
     {
-        //start the symfony kernel
-        $kernel = static::createKernel();
-        $kernel->boot();
-        //get the DI container
-        static::$container = $kernel->getContainer();
-        //now we can instantiate our service (if you want a fresh one for
-        //each test method, do this in setUp() instead
-        /** @var DocumentManager */
-        $dm = static::$container->get('doctrine_mongodb.odm.default_document_manager');
-        static::$dm = $dm;
-        static::$userRepo = static::$dm->getRepository(User::class);
-        static::$userManager = static::$container->get('fos_user.user_manager');
-        static::$policyService = static::$container->get('app.policy');
-        /** @var CheckoutService $checkout */
-        $checkout = static::$container->get('app.checkout');
-        static::$checkout = $checkout;
-        static::$user = static::createValidUser(
-            static::generateEmail(
-                'cancelledpaymentstestuser',
-                static::returnSelf(),
-                true
-            )
-        );
     }
 
     public function tearDown()
     {
-    }
-
-    private static function createValidUser($email)
-    {
-        /** @var User $user */
-        $user = static::createUser(static::$userManager, $email, 'foo');
-        $mobile = static::generateRandomMobile();
-
-        $user->setFirstName('foo');
-        $user->setLastName('bar');
-        $user->setMobileNumber($mobile);
-        $user->setBirthday(new \DateTime('1980-01-01'));
-        $address = new Address();
-        $address->setType(Address::TYPE_BILLING);
-        $address->setLine1('5 Martin Lane');
-        $address->setCity('London');
-        $address->setPostcode('EC4R 0DP');
-        $user->setBillingAddress($address);
-        static::$dm->persist($address);
-        static::$dm->flush();
-        return $user;
     }
 
     public function testCalculatePremium()
@@ -96,12 +33,7 @@ class PaymentTest extends WebTestCase
         $date = new \DateTime('2016-05-01');
         $premium = $phonePrice->createPremium(null, $date);
 
-        $phonePolicy = static::initPolicy(
-            static::$user,
-            static::$dm,
-            static::getRandomPhone(static::$dm)
-        );
-
+        $phonePolicy = new SalvaPhonePolicy();
         $phonePolicy->setPremium($premium);
 
         $payment = new CheckoutPayment();
@@ -151,11 +83,7 @@ class PaymentTest extends WebTestCase
 
     public function testSetCommissionMonthly()
     {
-        $policy = static::initPolicy(
-            static::$user,
-            static::$dm,
-            static::getRandomPhone(static::$dm)
-        );
+        $policy = new PhonePolicy();
 
         $premium = new PhonePremium();
         $premium->setIptRate(0.12);
@@ -188,11 +116,7 @@ class PaymentTest extends WebTestCase
 
     public function testSetCommissionYearly()
     {
-        $policy = static::initPolicy(
-            static::$user,
-            static::$dm,
-            static::getRandomPhone(static::$dm)
-        );
+        $policy = new PhonePolicy();
 
         $premium = new PhonePremium();
         $premium->setIptRate(0.12);
@@ -211,72 +135,11 @@ class PaymentTest extends WebTestCase
     }
 
     /**
-     * @expectedException \AppBundle\Exception\CommissionException
-     */
-    public function testSetCommissionRemainderFailsWithFalse()
-    {
-        $policy = static::initPolicy(
-            static::$user,
-            static::$dm,
-            static::getRandomPhone(static::$dm)
-        );
-
-        $premium = new PhonePremium();
-        $premium->setIptRate(0.12);
-        $premium->setGwp(5);
-        $premium->setIpt(1);
-        $policy->setPremium($premium);
-
-        $payment = new CheckoutPayment();
-        $payment->setAmount($premium->getMonthlyPremiumPrice() * 0.5);
-
-        $policy->addPayment($payment);
-
-        $payment->setCommission(false);
-    }
-
-    public function testSetCommissionRemainderDoesNotFailWithTrue()
-    {
-        $now = new \DateTime();
-
-        $policy = static::initPolicy(
-            static::$user,
-            static::$dm,
-            static::getRandomPhone(static::$dm),
-            $now,
-            false,
-            true,
-            true
-        );
-
-        $premium = new PhonePremium();
-        $premium->setIptRate(0.12);
-        $premium->setGwp(5);
-        $premium->setIpt(1);
-        $policy->setPremium($premium);
-
-        $payment = new CheckoutPayment();
-        $payment->setAmount($premium->getMonthlyPremiumPrice() * 0.5);
-
-        $policy->addPayment($payment);
-
-        $payment->setCommission(true);
-
-        $commission = $payment->getTotalCommission();
-        $this->assertGreaterThan(0, $commission);
-        static::assertLessThan(Salva::MONTHLY_TOTAL_COMMISSION, $commission);
-    }
-
-    /**
      * @expectedException \Exception
      */
     public function testSetCommissionUnknown()
     {
-        $policy = static::initPolicy(
-            static::$user,
-            static::$dm,
-            static::getRandomPhone(static::$dm)
-        );
+        $policy = new PhonePolicy();
 
         $premium = new PhonePremium();
         $premium->setIptRate(0.12);
