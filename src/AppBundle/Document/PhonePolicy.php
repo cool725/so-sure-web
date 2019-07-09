5fb93878e5b13398a29e1ed96a5a58dd127f7282
Daly Barron <daly@so-sure.com>
2019-07-09 16:21:50 +0100

    add excesses to phone policy api

--- src/AppBundle/Document/PhonePolicy.php
+++ src/AppBundle/Document/PhonePolicy.php
@@ -959,7 +959,9 @@ class PhonePolicy extends Policy
     {
         $picSureEnabled = $this->isPicSurePolicy();
         $picSureValidated = $this->isPicSureValidated();
-
+        $phonePremium = $this->getPremium();
+        $excess = $phonePremium->getExcess();
+        $picsureExcess = $phonePremium->getPicsureExcess();
         return array_merge(parent::toApiArray(), [
             'phone_policy' => [
                 'imei' => $this->getImei(),
@@ -969,6 +971,8 @@ class PhonePolicy extends Policy
                     $this->getDefaultName(),
                 'picsure_status' => $this->getPicSureStatusForApi(),
                 'excesses' => $this->getCurrentExcess() ? $this->getCurrentExcess()->toApiArray() : [],
+                'expected_excesses' => $excess ? $excess->toApiArray() : [],
+                'expected_picsure_excesses' => $picsureExcess() ? $picsureExcess->toApiArray() : [],
                 'detected_imei' => $this->getDetectedImei(),
             ],
         ]);
