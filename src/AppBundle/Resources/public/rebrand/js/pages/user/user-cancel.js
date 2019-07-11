210277ced52aef0d782c6b8ab324a7de501ea1e1
cool725 <109250241+cool725@users.noreply.github.com>
2019-07-11 11:39:41 +0100

    nickwaller/ch15590/migrate-to-webpack-cancellation-page (#5752)
    * Create basic page setup
    
    * Adding tracking and functionality
    
    * Update all steps, reduce copy and simplify functionality
    
    * Add tracking to submit
    
    * Move var
    
    * Update copy to be more relative to newer phone prices

---
+++ src/AppBundle/Resources/public/rebrand/js/pages/user/user-cancel.js
@@ -0,0 +1,74 @@
+// user-cancel.js
+
+require('../../../sass/pages/user/user-cancel.scss');
+
+// Require BS component(s)
+
+// Require components
+require('jquery-validation');
+require('../../common/validationMethods.js');
+import tracking from '../../common/trackData.js';
+
+$(function() {
+
+    let validateForm = $('.validate-form'),
+        trackr;
+
+    const addValidation = () => {
+        validateForm.validate({
+            debug: false,
+            onkeyup: false,
+            focusCleanup: false,
+            onclick: false,
+            validClass: 'is-valid-ss',
+            errorClass: 'is-invalid',
+            rules: {
+                "cancel_form[reason]" : {
+                    required: true
+                }
+            },
+            messages: {
+                "cancel_form[reason]" : {
+                    required: 'Please select your reason for cancelling'
+                }
+            },
+
+            submitHandler: function(form) {
+                if (confirm('Are you sure you want to cancel your policy?')) {
+                    tracking(trackr, '', '');
+                    form.submit();
+                }
+            },
+        })
+    }
+
+    // Add validation
+    if (validateForm.data('client-validation')) {
+        addValidation();
+    }
+
+    $('#cancel_form_reason').on('change', function(e) {
+        let reason = $(this).val(),
+            index = $(this).prop('selectedIndex');
+
+        $('.reason').hide();
+        $('#reason_' + index).fadeIn('fast');
+
+        // Show other text input
+        if (index == 8) {
+            $('#cancel_form_othertxt').removeClass('hideme')
+                                      .addClass('required');
+        } else {
+            $('#cancel_form_othertxt').addClass('hideme')
+                                      .removeClass('required')
+                                      .next('label').hide();
+        }
+
+        // Set tracking event
+        if (reason != '') {
+            trackr = 'cancel-page-' + reason;
+        } else {
+            trackr = '';
+        }
+    });
+});
