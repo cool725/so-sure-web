486017d633a0edc013a9911ce5c0ec4e040262bb
cool725 <109250241+cool725@users.noreply.github.com>
2019-08-01 12:04:24 +0100

    Setup page controller, template and assets

---
+++ src/AppBundle/Controller/PromoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Exception\InvalidEmailException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use AppBundle\Document\Invitation\Invitation;
+use AppBundle\Document\Phone;
+use AppBundle\Document\PhonePolicy;
+use AppBundle\Document\SCode;
+use AppBundle\Document\Lead;
+use AppBundle\Document\Reward;
+use AppBundle\Document\Connection\RewardConnection;
+use AppBundle\Service\MixpanelService;
+use AppBundle\Service\SixpackService;
+use AppBundle\Service\MailerService;
+use AppBundle\Form\Type\LeadEmailType;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class PromoController extends BaseController
+{
+    /**
+     * @Route("/promo/{code}", name="promo")
+     * @Template
+     */
+    public function promoAction(Request $request, $code)
+    {
+        $dm = $this->getManager();
+        $repo = $dm->getRepository(Reward::class);
+        $phoneRepo = $dm->getRepository(Phone::class);
+
+        $reward = null;
+
+        try {
+            if ($reward = $repo->findOneBy(['code' => $code])) {
+                if (!$reward->getReward() || !$reward->getReward()->getUser()) {
+                    throw new \Exception('Unknown promo code');
+                }
+            }
+        } catch (Exception $e) {
+            $reward = null;
+        }
+
+        $session = $this->get('session');
+        $session->set('reward', $code);
+
+        if ($reward && $request->getMethod === "GET") {
+            $this->get('app.mixpanel')->queuePersonProperties([
+                'Attribution Invitation Method' => 'promo',
+            ], true);
+        }
+
+        return [
+            'promo'    => $reward,
+            'use_code' => $code,
+        ];
+    }
+
+
+}
---
+++ src/AppBundle/Resources/public/rebrand/js/pages/promo.js
@@ -0,0 +1,9 @@
+// promo.js
+
+require('../../sass/pages/promo.scss');
+
+// Require BS component(s)
+
+// Require components
+
+$(function() { });
