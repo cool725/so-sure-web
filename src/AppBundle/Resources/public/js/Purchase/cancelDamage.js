d9da7bbbedbbf0641aad39a8caf8fcfbebf2e30c
cool725 <109250241+cool725@users.noreply.github.com>
2019-08-05 14:28:20 +0100

    Remove old files we don't use (#5832)

--- src/AppBundle/Resources/public/js/Purchase/cancelDamage.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $('#cancel_form_cancel').on("click",function() {
-        return confirm('Are you sure you want to cancel your policy? Cancellation can not be undone and once cancelled, your phone will no longer be covered for Theft, Loss, & Accidental Damage.');
-    });
-});
