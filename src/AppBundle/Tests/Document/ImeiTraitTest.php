3470599e3c34cde1eb691c0f8294e6ba02a259a9
Daly Barron <dalygbarron@gmail.com>
2019-08-14 13:48:55 +0100

    test

--- src/AppBundle/Tests/Document/ImeiTraitTest.php
+++ src/AppBundle/Tests/Document/ImeiTraitTest.php
@@ -24,4 +24,24 @@ class ImeiTraitTest extends \PHPUnit\Framework\TestCase
             $this->assertEquals('00', mb_substr($imei, 0, 2));
         }
     }
+
+    /**
+     * Makes sure that the generate random imei code produces imei numbers that are random and do not collide.
+     * NOTE: there is an extremely small chance that this test will fail but as long as it does not fail repeatedly
+     *       that is ok.
+     */
+    public function testImeiCollision()
+    {
+        for ($r = 0; $r < 10; $r++) {
+            $imeis = [];
+            for ($i = 0; $i < 10; $i++) {
+                $imeis[] = $this->generateRandomImei();
+            }
+            for ($a = 0; $a < count($imeis); $a++) {
+                for ($b = $a + 1; $b < count($imeis); $b++) {
+                    $this->assertNotEquals($imeis[$a], $imeis[$b]);
+                }
+            }
+        }
+    }
 }
