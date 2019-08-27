728e5b60f605e211e09780d72744e3412a2bb801
cool725 <109250241+cool725@users.noreply.github.com>
2019-08-27 14:37:36 +0100

    nickwaller/ch16082/add-trust-elements-on-promo-pages (#5925)
    * Add toasts to page
    
    * Reduce padding on larger screens
    
    * Tweak br

--- src/AppBundle/Resources/public/rebrand/js/pages/promo.js
+++ src/AppBundle/Resources/public/rebrand/js/pages/promo.js
@@ -4,6 +4,7 @@ require('../../sass/pages/promo.scss');
 
 // Require BS component(s)
 require('bootstrap/js/dist/carousel');
+require('bootstrap/js/dist/toast');
 require('bootstrap/js/dist/util');
 
 // Require components
@@ -15,11 +16,39 @@ import lozad from 'lozad';
 const observer = lozad(); // lazy loads elements with default selector as '.lozad'
 observer.observe();
 
-// let textFit = require('textfit');
+let textFit = require('textfit');
 
 $(function() {
 
-    // textFit($('.fit')[0], {detectMul tiLine: false});
+    textFit($('.fit')[0], {detectMultiLine: false});
+
+    setTimeout(function() {
+        $('.toast-1').toast('show');
+    }, 2000);
+
+    $('.toast-1').on('hidden.bs.toast', function () {
+        setTimeout(function() {
+            $('.toast-2').toast('show');
+        }, 200);
+    })
+
+    $('.toast-2').on('hidden.bs.toast', function () {
+        setTimeout(function() {
+            $('.toast-3').toast('show');
+        }, 200);
+    })
+
+    $('.toast-3').on('hidden.bs.toast', function () {
+        setTimeout(function() {
+            $('.toast-4').toast('show');
+        }, 200);
+    })
+
+    $('.toast-4').on('hidden.bs.toast', function () {
+        setTimeout(function() {
+            $('.toast-5').toast('show');
+        }, 200);
+    })
 
     // $('.terms-link').on('click', function(e) {
     //     e.preventDefault();
