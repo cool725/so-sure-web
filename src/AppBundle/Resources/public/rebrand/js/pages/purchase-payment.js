46455c5a5982f59462988b0040006fe3ae7df93e
ajaypayne <blake@so-sure.com>
2019-09-02 14:43:11 +0100

    Blakepayne/ch16059/one step checkout update (#5946)
    * Pushing so Nick can take a look
    
    * Form was inside form
    
    * Remove comment
    
    * Sort js to adjust form based off radio selection
    
    * Update JS
    
    * Tidy up the JS
    
    * Tidy up
    
    * Add param
    
    * Remove removing warning
    
    * Add premiumInstallments to action on new checkout flow
    
    * Move to configure on pay button press
    
    * add Checkout.removeAllEventHandlers(Checkout.Events.CARD_TOKENISED);
    as the card tokenised events were stacking up
    
    * Add class to form and remove if card form defined
    
    * Removed card-option-with-bacs feature flag check so that we can use card
    without it

--- src/AppBundle/Resources/public/rebrand/js/pages/purchase-payment.js
+++ src/AppBundle/Resources/public/rebrand/js/pages/purchase-payment.js
@@ -10,34 +10,22 @@ require('jquery-validation');
 require('../common/validationMethods.js');
 let textFit = require('textfit');
 
-const sosure = sosure || {};
+$(function(){
 
-sosure.purchaseStepPayment = (function() {
-    let self = {};
-    self.form = null;
-    self.isIE = null;
-    self.loader = null;
-    self.webPay = null;
+    textFit($('.fit')[0], {detectMultiLine: false});
 
-    self.init = () => {
-        self.form = $('.validate-form');
-        self.isIE = !!navigator.userAgent.match(/Trident/g) || !!navigator.userAgent.match(/MSIE/g);
-        self.loader = $('#so-sure-loader');
-        self.webPay = $('#webpay-form');
-        if (self.form.data('client-validation') && !self.isIE) {
-            self.addValidation();
-        }
-    }
+    let validateForm = $('.validate-form'),
+        isIE = !!navigator.userAgent.match(/Trident/g) || !!navigator.userAgent.match(/MSIE/g),
+        paymentForm = $('.payment-form');
 
-    self.addValidation = () => {
-        self.form.validate({
+    const addValidation = () => {
+        validateForm.validate({
             debug: true,
             // When to validate
             validClass: 'is-valid-ss',
             errorClass: 'is-invalid',
             onfocusout: false,
             onkeyup: false,
-            // onclick: false,
             rules: {
 
             },
@@ -59,14 +47,51 @@ sosure.purchaseStepPayment = (function() {
         });
     }
 
-    return self;
-})();
+    // Add validation
+    if (validateForm.data('client-validation') && !isIE) {
+        addValidation();
+    }
 
-$(function(){
-
-    textFit($('.fit')[0], {detectMultiLine: false});
-
-    sosure.purchaseStepPayment.init();
+    const configureCheckout = () => {
+        Checkout.configure({
+            publicKey: paymentForm.data('public-key'),
+            customerEmail: paymentForm.data('customer-email'),
+            value: paymentForm.data('value'),
+            currency: paymentForm.data('currency'),
+            debugMode: paymentForm.data('debug-mode'),
+            paymentMode: paymentForm.data('payment-mode'),
+            cardFormMode: paymentForm.data('card-form-mode'),
+            title: paymentForm.data('title'),
+            subtitle: paymentForm.data('subtitle'),
+            logoUrl: 'https://cdn.so-sure.com/images/rebrand/logo/so-sure_logo_checkout.svg',
+            themeColor: '#2593f3',
+            forceMobileRedirect: true,
+            redirectUrl: paymentForm.data('url'),
+            cardTokenised: function(event) {
+                $('html, body').animate({ scrollTop: 0 }, 'fast');
+                // Show loading screen
+                $('.loading-screen').fadeIn();
+                // Scroll to top of page
+                // console.log(event.data.cardToken);
+                let url = paymentForm.data('url'),
+                    csrf = paymentForm.data('csrf'),
+                    pennies = paymentForm.data('value');
+                // console.log(url);
+                $.post(url, {'csrf': csrf, 'token': event.data.cardToken, 'pennies': pennies}, function(resp) {
+                    // console.log(resp);
+                }).fail(function() {
+                    $('.loading-screen').fadeOut();
+                }).always(function() {
+                    let redirect = paymentForm.data('redirect-url');
+                    if (redirect) {
+                        window.location.href = redirect;
+                    } else {
+                        window.location.reload(false);
+                    }
+                });
+            }
+        });
+    }
 
     // TODO: Move to component
     $('.radio-btn').on('click', function(e) {
@@ -80,14 +105,43 @@ $(function(){
         $('input[name="purchase_form[amount]"][value="' + val + '"]').prop('checked', true);
 
         // Adjust the price in the copy
-        let premium = $(this).data('premium-type');
-        let price = $('#purchase_price');
+        let premium = $(this).data('premium-type'),
+            price = $('#purchase_price');
+
         price.html('&pound;' + val + ' a ' + premium);
+
+        let checkoutUrl = paymentForm.data('url'),
+            type = $(this).data('premium-param');
+            url = new URL(checkoutUrl);
+            amount = val * 100;
+        url.searchParams.set('pennies', amount);
+        url.searchParams.set('premium', type);
+        let newUrl = url.href;
+
+        console.log(newUrl);
+
+        // Clear payment data
+        paymentForm.removeData('value');
+        paymentForm.removeData('url');
+
+        // Update the checkout form
+        paymentForm.attr('data-value', amount);
+        paymentForm.attr('data-url', newUrl);
+
+        console.log(paymentForm.data());
     });
 
-    if ($.trim($('#Reference').val()).length > 0) {
-        // Show loading overlay
-        sosure.purchaseStepPayment.loader.show();
-        sosure.purchaseStepPayment.webPay.submit();
-    }
+    console.log(paymentForm.data());
+
+    $('.btn-card-pay').on('click', function(e) {
+        e.preventDefault();
+
+        // Ensurre that all previous configure event handlers are cleared before adding another
+        Checkout.removeAllEventHandlers(Checkout.Events.CARD_TOKENISED);
+        // Set configure
+        configureCheckout();
+
+        // Open the lightbox
+        Checkout.open();
+    });
 });
