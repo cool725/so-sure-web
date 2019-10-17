772e3e136783da3d05e3dd48f444a1f77e16cead
Elliot Mitchell <elliot@so-sure.com>
2019-10-17 17:18:40 +0100

    Reverse array to reverse pullout csv (#6117)

--- src/AppBundle/Command/PulloutCommand.php
+++ src/AppBundle/Command/PulloutCommand.php
@@ -176,6 +176,9 @@ class PulloutCommand extends ContainerAwareCommand
                 ];
             }
         }
+        // order descending by date
+        array_reverse($rows, true);
+
         // now write the array.
         $file = fopen($filename, "w");
         fwrite($file, "Date,Campaign,Channel,Device,Create Account,Purchased,Net\n");
