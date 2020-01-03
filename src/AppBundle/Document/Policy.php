c98257a4182e54f6249a874efe5c6b2a4fcc8648
Elliot Mitchell <elliot@so-sure.com>
2020-01-03 12:50:03 +0000

    elliot_mitchell/ch17283/api-to-return-helvetia-phone-type

--- src/AppBundle/Document/Policy.php
+++ src/AppBundle/Document/Policy.php
@@ -6390,7 +6390,7 @@ abstract class Policy
             'id' => $this->getId(),
             'status' => $this->getApiStatus(),
             //TODO - change this to fetch the actual policy_type
-            'type' => 'salva-phone',
+            'type' => 'helvetia-phone',
             'start_date' => $this->getStart() ? $this->getStart()->format(\DateTime::ATOM) : null,
             'end_date' => $this->getEnd() ? $this->getEnd()->format(\DateTime::ATOM) : null,
             'policy_number' => $this->getPolicyNumber(),
