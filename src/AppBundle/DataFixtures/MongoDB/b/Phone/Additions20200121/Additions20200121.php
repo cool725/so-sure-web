4e47b59139ce5e8488fd4525a0c7ebf47f1bfc0d
cool725 <109250241+cool725@users.noreply.github.com>
2020-01-21 17:24:49 +0000

    Add Sony Xperia 10 and 10 Plus (#6400)

---
+++ src/AppBundle/DataFixtures/MongoDB/b/Phone/Additions20200121/Additions20200121.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\DataFixtures\MongoDB\b\Phone\Additions20200121;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Document\Phone;
+use AppBundle\DataFixtures\MongoDB\b\Phone\LoadPhoneData;
+
+class Additions20200121 extends LoadPhoneData implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $this->loadCsv($manager, '20200121.csv', new \DateTime('2020-01-21'));
+    }
+}
