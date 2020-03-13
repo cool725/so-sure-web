730fa564f794b2874cec2bf3201a6e453692cc52
cool725 <109250241+cool725@users.noreply.github.com>
2020-03-13 11:55:25 +0000

    Add shortcut to hide dev bar (#6535)

--- src/AppBundle/Resources/public/rebrand/js/dev.js
+++ src/AppBundle/Resources/public/rebrand/js/dev.js
@@ -4,3 +4,9 @@ $('#dev-reload').on('click', function(e) {
     e.preventDefault();
     window.location.reload(true);
 });
+
+$(document).keydown(function(e) {
+    if(e.shiftKey && e.which == 71) {
+        $('#dev_bar').toggleClass('hideme');
+    };
+});
