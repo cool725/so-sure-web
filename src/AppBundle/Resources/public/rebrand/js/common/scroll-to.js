96ca803e2bbfb2cbfb7e0c3deb5a5c16c3c3ec0d
cool725 <109250241+cool725@users.noreply.github.com>
2020-04-08 10:39:50 +0100

    nickwaller/ch18026/change-order-of-sections-on-quote-page (#6595)
    * Moving content
    
    * Add offset to scroll-to
    
    * Wrong href

--- src/AppBundle/Resources/public/rebrand/js/common/scroll-to.js
+++ src/AppBundle/Resources/public/rebrand/js/common/scroll-to.js
@@ -3,10 +3,15 @@
 $('.scroll-to').on('click', function(e) {
     e.preventDefault();
 
-    let anchor = $(this).data('scroll-to-anchor');
+    let anchor = $(this).data('scroll-to-anchor'),
+        offset = 0;
+
+    if ($(this).data('scroll-to-offset')) {
+        offset = $(this).data('scroll-to-offset');
+    }
 
     $('html, body').animate({
-        scrollTop: $(anchor).offset().top
+        scrollTop: $(anchor).offset().top - offset
     }, 1500);
 
 });
