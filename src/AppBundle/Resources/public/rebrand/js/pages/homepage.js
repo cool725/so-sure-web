41b5a76a3f99f89455bc077294b403e9b79771c8
cool725 <109250241+cool725@users.noreply.github.com>
2020-10-22 11:00:37 +0100

    nickwaller/ch18890/add-tp-element-on-homepage (#6853)
    * Move TP widget to first in the cred bar
    
    * Add trust elements
    
    * Tweak positioning and add new reviews
    
    * Update reviews

--- src/AppBundle/Resources/public/rebrand/js/pages/homepage.js
+++ src/AppBundle/Resources/public/rebrand/js/pages/homepage.js
@@ -4,6 +4,7 @@ require('../../sass/pages/homepage.scss');
 
 // Require BS component(s)
 require('bootstrap/js/dist/util');
+require('bootstrap/js/dist/toast');
 require('bootstrap/js/dist/tab');
 
 // Require components
@@ -51,5 +52,33 @@ $(function() {
     $('.review__controls-next').on('click', function(e) {
         $carousel.flickity('next');
     });
+
+    setTimeout(function() {
+        $('.toast-1').toast('show');
+    }, 2000);
+
+    $('.toast-1').on('hidden.bs.toast', function () {
+        setTimeout(function() {
+            $('.toast-2').toast('show');
+        }, 200);
+    })
+
+    $('.toast-2').on('hidden.bs.toast', function () {
+        setTimeout(function() {
+            $('.toast-3').toast('show');
+        }, 200);
+    })
+
+    $('.toast-3').on('hidden.bs.toast', function () {
+        setTimeout(function() {
+            $('.toast-4').toast('show');
+        }, 200);
+    })
+
+    $('.toast-4').on('hidden.bs.toast', function () {
+        setTimeout(function() {
+            $('.toast-5').toast('show');
+        }, 200);
+    })
 });
 
