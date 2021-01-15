3b9a71d68b2f45f1ffbee11cee4d3d0497dbcaf2
Daly Barron <dalygbarron@gmail.com>
2021-01-15 09:41:10 +0000

    standards

--- src/AppBundle/Command/BacsCoverCommand.php
+++ src/AppBundle/Command/BacsCoverCommand.php
@@ -5,10 +5,11 @@ namespace AppBundle\Command;
 use AppBundle\Document\PaymentMethod\BacsPaymentMethod;
 use AppBundle\Document\PaymentMethod\CheckoutPaymentMethod;
 use AppBundle\Document\BankAccount;
+use AppBundle\Document\ScheduledPayment;
 use AppBundle\Document\Payment\BacsPayment;
 use AppBundle\Repository\BacsPaymentRepository;
 use AppBundle\Repository\UserRepository;
-use AppBundle\Service\CheckoutService;
+use AppBundle\Service\BacsService;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
@@ -25,22 +26,22 @@ use phpseclib\Crypt\RSA;
  */
 class BacsCoverCommand extends ContainerAwareCommand
 {
-    /** @var DocumentManager  */
+    /** @var DocumentManager */
     protected $dm;
 
-    /** @var CheckoutService  */
-    protected $checkoutService;
+    /** @var BacsService */
+    protected $bacsService;
 
     /**
      * Creates the command object and injects the dependencies.
-     * @param DocumentManager $dm              is used to query the database and stuff.
-     * @param CheckoutService $checkoutService is used to do checkout stuff.
+     * @param DocumentManager $dm          is used to query the database and stuff.
+     * @param BacsService     $bacsService is used to do checkout stuff.
      */
-    public function __construct(DocumentManager $dm, CheckoutService $checkoutService)
+    public function __construct(DocumentManager $dm, BacsService $bacsService)
     {
         parent::__construct();
         $this->dm = $dm;
-        $this->checkoutService = $checkoutService;
+        $this->bacsService = $bacsService;
     }
 
     /**
@@ -72,25 +73,14 @@ class BacsCoverCommand extends ContainerAwareCommand
                 $payment->getAmount()
             ));
             if ($wet) {
-                $paymentMethod = $payment->getPolicy()->getCheckoutPaymentMethod();
-                if ($paymentMethod) {
-                    $this->bacsService->scheduledBacsPayment(
-                        $payment->getPolicy(),
-                        0 - $payment->getAmount(),
-                        ScheduledPayment::TYPE_REFUND,
-                        'covering payment refund'
-                    );
-                    $payment->setCoveringPaymentRefunded(true);
-                    $bacsPaymentMethod = new BacsPaymentMethod();
-                    $bacsPaymentMethod->setBankAccount($paymentMethod->getCoveringBankAccount());
-                    $payment->getPolicy()->setPaymentMethod($bacsPaymentMethod);
-                    $this->dm->flush();
-                } else {
-                    $output->writeln(sprintf(
-                        'payment method has changed for policy %s',
-                        $payment->getPolicy()->getId()
-                    ));
-                }
+                $this->bacsService->scheduleBacsPayment(
+                    $payment->getPolicy(),
+                    0 - $payment->getAmount(),
+                    ScheduledPayment::TYPE_REFUND,
+                    'covering payment refund'
+                );
+                $payment->setCoveringPaymentRefunded(true);
+                $this->dm->flush();
             }
         }
     }
