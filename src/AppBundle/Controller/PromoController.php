21cb3778b5ea5b410d307faee6f8804d7115a8cd
cool725 <109250241+cool725@users.noreply.github.com>
2021-03-15 16:30:39 +0000

    Add landing pages (#7063)

--- src/AppBundle/Controller/PromoController.php
+++ src/AppBundle/Controller/PromoController.php
@@ -42,6 +42,8 @@ class PromoController extends BaseController
      * @Route("/canopy/{code}", name="canopy_promo")
      * @Route("/youatwork/{code}", name="youatwork_promo")
      * @Route("/creditspring/{code}", name="creditspring_promo")
+     * @Route("/ben/{code}", name="ben_promo")
+     * @Route("/suitsme/{code}", name="suitsme_promo")
      * @Template
      */
     public function promoAction(Request $request, $code)
@@ -111,6 +113,10 @@ class PromoController extends BaseController
             $custom = 'youatwork';
         } elseif ($request->get('_route') == 'creditspring_promo') {
             $custom = 'creditspring';
+        } elseif ($request->get('_route') == 'ben_promo') {
+            $custom = 'ben';
+        } elseif ($request->get('_route') == 'suitsme_promo') {
+            $custom = 'suitsme';
         }
 
         $lead = new Lead();
