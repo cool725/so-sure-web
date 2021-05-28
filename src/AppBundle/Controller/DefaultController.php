5f4c397dee1ecd914145225bd4a7d28060afbe77
cool725 <109250241+cool725@users.noreply.github.com>
2021-05-28 16:54:36 +0100

    nickwaller/ch20122/remove-experiment-hero (#7171)
    * Create hero experiment
    
    * Latest, remove exp
    
    * Add slight opactity to images

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -81,7 +81,6 @@ class DefaultController extends BaseController
      * @Route("/replacement-72", name="replacement_72_landing")
      * @Route("/reimagined", name="reimagined")
      * @Route("/hasslefree", name="hasslefree")
-     * @Route("/alt", name="homepage_alt")
      */
     public function indexAction(Request $request)
     {
@@ -107,26 +106,21 @@ class DefaultController extends BaseController
         ]);
 
         // $fromPrice = $this->getLowestPremium();
-        $alt = false;
-        if ($request->get('_route') == 'homepage_alt') {
-            $alt = true;
-        }
 
         // Set Greeting
         if (date('H') >= 12 && date('H') <= 18) {
-            $greeting = 'Afternoon';
+            $greeting = 'afternoon';
         } elseif (date('H') > 18 && date('H') <= 22) {
-            $greeting = 'Evening';
+            $greeting = 'evening';
         } elseif (date('H') > 22 && date('H') <= 5) {
-            $greeting = 'Night';
+            $greeting = 'night';
         } else {
-            $greeting = 'Morning';
+            $greeting = 'morning';
         }
 
         $data = array(
             'competitor' => $competitorData::$competitorComparisonData,
             'is_noindex' => $noindex,
-            'hero_exp' => $alt,
             'greeting' => $greeting,
             // 'from_price' => $fromPrice
         );
