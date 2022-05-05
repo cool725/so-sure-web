cc1669a7d49b4b7719d698a17fdc4a1bc9a923f4
AlexS-a <alex.s.a1994@gmail.com>
2022-05-05 16:46:24 +0100

    Fixed whitespace

--- src/AppBundle/Document/PhonePolicy.php
+++ src/AppBundle/Document/PhonePolicy.php
@@ -964,8 +964,8 @@ abstract class PhonePolicy extends Policy
      */
     public function fullPremiumToBePaidForClaim(\DateTime $date, $claimType)
     {
-        if (!in_array($claimType, [Claim::TYPE_THEFT, Claim::TYPE_LOSS]) || 
-            $this->getOutstandingPremium() <= 0 || 
+        if (!in_array($claimType, [Claim::TYPE_THEFT, Claim::TYPE_LOSS]) ||
+            $this->getOutstandingPremium() <= 0 ||
             $this->hasPreviousPolicy()
         ) {
             return false;
