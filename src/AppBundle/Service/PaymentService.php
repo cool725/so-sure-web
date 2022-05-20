108e25e957ef6397d2a7089fee4be1a6cad4ce73
Daly Graham Barron <dalygbarron@gmail.com>
2022-05-20 17:32:21 +0100

    wrong command again

--- src/AppBundle/Service/PaymentService.php
+++ src/AppBundle/Service/PaymentService.php
@@ -314,7 +314,7 @@ class PaymentService
     {
         $this->dm->createQueryBuilder(ScheduledPayment::class)
             ->hydrate(false)
-            ->field('labels')->popFirst($label)
+            ->field('labels')->pullAll($label)
             ->getQuery()->execute();
     }
 
